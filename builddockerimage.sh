7e63c9b441eed188b17c9b4e435fc6f182012a72
dharlabwustl <atulkumar@wustl.edu>
2022-12-27 12:35:44 -0600

    3rd commit

--- builddockerimage.sh
+++ builddockerimage.sh
@@ -1,16 +1,17 @@
-# ./bashtowriteDockerfile.sh
-cat ./Dockerfile_part1 > Dockerfile
+ ./bashtowriteDockerfile.sh
+parent_dir=${1}
+#cat ${parent_dir}/Dockerfile_part1 > ${parent_dir}/Dockerfile
 echo "  "
 command=""
-for x in *.json ;
+for x in ${parent_dir}/*.json ;
 do 
 	command="${command}   ${x}  "
 done
 echo $command
-python /media/atul/WDJan2022/WASHU_WORKS/PROJECTS/FROM_DOCUMENTS/docker-images/command2label.py  $command  >> ./Dockerfile
+python /media/atul/WDJan2022/WASHU_WORKS/PROJECTS/FROM_DOCUMENTS/docker-images/command2label.py  $command  >> ${parent_dir}/Dockerfile
 # imagename=$1
-imagename=fsl502py369withpacksnltx
+imagename=${2} #fsl502py369withpacksnltx
 #docker build -t sharmaatul11/${imagename} .
 #docker push sharmaatul11/${imagename}
-docker build -t registry.nrg.wustl.edu/docker/nrg-repo/sharmaatul11/${imagename} .
+docker build -t registry.nrg.wustl.edu/docker/nrg-repo/sharmaatul11/${imagename} ${parent_dir}
 docker push registry.nrg.wustl.edu/docker/nrg-repo/sharmaatul11/${imagename}

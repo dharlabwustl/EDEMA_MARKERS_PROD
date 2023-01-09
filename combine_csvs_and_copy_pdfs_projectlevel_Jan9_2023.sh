78345c9e7cb00a49650fe94879295b6008a318de
dharlabwustl <atulkumar@wustl.edu>
2023-01-09 13:03:30 -0600

    3rd commit

--- combine_csvs_and_copy_pdfs_projectlevel_Jan9_2023.sh
+++ combine_csvs_and_copy_pdfs_projectlevel_Jan9_2023.sh
@@ -343,7 +343,7 @@ combine_all_csvfiles_of_edema_biomarker  ${working_dir} ${output_directory} ${ex
 ## COPY IT TO THE SNIPR RESPECTIVE PROJECT RESOURCES
 
 snipr_output_foldername="EDEMA_BIOMARKER"
-file_suffixes=(  .pdf columndropped.csv ) #sys.argv[5]
+file_suffixes=(  .pdf .csv ) #sys.argv[5]
 for file_suffix in ${file_suffixes[@]}
 do
     copyoutput_to_snipr_projectlevel  ${projectID} ${output_directory} "${snipr_output_foldername}"   ${file_suffix}  
@@ -355,7 +355,7 @@ done
 ## COPY PDFs TO THE SNIPR RESPECTIVE PROJECT RESOURCES
 
 snipr_output_foldername="EDEMA_BIOMARKER"
-file_suffixes=(  .pdf ) #sys.argv[5]
+file_suffixes=(  .pdf columndropped.csv ) #sys.argv[5]
 for file_suffix in ${file_suffixes[@]}
 do
     copyoutput_to_snipr_projectlevel  ${projectID} ${working_dir} "${snipr_output_foldername}"   ${file_suffix}  

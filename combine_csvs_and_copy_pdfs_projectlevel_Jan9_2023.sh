1a3f1ce3c5b974651042db4235c70ae87ff6aa82
dharlabwustl <atulkumar@wustl.edu>
2023-01-09 13:40:30 -0600

    3rd commit

--- combine_csvs_and_copy_pdfs_projectlevel_Jan9_2023.sh
+++ combine_csvs_and_copy_pdfs_projectlevel_Jan9_2023.sh
@@ -354,12 +354,12 @@ done
 ######################################################################################################################
 ## COPY PDFs TO THE SNIPR RESPECTIVE PROJECT RESOURCES
 
-snipr_output_foldername="EDEMA_BIOMARKER"
-file_suffixes=(  .pdf ) #sys.argv[5]
-for file_suffix in ${file_suffixes[@]}
-do
-    copyoutput_to_snipr_projectlevel  ${projectID} ${working_dir} "${snipr_output_foldername}"   ${file_suffix}  
-done
+#snipr_output_foldername="EDEMA_BIOMARKER"
+#file_suffixes=(  .pdf ) #sys.argv[5]
+#for file_suffix in ${file_suffixes[@]}
+#do
+#    copyoutput_to_snipr_projectlevel  ${projectID} ${working_dir} "${snipr_output_foldername}"   ${file_suffix}
+#done
 
 ######################################################################################################################
 ## for one project:

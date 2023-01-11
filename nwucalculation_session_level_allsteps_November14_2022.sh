85956e1fa0c731028e49486fbbf332c739b26d57
dharlabwustl <atulkumar@wustl.edu>
2023-01-11 10:40:22 -0600

    3rd commit

--- nwucalculation_session_level_allsteps_November14_2022.sh
+++ nwucalculation_session_level_allsteps_November14_2022.sh
@@ -95,7 +95,7 @@ thisfile_basename=$(basename $this_filename)
 # for texfile in $(/usr/lib/fsl/5.0/remove_ext ${output_directory}/$thisfile_basename)*.tex ; 
 for texfile in  ${output_directory}/*.tex ; 
 do 
-pdflatex -output-directory=${output_directory} $texfile   ##${output_directory}/$(/usr/lib/fsl/5.0/remove_ext $this_filename)*.tex
+pdflatex -output-directory=${output_directory} $texfile  || break && exit   ##${output_directory}/$(/usr/lib/fsl/5.0/remove_ext $this_filename)*.tex
 rm ${output_directory}/*.aux
 rm ${output_directory}/*.log
 done 

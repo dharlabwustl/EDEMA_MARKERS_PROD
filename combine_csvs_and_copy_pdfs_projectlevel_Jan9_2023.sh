aac4543f4a1ebe61a96fa919b79a51f3301d19c3
dharlabwustl <atulkumar@wustl.edu>
2023-01-24 16:22:21 -0600

    3rd commit

--- combine_csvs_and_copy_pdfs_projectlevel_Jan9_2023.sh
+++ combine_csvs_and_copy_pdfs_projectlevel_Jan9_2023.sh
@@ -325,6 +325,19 @@ sys.path.append('/software');
 from download_with_session_ID import *; 
 call_combine_all_csvfiles_of_edema_biomarker()"  ${working_directory} ${working_directory_tocombinecsv} ${extension} ${outputfilename}
 }
+copy_latest_pdfs()
+{
+pdffileprefix=${1}
+pdffiledirectory=${2}
+destinationdirectory=${3}
+
+
+python3 -c "
+import sys
+sys.path.append('/software');
+from download_with_session_ID import *;
+call_copy_latest_pdffile()"  ${working_directory} ${working_directory_tocombinecsv} ${extension} ${outputfilename}
+}
 
 projectID=${1}
 

6afea71462a34b9fa28a5e82ddd36ac3669575b1
dharlabwustl <atulkumar@wustl.edu>
2023-01-27 14:03:16 -0600

    3rd commit

--- analyzed_session_list.sh
+++ analyzed_session_list.sh
@@ -338,6 +338,19 @@ sys.path.append('/software');
 from download_with_session_ID import *;
 call_copy_latest_pdffile()"  ${pdffileprefix} ${pdffiledirectory} ${destinationdirectory}
 }
+add_a_column_to_csv()
+{
+csvfile=sys.argv[1]
+columname=sys.argv[2]
+columnvalue=sys.argv[3]
+
+
+python3 -c "
+import sys
+sys.path.append('/software');
+from download_with_session_ID import *;
+call_add_a_column()"  ${csvfile} ${columname} ${columnvalue}
+}
 
 projectID=${1}
 
@@ -345,41 +358,46 @@ projectID=${1}
 ## 1. download all the sessions list. 2. For each session download its metadata. 3. if it has type == z-axial get its metadata 4. If the scan metadata URI has 'EDEMA_BIOMARKERS' in it
 #then  download the csv and pdf from this metadata
 get_all_selected_scan_in_a_project ${projectID} ${working_dir}
+for each_csv in ${working_dir}/SNI*.csv ;
+do
+add_a_column_to_csv ${each_csv} "SESSION_ID" $(basename ${each_csv%.csv})
+done
 ############################## get_all_EDEMA_BIOMARKER_csvfiles_of_allselectedscan   #############################################
-get_all_EDEMA_BIOMARKER_csvfiles_of_allselectedscan ${working_dir}
+#get_all_EDEMA_BIOMARKER_csvfiles_of_allselectedscan ${working_dir}
 
 ############################## combine_all_csvfiles_of_edema_biomarker   #############################################
-extension_csv="columndropped.csv" #"0_40TOTAL.csv"
-combined_csv_outputfilename=${projectID}_EDEMA_BIOMARKERS_COMBINED_${extension_csv}
-output_directory="/workingoutput"
-combine_all_csvfiles_of_edema_biomarker  ${working_dir} ${output_directory} ${extension_csv} ${combined_csv_outputfilename}
+#extension_csv="columndropped.csv" #"0_40TOTAL.csv"
+#combined_csv_outputfilename=${projectID}_EDEMA_BIOMARKERS_COMBINED_${extension_csv}
+#output_directory="/workingoutput"
+##combine_all_csvfiles_of_edema_biomarker  ${working_dir} ${output_directory} ${extension_csv} ${combined_csv_outputfilename}
 
-combinedfilename=$(find ${output_directory} -name *COMBINED_columndropped.csv)
-while IFS=',' read -ra array; do
-copy_latest_pdfs ${array[0]} ${working_dir} ${final_output_directory}
-done < <( tail -n +2 "${combinedfilename}" )
+#combinedfilename=$(find ${output_directory} -name *COMBINED_columndropped.csv)
+#while IFS=',' read -ra array; do
+#copy_latest_pdfs ${array[0]} ${working_dir} ${final_output_directory}
+#done < <( tail -n +2 "${combinedfilename}" )
 ######################################################################################################################
 ## COPY IT TO THE SNIPR RESPECTIVE PROJECT RESOURCES
-
+### ALL SESSIONS IN THIS PROJECT
+curl  -u   $XNAT_USER:$XNAT_PASS  -X GET   $XNAT_HOST/data/projects/${projectID}/experiments/?format=csv  > ${working_dir}/all_sessions.csv
 snipr_output_foldername="EDEMA_BIOMARKER"
-file_suffixes=( COMBINED_columndropped.csv ) #sys.argv[5] .mat
-for file_suffix in ${file_suffixes[@]}
-do
-    copyoutput_to_snipr_projectlevel  ${projectID} ${output_directory} "${snipr_output_foldername}"   ${file_suffix}
-done
+#file_suffixes=( COMBINED_columndropped.csv ) #sys.argv[5] .mat
+#for file_suffix in ${file_suffixes[@]}
+#do
+#    copyoutput_to_snipr_projectlevel  ${projectID} ${output_directory} "${snipr_output_foldername}"   ${file_suffix}
+#done
 
 ######################################################################################################################
 
 ######################################################################################################################
 ## COPY PDFs TO THE SNIPR RESPECTIVE PROJECT RESOURCES
 
-snipr_output_foldername="EDEMA_BIOMARKER"
-file_suffixes=(  .pdf COMBINED_columndropped.csv ) #sys.argv[5]
-for file_suffix in ${file_suffixes[@]}
-do
-#    copyoutput_to_snipr_projectlevel  ${projectID} ${working_dir} "${snipr_output_foldername}"   ${file_suffix}
-     copyoutput_to_snipr_projectlevel  ${projectID} ${final_output_directory} "${snipr_output_foldername}"   ${file_suffix}
-done
+#snipr_output_foldername="EDEMA_BIOMARKER"
+#file_suffixes=(  .pdf COMBINED_columndropped.csv ) #sys.argv[5]
+#for file_suffix in ${file_suffixes[@]}
+#do
+##    copyoutput_to_snipr_projectlevel  ${projectID} ${working_dir} "${snipr_output_foldername}"   ${file_suffix}
+#     copyoutput_to_snipr_projectlevel  ${projectID} ${final_output_directory} "${snipr_output_foldername}"   ${file_suffix}
+#done
 
 ######################################################################################################################
 ## for one project:

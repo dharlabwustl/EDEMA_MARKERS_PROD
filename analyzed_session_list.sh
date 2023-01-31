0e5ed52e5cf7e7d5fdd20ff23cf367d0c07b5dd2
dharlabwustl <atulkumar@wustl.edu>
2023-01-31 11:59:23 -0600

    3rd commit

--- analyzed_session_list.sh
+++ analyzed_session_list.sh
@@ -382,8 +382,9 @@ snipr_output_foldername="EDEMA_BIOMARKER"
 prefix="SNIPR"
 #combine_all_csvfiles_of_edema_biomarker_withprefix  ${working_dir} ${output_directory} ${prefix} ${combined_csv_outputfilename}
 ## download all the pdf and csv at the project level to a folder:
-curl  -u   $XNAT_USER:$XNAT_PASS  -X GET   $XNAT_HOST/data/projects/${projectID}/resources/${snipr_output_foldername}/files?format=csv  > ${working_dir}/allfilesinprojectoutput.csv
-curl  -u   $XNAT_USER:$XNAT_PASS  -X GET   $XNAT_HOST/data/projects/${projectID}/experiments/?format=csv  > ${working_dir}/all_sessions.csv
+echo $XNAT_USER:$XNAT_PASS:$XNAT_HOST
+#curl  -u   $XNAT_USER:$XNAT_PASS  -X GET   $XNAT_HOST/data/projects/${projectID}/resources/${snipr_output_foldername}/files?format=csv  > ${working_dir}/allfilesinprojectoutput.csv
+#curl  -u   $XNAT_USER:$XNAT_PASS  -X GET   $XNAT_HOST/data/projects/${projectID}/experiments/?format=csv  > ${working_dir}/all_sessions.csv
 
 
 ############################## get_all_EDEMA_BIOMARKER_csvfiles_of_allselectedscan   #############################################

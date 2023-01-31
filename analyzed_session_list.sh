1cb794da4ebf37f84716f6438ffcc72e48371fed
dharlabwustl <atulkumar@wustl.edu>
2023-01-31 12:43:45 -0600

    3rd commit

--- analyzed_session_list.sh
+++ analyzed_session_list.sh
@@ -377,7 +377,7 @@ do
 add_a_column_to_csv ${each_csv} "SESSION_ID" $(basename ${each_csv%.csv})
 done
 #extension_csv="columndropped.csv" #"0_40TOTAL.csv"
-combined_csv_outputfilename="${projectID}_${snipr_output_foldername}_COMBINED_${extension_csv}"
+combined_csv_outputfilename="${projectID}_${snipr_output_foldername}_ANALYZED_${extension_csv}"
 output_directory="/workingoutput"
 prefix="SNIPR"
 combine_all_csvfiles_of_edema_biomarker_withprefix  ${working_dir} ${output_directory} ${prefix} ${combined_csv_outputfilename}

23414cd7a5e691c340a65b44ee54999403e23f8f
dharlabwustl <atulkumar@wustl.edu>
2023-01-31 12:33:57 -0600

    3rd commit

--- script_to_call_main_program.sh
+++ script_to_call_main_program.sh
@@ -1,46 +1,46 @@
 #!/bin/bash 
-
-SESSION_ID=${1}
-XNAT_USER=${2}
-XNAT_PASS=${3}
-TYPE_OF_PROGRAM=${4}
-export XNAT_HOST=${5}
-echo ${TYPE_OF_PROGRAM}::TYPE_OF_PROGRAM
-if [[ ${TYPE_OF_PROGRAM} == 2 ]] ;
-then 
-    /software/nwucalculation_session_level_allsteps_November14_2022.sh $SESSION_ID $XNAT_USER $XNAT_PASS $XNAT_HOST /input /output
-fi 
-if [[ ${TYPE_OF_PROGRAM} == 1 ]] ;
-then 
-    /software/dicom2nifti_call_sessionlevel_selected.sh  ${SESSION_ID} $XNAT_USER $XNAT_PASS $XNAT_HOST
-fi 
-
-if [[ ${TYPE_OF_PROGRAM} == 3 ]] ;
-then
-  PROJECT_ID=${1}
-    /software/combine_csvs_and_copy_pdfs_projectlevel_Jan9_2023.sh  ${PROJECT_ID} $XNAT_USER $XNAT_PASS $XNAT_HOST
-fi
-
-if [[ ${TYPE_OF_PROGRAM} == 4 ]] ;
-then
-
-   /software/nwucalculation_scan_level_allsteps.sh
-fi
-
-
-if [[ ${TYPE_OF_PROGRAM} == 5 ]] ;
-then
-
-   /software/nwucalculation_onlocalcomp_aftersegJan172022.sh
-fi
-if [[ ${TYPE_OF_PROGRAM} == 6 ]] ;
-then
-
-   /software/combine_csvs_and_copy_pdfs_projectlevel_Jan17_2023_LocalComputer.sh
-fi
-
-if [[ ${TYPE_OF_PROGRAM} == 7 ]] ;
-then
-echo ${PROJECT_ID}::$XNAT_USER::$XNAT_PASS::$XNAT_HOST
-#   /software/analyzed_session_list.sh  ${PROJECT_ID} $XNAT_USER $XNAT_PASS $XNAT_HOST
-fi
+echo "I AM HERE ATUL"
+#SESSION_ID=${1}
+#XNAT_USER=${2}
+#XNAT_PASS=${3}
+#TYPE_OF_PROGRAM=${4}
+#export XNAT_HOST=${5}
+#echo ${TYPE_OF_PROGRAM}::TYPE_OF_PROGRAM
+#if [[ ${TYPE_OF_PROGRAM} == 2 ]] ;
+#then
+#    /software/nwucalculation_session_level_allsteps_November14_2022.sh $SESSION_ID $XNAT_USER $XNAT_PASS $XNAT_HOST /input /output
+#fi
+#if [[ ${TYPE_OF_PROGRAM} == 1 ]] ;
+#then
+#    /software/dicom2nifti_call_sessionlevel_selected.sh  ${SESSION_ID} $XNAT_USER $XNAT_PASS $XNAT_HOST
+#fi
+#
+#if [[ ${TYPE_OF_PROGRAM} == 3 ]] ;
+#then
+#  PROJECT_ID=${1}
+#    /software/combine_csvs_and_copy_pdfs_projectlevel_Jan9_2023.sh  ${PROJECT_ID} $XNAT_USER $XNAT_PASS $XNAT_HOST
+#fi
+#
+#if [[ ${TYPE_OF_PROGRAM} == 4 ]] ;
+#then
+#
+#   /software/nwucalculation_scan_level_allsteps.sh
+#fi
+#
+#
+#if [[ ${TYPE_OF_PROGRAM} == 5 ]] ;
+#then
+#
+#   /software/nwucalculation_onlocalcomp_aftersegJan172022.sh
+#fi
+#if [[ ${TYPE_OF_PROGRAM} == 6 ]] ;
+#then
+#
+#   /software/combine_csvs_and_copy_pdfs_projectlevel_Jan17_2023_LocalComputer.sh
+#fi
+#
+#if [[ ${TYPE_OF_PROGRAM} == 7 ]] ;
+#then
+#echo ${PROJECT_ID}::$XNAT_USER::$XNAT_PASS::$XNAT_HOST
+##   /software/analyzed_session_list.sh  ${PROJECT_ID} $XNAT_USER $XNAT_PASS $XNAT_HOST
+#fi

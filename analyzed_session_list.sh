8347b5c0eb797b69259d349495c19fabca1c0159
dharlabwustl <atulkumar@wustl.edu>
2023-02-01 15:12:22 -0600

    3rd commit

--- analyzed_session_list.sh
+++ analyzed_session_list.sh
@@ -406,7 +406,7 @@ selectedniftifilelist_file=${output_directory}/${combined_csv_outputfilename}
 allsessionlist_file=${output_directory}/all_sessions.csv
 output_list_csvfile="${output_directory}/${projectID}_${snipr_output_foldername}_ANALYZED_LIST_LABELED${extension_csv}"
 pdffilelist_file_ext="_thresh" #${5} #sys.argv[5]
-stringtofilterallsessionlist="" #${6} #sys.argv[6]
+stringtofilterallsessionlist="COLI" #${6} #sys.argv[6]
 create_list_analyzed_session  ${pdffilelist_file} ${selectedniftifilelist_file} ${allsessionlist_file}  ${output_list_csvfile}  ${pdffilelist_file_ext} ${stringtofilterallsessionlist}
 cp ${output_list_csvfile} ${final_output_directory}
 

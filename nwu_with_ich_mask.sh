a1b536d940380acfc3df18e34774fabd4822644c
dharlabwustl <atulkumar@wustl.edu>
2023-02-23 15:42:05 -0600

    Commit Feb 23 2023

--- nwu_with_ich_mask.sh
+++ nwu_with_ich_mask.sh
@@ -77,53 +77,53 @@ echo "this_filename=${1}
       this_infarctmask1filename=${5}"
 echo "BET USING LEVELSET MASK"
  
-#/software/bet_withlevelset.sh $this_filename ${this_betfilename} #${output_directory} #Helsinki2000_1019_10132014_1048_Head_2.0_ax_Tilt_1_levelset # ${3} # Helsinki2000_702_12172013_2318_Head_2.0_ax_levelset.nii.gz #${3} # $6 $7 $8 $9 ${10}
-#
-#echo "bet_withlevelset successful" > ${output_directory}/success.txt
-#this_filename_brain=${this_filename%.nii*}_brain_f.nii.gz
-## cp ${this_filename_brain} ${output_directory}/ #  ${final_output_directory}/
-#echo "LINEAR REGISTRATION TO TEMPLATE"
-#/software/linear_rigid_registration.sh ${this_filename_brain} #${templatefilename} #$3 ${6} WUSTL_233_11122015_0840__levelset_brain_f.nii.gz
-#echo "linear_rigid_registration successful" >> ${output_directory}/success.txt
-#echo "RUNNING IML FSL PART"
-#/software/ideal_midline_fslpart.sh ${this_filename} # ${templatefilename} ${mask_on_template}  #$9 #${10} #$8
-#echo "ideal_midline_fslpart successful" >> ${output_directory}/success.txt
-#echo "RUNNING IML PYTHON PART"
-#
-#/software/ideal_midline_pythonpart.sh  ${this_filename} #${templatefilename}  #$3 #$8 $9 ${10}
-#echo "ideal_midline_pythonpart successful" >> ${output_directory}/success.txt
-#
-#echo "RUNNING NWU AND CSF VOLUME CALCULATION "
+/software/bet_withlevelset.sh $this_filename ${this_betfilename} #${output_directory} #Helsinki2000_1019_10132014_1048_Head_2.0_ax_Tilt_1_levelset # ${3} # Helsinki2000_702_12172013_2318_Head_2.0_ax_levelset.nii.gz #${3} # $6 $7 $8 $9 ${10}
+
+echo "bet_withlevelset successful" > ${output_directory}/success.txt
+this_filename_brain=${this_filename%.nii*}_brain_f.nii.gz
+# cp ${this_filename_brain} ${output_directory}/ #  ${final_output_directory}/
+echo "LINEAR REGISTRATION TO TEMPLATE"
+/software/linear_rigid_registration.sh ${this_filename_brain} #${templatefilename} #$3 ${6} WUSTL_233_11122015_0840__levelset_brain_f.nii.gz
+echo "linear_rigid_registration successful" >> ${output_directory}/success.txt
+echo "RUNNING IML FSL PART"
+/software/ideal_midline_fslpart.sh ${this_filename} # ${templatefilename} ${mask_on_template}  #$9 #${10} #$8
+echo "ideal_midline_fslpart successful" >> ${output_directory}/success.txt
+echo "RUNNING IML PYTHON PART"
+
+/software/ideal_midline_pythonpart.sh  ${this_filename} #${templatefilename}  #$3 #$8 $9 ${10}
+echo "ideal_midline_pythonpart successful" >> ${output_directory}/success.txt
+
+echo "RUNNING NWU AND CSF VOLUME CALCULATION "
 
 /software/ich_csf_volume.sh  ${this_filename}   ${this_betfilename} ${this_csfmaskfilename} ${this_infarctmaskfilename}  ${this_infarctmask1filename}  #${upper_threshold}
 echo "ich_csf_volume successful" >> ${output_directory}/success.txt
-#thisfile_basename=$(basename $this_filename)
-## for texfile in $(/usr/lib/fsl/5.0/remove_ext ${output_directory}/$thisfile_basename)*.tex ;
-#for texfile in  ${output_directory}/*.tex ;
-#do
-#pdflatex -halt-on-error -interaction=nonstopmode   -output-directory=${output_directory} $texfile  ##${output_directory}/$(/usr/lib/fsl/5.0/remove_ext $this_filename)*.tex
-#rm ${output_directory}/*.aux
-#rm ${output_directory}/*.log
-#done
-#
-#for filetocopy in $(/usr/lib/fsl/5.0/remove_ext ${output_directory}/$thisfile_basename)*_brain_f.nii.gz ;
-#do
-#cp ${filetocopy} ${final_output_directory}/
-#done
-#
-#for filetocopy in $(/usr/lib/fsl/5.0/remove_ext ${output_directory}/$thisfile_basename)*.mat ;
-#do
-#cp ${filetocopy} ${final_output_directory}/
-#done
-#
-#for filetocopy in ${output_directory}/*.pdf ;
-#do
-#cp ${filetocopy} ${final_output_directory}/
-#done
-#for filetocopy in  ${output_directory}/*.csv ;
-#do
-#cp ${filetocopy} ${final_output_directory}/
-#done
+thisfile_basename=$(basename $this_filename)
+# for texfile in $(/usr/lib/fsl/5.0/remove_ext ${output_directory}/$thisfile_basename)*.tex ;
+for texfile in  ${output_directory}/*.tex ;
+do
+pdflatex -halt-on-error -interaction=nonstopmode   -output-directory=${output_directory} $texfile  ##${output_directory}/$(/usr/lib/fsl/5.0/remove_ext $this_filename)*.tex
+rm ${output_directory}/*.aux
+rm ${output_directory}/*.log
+done
+
+for filetocopy in $(/usr/lib/fsl/5.0/remove_ext ${output_directory}/$thisfile_basename)*_brain_f.nii.gz ;
+do
+cp ${filetocopy} ${final_output_directory}/
+done
+
+for filetocopy in $(/usr/lib/fsl/5.0/remove_ext ${output_directory}/$thisfile_basename)*.mat ;
+do
+cp ${filetocopy} ${final_output_directory}/
+done
+
+for filetocopy in ${output_directory}/*.pdf ;
+do
+cp ${filetocopy} ${final_output_directory}/
+done
+for filetocopy in  ${output_directory}/*.csv ;
+do
+cp ${filetocopy} ${final_output_directory}/
+done
 
 }
 

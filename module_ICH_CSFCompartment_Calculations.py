c15aa2890dc48a435177083dd91193014ac26fb9
dharlabwustl <atulkumar@wustl.edu>
2023-02-22 18:16:18 -0600

    3rd commit

--- module_ICH_CSFCompartment_Calculations.py
+++ module_ICH_CSFCompartment_Calculations.py
@@ -616,7 +616,7 @@ def measure_compartments_with_reg_round5_one_file_sh_v1() : #niftifilenamedir,np
                 lower_thresh=int(float(sys.argv[7]))
                 upper_thresh=int(float(sys.argv[8]))
                 ## check if infarct file exists: sys.argv[4]
-                lower_thresh,upper_thresh,lower_thresh_normal,upper_thresh_normal, infarct_total_voxels_volume,infarct_side,NWU,infarct_pixels_number,infarct_pixels_density,nonfarct_pixels_number,noninfarct_pixels_density, overall_infarct_vol,overall_non_infarct_vol= measure_ICH_Feb22_2023()
+                # lower_thresh,upper_thresh,lower_thresh_normal,upper_thresh_normal, infarct_total_voxels_volume,infarct_side,NWU,infarct_pixels_number,infarct_pixels_density,nonfarct_pixels_number,noninfarct_pixels_density, overall_infarct_vol,overall_non_infarct_vol= measure_ICH_Feb22_2023()
 
 
         # niftifilename_basename_split_nii=os.path.basename(niftifilename).split(".nii")[0] #.split("_")

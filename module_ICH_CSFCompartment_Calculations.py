787b434ac1a36101dc95f8dd1e6fee40f8ecbd54
dharlabwustl <atulkumar@wustl.edu>
2023-04-12 10:56:52 -0500

    Commit Feb 23 2023

--- module_ICH_CSFCompartment_Calculations.py
+++ module_ICH_CSFCompartment_Calculations.py
@@ -233,13 +233,13 @@ def measure_ICH_CLASS2_Feb_24_2023(): #niftifilename,npyfiledirectory,niftifilen
         min_val=np.min(csf_seg_np)
         filename_gray_data_np=contrast_stretch_np(filename_gray_data_np,1) #exposure.rescale_intensity( filename_gray_data_np , in_range=(1000, 1200))
         filename_gray_data_np_1=contrast_stretch_np(resizeinto_512by512(nib.load(grayfilename).get_fdata()),1)*255 #np.uint8(filename_gray_data_np*255) contrast_stretch_np(nib.load(grayfilename).get_fdata(),1)*255  #c
-        filename_gray_data_np[csf_seg_np>min_val]=np.min(filename_gray_data_np)#255
+        # filename_gray_data_np[csf_seg_np>min_val]=np.min(filename_gray_data_np)#255
         numpy_image=normalizeimage0to1(filename_gray_data_np)*255 #filename_gray_data_np #
 
         print('filename_gray_data_np_copy size = {}'.format(filename_gray_data_np_copy.shape))
         print('csf_seg_np size = {}'.format(csf_seg_np.shape))
 
-        filename_gray_data_np_copy[csf_seg_np>min_val]=np.min(filename_gray_data_np_copy) #+10.0 ##255
+        # filename_gray_data_np_copy[csf_seg_np>min_val]=np.min(filename_gray_data_np_copy) #+10.0 ##255
 
         ICH_side,ICH_Mask_filename_data_np=determine_ICH_side(numpy_image,filename_gray_data_np_copy,niftifilename,npyfiledirectory,csf_seg_np,ICH_Mask_filename_data_np)
         # filename_gray_data_np_copy=normalizeimage0to1(filename_gray_data_np_copy)*255
@@ -663,13 +663,13 @@ def measure_ICH_Class1_Feb24_2023(): #niftifilename,npyfiledirectory,niftifilena
         min_val=np.min(csf_seg_np)
         filename_gray_data_np=contrast_stretch_np(filename_gray_data_np,1) #exposure.rescale_intensity( filename_gray_data_np , in_range=(1000, 1200))
         filename_gray_data_np_1=contrast_stretch_np(resizeinto_512by512(nib.load(grayfilename).get_fdata()),1)*255 #np.uint8(filename_gray_data_np*255) contrast_stretch_np(nib.load(grayfilename).get_fdata(),1)*255  #c
-        filename_gray_data_np[csf_seg_np>min_val]=np.min(filename_gray_data_np)#255
+        # filename_gray_data_np[csf_seg_np>min_val]=np.min(filename_gray_data_np)#255
         numpy_image=normalizeimage0to1(filename_gray_data_np)*255 #filename_gray_data_np #
 
         print('filename_gray_data_np_copy size = {}'.format(filename_gray_data_np_copy.shape))
         print('csf_seg_np size = {}'.format(csf_seg_np.shape))
 
-        filename_gray_data_np_copy[csf_seg_np>min_val]=np.min(filename_gray_data_np_copy) #+10.0 #255
+        # filename_gray_data_np_copy[csf_seg_np>min_val]=np.min(filename_gray_data_np_copy) #+10.0 #255
         ICH_side,ICH_Mask_filename_data_np=determine_ICH_side(numpy_image,filename_gray_data_np_copy,niftifilename,npyfiledirectory,csf_seg_np,ICH_Mask_filename_data_np)
         numpy_image_mask=ICH_Mask_filename_data_np
         lower_thresh=np.min(filename_gray_data_np_copy) #int(float(sys.argv[7])) #20 #0 # 20 #

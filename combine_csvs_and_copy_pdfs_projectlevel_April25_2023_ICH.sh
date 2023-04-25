c7ff7dbb3c0a3ced2193358f3983f94095dcf2ec
dharlabwustl <atulkumar@wustl.edu>
2023-04-25 11:48:17 -0500

    Commit Feb 23 2023

--- combine_csvs_and_copy_pdfs_projectlevel_April25_2023_ICH.sh
+++ combine_csvs_and_copy_pdfs_projectlevel_April25_2023_ICH.sh
@@ -356,96 +356,96 @@ call_list_analyzed_session()"  ${pdffilelist_file} ${selectedniftifilelist_file}
 }
 
 projectID=${1}
-
-############################## get_all_selected_scan_in_a_project   #############################################
-## 1. download all the sessions list. 2. For each session download its metadata. 3. if it has type == z-axial get its metadata 4. If the scan metadata URI has 'EDEMA_BIOMARKERS' in it
-#then  download the csv and pdf from this metadata
-get_all_selected_scan_in_a_project ${projectID} ${working_dir}
-############################## get_all_EDEMA_BIOMARKER_csvfiles_of_allselectedscan   #############################################
-get_all_EDEMA_BIOMARKER_csvfiles_of_allselectedscan ${working_dir}
-
-############################## combine_all_csvfiles_of_edema_biomarker   #############################################
-extension_csv=".csv" #"0_40TOTAL.csv"
-combined_csv_outputfilename=${projectID}_EDEMA_BIOMARKERS_COMBINED_${extension_csv}
-output_directory="/workingoutput"
-combine_all_csvfiles_of_edema_biomarker  ${working_dir} ${output_directory} ${extension_csv} ${combined_csv_outputfilename}
-
-combinedfilename=$(find ${output_directory} -name *COMBINED_columndropped.csv)
-while IFS=',' read -ra array; do
-copy_latest_pdfs ${array[0]} ${working_dir} ${final_output_directory}
-done < <( tail -n +2 "${combinedfilename}" )
-######################################################################################################################
-## COPY IT TO THE SNIPR RESPECTIVE PROJECT RESOURCES
-
-snipr_output_foldername="ICH_QUANTIFICATION"
-file_suffixes=( COMBINED_columndropped.csv ) #sys.argv[5] .mat
-for file_suffix in ${file_suffixes[@]}
-do
-    copyoutput_to_snipr_projectlevel  ${projectID} ${output_directory} "${snipr_output_foldername}"   ${file_suffix}
-done
-
-######################################################################################################################
-
-######################################################################################################################
-## COPY PDFs TO THE SNIPR RESPECTIVE PROJECT RESOURCES
-
-snipr_output_foldername="ICH_QUANTIFICATION"
-file_suffixes=(  .pdf COMBINED_columndropped.csv ) #sys.argv[5]
-for file_suffix in ${file_suffixes[@]}
-do
-#    copyoutput_to_snipr_projectlevel  ${projectID} ${working_dir} "${snipr_output_foldername}"   ${file_suffix}
-     copyoutput_to_snipr_projectlevel  ${projectID} ${final_output_directory} "${snipr_output_foldername}"   ${file_suffix}
-done
-
-######################################################################################################################
-## for one project:
-
-## find all the scans with the selections criteria to rung segmentation
-
-## download csv files from the EDEMA_BIOMARKER directory:
-
-
-## combine all the csv files
-
-## upload the combined csv files to the project directory level
-
-
-##
-
-#########################################################################
-## GET THE SINGLE CT NIFTI FILE NAME AND COPY IT TO THE WORKING_DIR
-# niftifile_csvfilename=${working_dir}/'this_session_final_ct.csv'
-# get_nifti_scan_uri ${sessionID}  ${working_dir} ${niftifile_csvfilename}
-# copy_scan_data ${niftifile_csvfilename} ${working_dir}
-
-
-
-
-# ###############################################################################################################
-
-# ## GET THE RESPECTIVS MASKS NIFTI FILE NAME AND COPY IT TO THE WORKING_DIR
-
-# #####################################################################################
-# resource_dirname='MASKS'
-# output_dirname=${working_dir}
-# while IFS=',' read -ra array; do
-# scanID=${array[2]}
-# echo sessionId::${sessionID}
-# echo scanId::${scanID}
-# done < <( tail -n +2 "${niftifile_csvfilename}" )
-# echo working_dir::${working_dir}
-# echo output_dirname::${output_dirname}
-# copy_masks_data   ${sessionID}  ${scanID} ${resource_dirname} ${output_dirname}
-# ######################################################################################################################
-# ## CALCULATE EDEMA BIOMARKERS
-# nwucalculation_each_scan
-# ######################################################################################################################
-# ## COPY IT TO THE SNIPR RESPECTIVE SCAN RESOURCES
-# snipr_output_foldername="EDEMA_BIOMARKER"
-# file_suffixes=(  .pdf .mat .csv ) #sys.argv[5]
-# for file_suffix in ${file_suffixes[@]}
-# do
-#     copyoutput_to_snipr  ${sessionID} ${scanID} "${final_output_directory}"  ${snipr_output_foldername}  ${file_suffix}
-# done
-# ######################################################################################################################
-
+echo "$projectID"
+############################### get_all_selected_scan_in_a_project   #############################################
+### 1. download all the sessions list. 2. For each session download its metadata. 3. if it has type == z-axial get its metadata 4. If the scan metadata URI has 'EDEMA_BIOMARKERS' in it
+##then  download the csv and pdf from this metadata
+#get_all_selected_scan_in_a_project ${projectID} ${working_dir}
+############################### get_all_EDEMA_BIOMARKER_csvfiles_of_allselectedscan   #############################################
+#get_all_EDEMA_BIOMARKER_csvfiles_of_allselectedscan ${working_dir}
+#
+############################### combine_all_csvfiles_of_edema_biomarker   #############################################
+#extension_csv=".csv" #"0_40TOTAL.csv"
+#combined_csv_outputfilename=${projectID}_EDEMA_BIOMARKERS_COMBINED_${extension_csv}
+#output_directory="/workingoutput"
+#combine_all_csvfiles_of_edema_biomarker  ${working_dir} ${output_directory} ${extension_csv} ${combined_csv_outputfilename}
+#
+#combinedfilename=$(find ${output_directory} -name *COMBINED_columndropped.csv)
+#while IFS=',' read -ra array; do
+#copy_latest_pdfs ${array[0]} ${working_dir} ${final_output_directory}
+#done < <( tail -n +2 "${combinedfilename}" )
+#######################################################################################################################
+### COPY IT TO THE SNIPR RESPECTIVE PROJECT RESOURCES
+#
+#snipr_output_foldername="ICH_QUANTIFICATION"
+#file_suffixes=( COMBINED_columndropped.csv ) #sys.argv[5] .mat
+#for file_suffix in ${file_suffixes[@]}
+#do
+#    copyoutput_to_snipr_projectlevel  ${projectID} ${output_directory} "${snipr_output_foldername}"   ${file_suffix}
+#done
+#
+#######################################################################################################################
+#
+#######################################################################################################################
+### COPY PDFs TO THE SNIPR RESPECTIVE PROJECT RESOURCES
+#
+#snipr_output_foldername="ICH_QUANTIFICATION"
+#file_suffixes=(  .pdf COMBINED_columndropped.csv ) #sys.argv[5]
+#for file_suffix in ${file_suffixes[@]}
+#do
+##    copyoutput_to_snipr_projectlevel  ${projectID} ${working_dir} "${snipr_output_foldername}"   ${file_suffix}
+#     copyoutput_to_snipr_projectlevel  ${projectID} ${final_output_directory} "${snipr_output_foldername}"   ${file_suffix}
+#done
+#
+#######################################################################################################################
+### for one project:
+#
+### find all the scans with the selections criteria to rung segmentation
+#
+### download csv files from the EDEMA_BIOMARKER directory:
+#
+#
+### combine all the csv files
+#
+### upload the combined csv files to the project directory level
+#
+#
+###
+#
+##########################################################################
+### GET THE SINGLE CT NIFTI FILE NAME AND COPY IT TO THE WORKING_DIR
+## niftifile_csvfilename=${working_dir}/'this_session_final_ct.csv'
+## get_nifti_scan_uri ${sessionID}  ${working_dir} ${niftifile_csvfilename}
+## copy_scan_data ${niftifile_csvfilename} ${working_dir}
+#
+#
+#
+#
+## ###############################################################################################################
+#
+## ## GET THE RESPECTIVS MASKS NIFTI FILE NAME AND COPY IT TO THE WORKING_DIR
+#
+## #####################################################################################
+## resource_dirname='MASKS'
+## output_dirname=${working_dir}
+## while IFS=',' read -ra array; do
+## scanID=${array[2]}
+## echo sessionId::${sessionID}
+## echo scanId::${scanID}
+## done < <( tail -n +2 "${niftifile_csvfilename}" )
+## echo working_dir::${working_dir}
+## echo output_dirname::${output_dirname}
+## copy_masks_data   ${sessionID}  ${scanID} ${resource_dirname} ${output_dirname}
+## ######################################################################################################################
+## ## CALCULATE EDEMA BIOMARKERS
+## nwucalculation_each_scan
+## ######################################################################################################################
+## ## COPY IT TO THE SNIPR RESPECTIVE SCAN RESOURCES
+## snipr_output_foldername="EDEMA_BIOMARKER"
+## file_suffixes=(  .pdf .mat .csv ) #sys.argv[5]
+## for file_suffix in ${file_suffixes[@]}
+## do
+##     copyoutput_to_snipr  ${sessionID} ${scanID} "${final_output_directory}"  ${snipr_output_foldername}  ${file_suffix}
+## done
+## ######################################################################################################################
+#

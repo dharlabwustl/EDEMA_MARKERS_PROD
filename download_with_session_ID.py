8b2e0ec0496b093a4673531adcb31ac6ea96f437
dharlabwustl <atulkumar@wustl.edu>
2023-05-17 13:42:41 -0500

    Commit Feb 23 2023

--- download_with_session_ID.py
+++ download_with_session_ID.py
@@ -1039,7 +1039,7 @@ def download_a_singlefile_with_URLROW(url,dir_to_save):
     xnatSession.renew_httpsession()
     command="echo  " + url['URI'] + " >> " +  os.path.join(dir_to_save,"test.csv")
     subprocess.call(command,shell=True)
-    # response = xnatSession.httpsess.get(xnatSession.host +url["URI"]) #/data/projects/ICH/resources/179772/files/ICH_CTSESSIONS_202305170753.csv") #
+    response = xnatSession.httpsess.get(xnatSession.host +url["URI"]) #/data/projects/ICH/resources/179772/files/ICH_CTSESSIONS_202305170753.csv") #
     #                                                       # "/data/experiments/SNIPR02_E03548/scans/1-CT1/resources/147851/files/ICH_0001_01022017_0414_1-CT1_threshold-1024.0_22121.0TOTAL_VersionDate-11302022_04_22_2023.csv") ## url['URI'])
     # zipfilename=os.path.join(dir_to_save,os.path.basename("/data/projects/ICH/resources/179772/files/ICH_CTSESSIONS_202305170753.csv")) #sessionId+scanId+'.zip'
     # with open(zipfilename, "wb") as f:

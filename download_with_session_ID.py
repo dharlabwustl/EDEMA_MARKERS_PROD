91821dbd4d4024b9aea26701464fc204de7df7e1
dharlabwustl <atulkumar@wustl.edu>
2023-05-17 13:12:40 -0500

    Commit Feb 23 2023

--- download_with_session_ID.py
+++ download_with_session_ID.py
@@ -1031,7 +1031,7 @@ def get_latest_file(df_listfile):
     allfileswithprefix1_df['DATETIME'] = pd.to_datetime(allfileswithprefix1_df['DATETIME'], format='%Y%m%d%H%M', errors='coerce')
     allfileswithprefix1_df = allfileswithprefix1_df.sort_values(by=['DATETIME'], ascending=False)
     print(allfileswithprefix1_df["DATETIME"])
-    # allfileswithprefix1_df=allfileswithprefix1_df.reset_index(drop=True)
+    allfileswithprefix1_df=allfileswithprefix1_df.reset_index(drop=True)
     # x_df=allfileswithprefix1_df[0]
     return allfileswithprefix1_df
 def download_a_singlefile_with_URLROW(url,dir_to_save):

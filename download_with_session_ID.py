e8efc5065ab6bee11c0010a6bf9be8c98dfef7ee
dharlabwustl <atulkumar@wustl.edu>
2023-05-19 12:12:52 -0500

    Commit Feb 23 2023

--- download_with_session_ID.py
+++ download_with_session_ID.py
@@ -1098,7 +1098,7 @@ def download_files_in_a_resource(URI,dir_to_save):
         # download_a_singlefile_with_URLROW(df_listfile,dir_to_save)
         for x in range(df_listfile.shape[0]):
             # print("row::{}".format(row))
-            download_a_singlefile_with_URLROW(df_listfile.iloc[x],dir_to_save)
+            download_a_singlefile_with_URLROW(df_listfile.iloc[[x]],dir_to_save)
             # print("DOWNLOADED ::{}".format(row['Name']))
     except:
         print("FAILED AT ::{}".format("download_files_in_a_resource"))

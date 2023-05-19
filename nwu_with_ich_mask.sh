412d4ec0ec8e8615da43feae2a52aa4209ddab83
dharlabwustl <atulkumar@wustl.edu>
2023-05-19 12:19:06 -0500

    Commit Feb 23 2023

--- nwu_with_ich_mask.sh
+++ nwu_with_ich_mask.sh
@@ -294,9 +294,16 @@ get_maskfile_scan_metadata()" ${sessionId}  ${scanId}  ${resource_foldername} ${
 
 #########################################################################
 ## GET THE SINGLE CT NIFTI FILE NAME AND COPY IT TO THE WORKING_DIR
-niftifile_csvfilename=${working_dir}/'this_session_final_ct.csv'
-get_nifti_scan_uri ${sessionID}  ${working_dir} ${niftifile_csvfilename}
+#niftifile_csvfilename=${working_dir}/'this_session_final_ct.csv'
+#get_nifti_scan_uri ${sessionID}  ${working_dir} ${niftifile_csvfilename}
 ########################################
+call_download_files_in_a_resource_in_a_session_arguments=('call_download_files_in_a_resource_in_a_session' ${sessionID} "NIFTI_LOCATION" ${working_dir})
+outputfiles_present=$(python3 download_with_session_ID.py "${call_download_files_in_a_resource_in_a_session_arguments[@]}")
+echo '$outputfiles_present'::$outputfiles_present
+########################################
+for niftifile_csvfilename in ${working_dir}/*NIFTILOCATION.csv; do
+  outputfiles_present=0
+echo $niftifile_csvfilename
 outputfiles_present=0
 while IFS=',' read -ra array; do
 scanID=${array[2]}
@@ -354,3 +361,4 @@ else
   echo " FILES ARE PRESENT "
 ######################################################################################################################
 fi
+done

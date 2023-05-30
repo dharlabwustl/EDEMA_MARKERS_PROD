9f4946d56a517287945590573e0c36de69327e88
dharlabwustl <atulkumar@wustl.edu>
2023-05-30 14:53:34 -0500

    Commit Feb 23 2023

--- download_with_session_ID.py
+++ download_with_session_ID.py
@@ -381,15 +381,15 @@ def decision_which_nifti_multiplescans(sessionId,dir_to_receive_the_data="",outp
                 number_slice=nifti_number_slice(os.path.join(dir_to_receive_the_data,x[1]))
                 # final_ct_file=[[each_nifti['URI'],each_nifti['Name'],each_axial['ID'],number_slice]]
                 list_of_usables_withsize=[]
-                # list_of_usables_withsize.append([each_nifti['URI'],each_nifti['Name'],each_axial['ID'],number_slice])
-                # jsonStr = json.dumps(list_of_usables_withsize)
-                # # print(jsonStr)
-                # df = pd.read_json(jsonStr)
-                # df.columns=['URI','Name','ID','NUMBEROFSLICES']
-                # pd.DataFrame(df).T.to_csv(niftifile_location,index=False)
-                # resource_dirname="NIFTI_LOCATION"
-                # url = (("/data/experiments/%s") % (sessionId))
-                # uploadsinglefile_with_URI(url,niftifile_location,resource_dirname)
+                list_of_usables_withsize.append([each_nifti['URI'],each_nifti['Name'],each_axial['ID'],number_slice])
+                jsonStr = json.dumps(list_of_usables_withsize)
+                # print(jsonStr)
+                df = pd.read_json(jsonStr)
+                df.columns=['URI','Name','ID','NUMBEROFSLICES']
+                pd.DataFrame(df).T.to_csv(niftifile_location,index=False)
+                resource_dirname="NIFTI_LOCATION"
+                url = (("/data/experiments/%s") % (sessionId))
+                uploadsinglefile_with_URI(url,niftifile_location,resource_dirname)
 
     return
 def decision_which_nifti(sessionId,dir_to_receive_the_data="",output_csvfile=""):

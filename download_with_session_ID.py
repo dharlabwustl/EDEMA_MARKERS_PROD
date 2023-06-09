10ffbf376576f7d1c6f53c0f11ad43618946c5d1
dharlabwustl <atulkumar@wustl.edu>
2023-06-09 14:51:40 -0500

    Commit Feb 23 2023

--- download_with_session_ID.py
+++ download_with_session_ID.py
@@ -1261,10 +1261,10 @@ def call_concatenate_csv_list(args):
     df.to_csv(outputfilename,index=False)
 
 def download_all_csv_files_givena_URIdf(URI_DF,projectname,dir_to_save):
-    URI_DF_WITH_CSVFILES=URI_DF[URI_DF['ICH_CSVFILE_AVAILABLE']==1]
+    URI_DF_WITH_CSVFILES=URI_DF[URI_DF[projectname+'_CSVFILE_AVAILABLE']==1]
     print("URI_DF_WITH_CSVFILESshape::{}".format(URI_DF_WITH_CSVFILES.shape))
-    for item_id1, each_selected_scan in URI_DF_WITH_CSVFILES.iterrows():
-        download_a_singlefile_with_URIString(each_selected_scan[projectname+'_CSVFILENAME'],os.path.basename(each_selected_scan[projectname+'_CSVFILENAME']),dir_to_save)
+    # for item_id1, each_selected_scan in URI_DF_WITH_CSVFILES.iterrows():
+    #     download_a_singlefile_with_URIString(each_selected_scan[projectname+'_CSVFILENAME'],os.path.basename(each_selected_scan[projectname+'_CSVFILENAME']),dir_to_save)
 
         # pass
     return

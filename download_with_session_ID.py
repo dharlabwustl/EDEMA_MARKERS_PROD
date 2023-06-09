39d9a4aada19ec47c862ca35a184b71aaf4b75f3
dharlabwustl <atulkumar@wustl.edu>
2023-06-09 13:03:47 -0500

    Commit Feb 23 2023

--- download_with_session_ID.py
+++ download_with_session_ID.py
@@ -1261,14 +1261,15 @@ def call_concatenate_csv_list(args):
 
 def call_project_resource_latest_analytic_file(args):
     try:
-        projectID=args.stuff[1]
-        # scanID=args.stuff[2]
-        resource_dir=args.stuff[2]
-        URI="/data/projects/"+projectID #+"/scans/"+scanID
-        URI = (URI+'/resources/' + resource_dir +'/files?format=json')
-        extension_to_find_list=args.stuff[3]
-        dir_to_save=args.stuff[4]
-        print("projectID::{}::URI::{}::extension_to_find_list::{}::dir_to_save::{}")
+        print("WO ZAI call_project_resource_latest_analytic_file try")
+        # projectID=args.stuff[1]
+        # # scanID=args.stuff[2]
+        # resource_dir=args.stuff[2]
+        # URI="/data/projects/"+projectID #+"/scans/"+scanID
+        # URI = (URI+'/resources/' + resource_dir +'/files?format=json')
+        # extension_to_find_list=args.stuff[3]
+        # dir_to_save=args.stuff[4]
+        # print("projectID::{}::URI::{}::extension_to_find_list::{}::dir_to_save::{}")
         # df_listfile=listoffile_witha_URI_as_df(URI)
         # df_listfile=df_listfile[df_listfile.URI.str.contains(extension_to_find_list)]
         # latest_filename=get_latest_file(df_listfile)

b30c35067ed9d9be50138fedc479661bd45c175a
dharlabwustl <atulkumar@wustl.edu>
2023-06-09 11:01:27 -0500

    Commit Feb 23 2023

--- fillmaster_session_list.py
+++ fillmaster_session_list.py
@@ -332,7 +332,7 @@ def get_scan_description(sessionId,scanId):
     this_session_metadata=get_metadata_session(sessionId)
     jsonStr = json.dumps(this_session_metadata)
     df = pd.read_json(jsonStr)
-    this_session_metadata_df_scanid=df.loc[df['ID'] == scanId]
+    this_session_metadata_df_scanid=df.loc[df['ID'] > scanId]
     print("df={}::scanId::{}::this_session_metadata_df_scanid:{}".format(df['ID'],scanId,this_session_metadata_df_scanid))
 
 

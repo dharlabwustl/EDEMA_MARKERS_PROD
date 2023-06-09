f94a4eb4fdf2f80f1ddda98b4f047f7b8aa81f8d
dharlabwustl <atulkumar@wustl.edu>
2023-06-09 12:03:27 -0500

    Commit Feb 23 2023

--- combine_csvs_and_copy_pdfs_projectlevel_May262023_ICH.sh
+++ combine_csvs_and_copy_pdfs_projectlevel_May262023_ICH.sh
@@ -390,7 +390,7 @@ directorytosave=$final_output_directory
 ## COPY IT TO THE SNIPR RESPECTIVE SCAN RESOURCES
 snipr_output_foldername="SNIPR_ANALYTICS"
 call_project_resource_latest_analytic_file_arguments=('call_project_resource_latest_analytic_file' ${project_ID}   ${snipr_output_foldername} .csv $directorytosave )
-#outputfiles_present=$(python3 download_with_session_ID.py "${call_project_resource_latest_analytic_file_arguments[@]}" )
+outputfiles_present=$(python3 download_with_session_ID.py "${call_project_resource_latest_analytic_file_arguments[@]}" )
 echo "WO ZAI ZE-LIIIIIIIIIIIIIII"
 
 #

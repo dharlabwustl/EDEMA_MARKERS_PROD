4f06d53fdcb07679268c4e582712b61f4f7ab452
dharlabwustl <atulkumar@wustl.edu>
2023-06-14 10:34:56 -0500

    Commit Feb 23 2023

--- create_pipeline_performancetable_subsequent_run.sh
+++ create_pipeline_performancetable_subsequent_run.sh
@@ -448,8 +448,8 @@ directorytosave=$final_output_directory
 fillmaster_session_list ${session_csvfile} ${dir_csv}  ${filenametosave} ${directorytosave} ${filename_latex_tosave}
 
 ###filenametosave=${directorytosave}/${project_ID}_CTSESSIONS_${time_now}.csv #4
-##call_concatenate_csv_list_arguments=('call_concatenate_csv_list' ${filenametosave} ${directorytosave}/temp.csv $directorytosave/sessions_done.csv )
-##outputfiles_present=$(python3 download_with_session_ID.py "${call_concatenate_csv_list_arguments[@]}" )
+call_concatenate_csv_list_arguments=('call_concatenate_csv_list' ${filenametosave} ${directorytosave}/temp.csv ${listofsession_previous} )
+outputfiles_present=$(python3 download_with_session_ID.py "${call_concatenate_csv_list_arguments[@]}" )
 ####filename_latex_tosave=${directorytosave}/${project_ID}_CTSESSIONS_${time_now}.tex
 ###call_pdffromanalytics_arguments=('call_pdffromanalytics' ${filenametosave} ${filename_latex_tosave}  )
 ###outputfiles_present=$(python3 fillmaster_session_list.py "${call_pdffromanalytics_arguments[@]}" )

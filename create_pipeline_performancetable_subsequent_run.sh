60aa55296cfae0e6a1adc18e757adab5176462c4
dharlabwustl <atulkumar@wustl.edu>
2023-06-14 10:30:54 -0500

    Commit Feb 23 2023

--- create_pipeline_performancetable_subsequent_run.sh
+++ create_pipeline_performancetable_subsequent_run.sh
@@ -437,16 +437,16 @@ fi
 
 #copy_latest_pdfs "ICH" ${working_dir} ${final_output_directory}
 done < <( tail -n +2 "${listofsession_current}" )
-##session_csvfile='sessions.csv' #$1
-##dir_csv=$final_output_directory
-### typeofmask="ICH" #$3 #"MASKS" #sys.argv[4]
-##time_now=$(date -dnow +%Y%m%d%H%M)
-##filenametosave=temp.csv # ${project_ID}_CTSESSIONS_${time_now}.csv #4
-##filename_latex_tosave=temp.tex # ${project_ID}_CTSESSIONS_${time_now}.tex #
-##filename_pdf_tosave=temp.pdf ## ${project_ID}_CTSESSIONS_${time_now}.pdf ##
-##directorytosave=$final_output_directory
-##fillmaster_session_list ${session_csvfile} ${dir_csv}  ${filenametosave} ${directorytosave} ${filename_latex_tosave}
-##
+session_csvfile=${listofsession_current} #'sessions.csv' #$1
+dir_csv=$final_output_directory
+# typeofmask="ICH" #$3 #"MASKS" #sys.argv[4]
+time_now=$(date -dnow +%Y%m%d%H%M)
+filenametosave=temp.csv # ${project_ID}_CTSESSIONS_${time_now}.csv #4
+filename_latex_tosave=temp.tex # ${project_ID}_CTSESSIONS_${time_now}.tex #
+filename_pdf_tosave=temp.pdf ## ${project_ID}_CTSESSIONS_${time_now}.pdf ##
+directorytosave=$final_output_directory
+fillmaster_session_list ${session_csvfile} ${dir_csv}  ${filenametosave} ${directorytosave} ${filename_latex_tosave}
+
 ###filenametosave=${directorytosave}/${project_ID}_CTSESSIONS_${time_now}.csv #4
 ##call_concatenate_csv_list_arguments=('call_concatenate_csv_list' ${filenametosave} ${directorytosave}/temp.csv $directorytosave/sessions_done.csv )
 ##outputfiles_present=$(python3 download_with_session_ID.py "${call_concatenate_csv_list_arguments[@]}" )

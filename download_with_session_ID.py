86af615350778accab86ef1efbfd07a265cc47a9
dharlabwustl <atulkumar@wustl.edu>
2023-06-14 13:59:12 -0500

    Commit Feb 23 2023

--- download_with_session_ID.py
+++ download_with_session_ID.py
@@ -1325,7 +1325,8 @@ def download_files_with_mastersessionlist(sessionlist_filename,masktype,filetype
             this_filename=os.path.join(dir_to_save,os.path.basename(each_selected_scan[masktype+'_'+filetype+'FILENAME']))
             download_a_singlefile_with_URIString(each_selected_scan[masktype+'_'+filetype+'FILENAME'],os.path.basename(each_selected_scan[masktype+'_'+filetype+'FILENAME']),dir_to_save)
             this_filename_df=pd.read_csv(this_filename)
-            this_filename_df['SESSION_ID_'+filetype]=each_selected_scan[masktype+'_'+filetype+'FILENAME'].split('/')[2]
+            this_filename_df['SESSION_ID_'+filetype]=each_selected_scan[masktype+'_'+filetype+'FILENAME'].split('/')[3]
+            this_filename_df['FILEPATH'+filetype]=each_selected_scan[masktype+'_'+filetype+'FILENAME'] #.split('/')[3]
             this_filename_df.to_csv(this_filename,index=False)
             files_local_location.append(this_filename)
         print("I SUCCEEDED AT ::{}".format(inspect.stack()[0][3]))

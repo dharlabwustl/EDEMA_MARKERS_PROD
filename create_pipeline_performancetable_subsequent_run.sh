60492e57a76351d851b7459db69e1dcf845e968f
dharlabwustl <atulkumar@wustl.edu>
2023-06-14 10:21:32 -0500

    Commit Feb 23 2023

--- create_pipeline_performancetable_subsequent_run.sh
+++ create_pipeline_performancetable_subsequent_run.sh
@@ -374,7 +374,8 @@ outputfiles_present=$(python3 download_with_session_ID.py "${call_project_resour
 echo ${outputfiles_present}
 csvfileslist=${outputfiles_present##*CSVMASTERFILE::}
 echo ${csvfileslist}
-call_divide_sessionlist_done_vs_undone_arguments=('call_divide_sessionlist_done_vs_undone' ${csvfileslist} )
+masktype="INFARCT"
+call_divide_sessionlist_done_vs_undone_arguments=('call_divide_sessionlist_done_vs_undone' ${csvfileslist} ${masktype})
 outputfiles_present=$(python3 download_with_session_ID.py "${call_divide_sessionlist_done_vs_undone_arguments[@]}" )
 echo ${outputfiles_present}
 #session_csvfile=$(ls $directorytosave/*.csv)

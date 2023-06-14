fcfbdee64ad877346bbaa84fc6323e0711d3a82b
dharlabwustl <atulkumar@wustl.edu>
2023-06-14 12:37:09 -0500

    Commit Feb 23 2023

--- download_with_session_ID.py
+++ download_with_session_ID.py
@@ -1308,7 +1308,7 @@ def call_download_files_with_mastersessionlist(args):
 
         files_local_location=download_files_with_mastersessionlist(sessionlist_filename,masktype,filetype,dir_to_save)
         files_local_location_df=pd.DataFrame(files_local_location)
-        files_local_location_df.columns=['LOCALFILENAME']
+        files_local_location_df.columns=['LOCAL_FILENAME']
         files_local_location_df.to_csv(localfilelist_csv,index=False)
         # if upload_flag==1:
         #     projectId=args.stuff[6]

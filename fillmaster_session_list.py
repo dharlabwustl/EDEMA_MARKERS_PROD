bab650dc9c4fdf09806bdea9afa4a07e06d2a711
dharlabwustl <atulkumar@wustl.edu>
2023-06-15 22:13:36 -0500

    Commit Feb 23 2023

--- fillmaster_session_list.py
+++ fillmaster_session_list.py
@@ -385,7 +385,7 @@ def get_scan_type(sessionId,scanId1):
         print("scanId1type::{}".format(type(scanId1)))
 
         this_session_metadata_df_scanid=pd.DataFrame(df1.loc[df1['ID'] == int(scanId1)])
-        subprocess.call("echo " + "scanId1type::{}  >> /workingoutput/error.txt".format(this_session_metadata_df_scanid) ,shell=True )
+        subprocess.call("echo " + "scanId1type::{}  >> /workingoutput/error.txt".format(str(this_session_metadata_df_scanid)) ,shell=True )
         # this_session_metadata_df_scanid.reset_index(inplace=True)
         # print("df={}::scanId::{}::this_session_metadata_df_scanid:{}".format(df1['ID'],scanId1,this_session_metadata_df_scanid.loc[0,'type']))
         print("I SUCCEEDED AT ::{}".format(inspect.stack()[0][3]))

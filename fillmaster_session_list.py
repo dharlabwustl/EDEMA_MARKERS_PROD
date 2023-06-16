d2b122e86c2311bee547a878a6757ad1398e9c59
dharlabwustl <atulkumar@wustl.edu>
2023-06-15 20:59:13 -0500

    Commit Feb 23 2023

--- fillmaster_session_list.py
+++ fillmaster_session_list.py
@@ -199,7 +199,7 @@ def insertniftifilename(sessioncsv_df,dir_csv):
                 scan_type=get_scan_type(sessionId,scanId)
 
                 sessioncsv_df=writetomastersession(sessioncsv_df,filename,filename_available,row,xx)
-                sessioncsv_df=write_scantype_tomastersession(sessioncsv_df,scan_type,xx)
+                # sessioncsv_df=write_scantype_tomastersession(sessioncsv_df,scan_type,xx)
                 filename=''
                 filename_available=''
             print("I SUCCEEDED AT::{}".format(inspect.stack()[0][3]))

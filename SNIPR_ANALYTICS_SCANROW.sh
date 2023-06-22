5bf61e8ffa2a045c5e15c468ecfac803c62d558d
dharlabwustl <atulkumar@wustl.edu>
2023-06-22 15:37:05 -0500

    Commit Feb 23 2023

--- SNIPR_ANALYTICS_SCANROW.sh
+++ SNIPR_ANALYTICS_SCANROW.sh
@@ -69,6 +69,8 @@ while IFS=',' read -ra array; do
           call_get_resourcefiles_metadata_saveascsv_args ${URI} ${resource_dir} ${final_output_directory} ${output_csvfile}
           resource_dir="MASKS"
           call_get_resourcefiles_metadata_saveascsv_args ${URI} ${resource_dir} ${final_output_directory} ${output_csvfile}
+          resource_dir="EDEMA_BIOMARKER"
+          call_get_resourcefiles_metadata_saveascsv_args ${URI} ${resource_dir} ${final_output_directory} ${output_csvfile}
           #          call_download_files_in_a_resource_in_a_session_arguments=('call_get_resourcefiles_metadata_saveascsv_args' ${URI} ${resource_dir} ${final_output_directory} ${output_csvfile})
           #          outputfiles_present=$(python3 download_with_session_ID.py "${call_download_files_in_a_resource_in_a_session_arguments[@]}")
           #          NIFTIFILE_CSV_FLAG=${outputfiles_present: -1}

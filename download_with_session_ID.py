b7dc3c68ba95370ed133adcb9530099ecb7b2ce9
dharlabwustl <atulkumar@wustl.edu>
2023-06-22 22:21:46 -0500

    Commit Feb 23 2023

--- download_with_session_ID.py
+++ download_with_session_ID.py
@@ -388,7 +388,7 @@ def get_single_value_from_metadata_forascan(sessionId,scanId,metadata_field):
     df_1=df_1.reset_index()
     print(" I AM AT get_single_value_from_metadata_forascan")
     print(df.columns)
-    return "XX" #str(df_1.iloc[0][metadata_field])
+    return str(df_1.iloc[0][metadata_field])
 def decision_which_nifti_multiplescans(sessionId,dir_to_receive_the_data="",output_csvfile=""):
     this_session_metadata=get_metadata_session(sessionId)
     jsonStr = json.dumps(this_session_metadata)

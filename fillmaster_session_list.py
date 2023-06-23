56eabcd8a3b6cf5674d997471148165b11879c88
dharlabwustl <atulkumar@wustl.edu>
2023-06-23 11:42:35 -0500

    Commit Feb 23 2023

--- fillmaster_session_list.py
+++ fillmaster_session_list.py
@@ -431,6 +431,8 @@ def create_analytics_file(sessionlist_filename,csvfilename):
             counter_nifti_location=0
             for each_niftilocationfile in niftilocation_files:
                 print(each_niftilocationfile)
+                each_niftilocationfile_df=pd.read_csv(each_niftilocationfile)
+                fill_single_row_each_scan(each_niftilocationfile_df.iloc[0]['ID'],"SESSION_ID",row['ID'],row['label'],csvfilename)
             if counter_nifti_location==0:
                 fill_single_row_each_scan(identifier,"SESSION_ID",row['ID'],row['label'],csvfilename)
             counter=counter+1

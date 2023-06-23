0b574b7d461beb7a8d866bd79cafdefd4d2fd557
dharlabwustl <atulkumar@wustl.edu>
2023-06-22 22:04:39 -0500

    Commit Feb 23 2023

--- fillmaster_session_list.py
+++ fillmaster_session_list.py
@@ -419,8 +419,16 @@ def fill_single_row_each_scan(identifier,columnname,columnvalue,csvfilename):
     returnvalue=0
     try:
         if os.path.exists(csvfilename):
+            scan_type=get_single_value_from_metadata_forascan(columnvalue,str(identifier),'type')
+            scan_description=get_single_value_from_metadata_forascan(columnvalue,str(identifier),'series_description')
+            this_scan_dict={"ROW_IDENTIFIER":columnvalue+"_"+str(identifier),columnname:columnvalue, "SCAN_ID":identifier,"SCAN_TYPE":scan_type,"scan_description":scan_description}
+            this_scan_dict_df=pd.DataFrame([this_scan_dict])
             csvfilename_df=pd.read_csv(csvfilename)
-            this_scan_dict={"SCAN_ID":identifier,columnname:columnvalue}
+            csvfilename_df  = pd.concat([csvfilename_df,this_scan_dict_df],ignore_index=True)
+            csvfilename_df.to_csv(csvfilename,index=False)
+            # # this_scan_dict={"SCAN_ID":identifier,columnname:columnvalue}
+            # last_row_index=csvfilename_df['ROW_IDENTIFIER'].iget(-1)
+            # csvfilename_df.at[last_row_index+1,
         else:
             columnvalue_flag=0
             identifier=identifier

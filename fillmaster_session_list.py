1c43c7bddb4dff6493851fe8a75b500bca425f3d
dharlabwustl <atulkumar@wustl.edu>
2023-06-28 19:41:26 -0500

    Commit Feb 23 2023

--- fillmaster_session_list.py
+++ fillmaster_session_list.py
@@ -707,12 +707,12 @@ def creat_analytics_scanasID(sessionlist_filename,csvfilename,projectID,output_d
         # upload_pdfs(csvfilename,X_level,level_name,dir_to_save,resource_dirname_at_snipr)
         download_csvs_combine_upload(csvfilename,X_level,level_name,output_directory,resource_dirname_at_snipr)
         now=datetime.datetime.now()
-        date_time = now.strftime("%m_%d_%Y") #, %H:%M:%S")
+        date_time = now.strftime("%m%d%Y%H%M%S") #, %H:%M:%S")
         outputfilename=level_name+ "_"+"COMBINED_EDEMA_BIOMARKER_" + date_time+".csv"
 
         combinecsvs_inafiles_list(glob.glob(os.path.join(output_directory,"*.csv")),output_directory,outputfilename,sessionlist_filename)
         resource_dirname_at_snipr="EDEMA_BIOMARKER_TEST"
-        # uploadsinglefile_X_level(X_level,level_name,csvfilename,resource_dirname_at_snipr)
+        uploadsinglefile_X_level(X_level,level_name,os.path.join(output_directory,outputfilename),resource_dirname_at_snipr)
         returnvalue=1
 
     except:

1c9b248dac856ac9cb3bbfaac63ab00f9f7b20b4
dharlabwustl <atulkumar@wustl.edu>
2023-06-29 18:16:55 -0500

    Commit Feb 23 2023

--- fillmaster_session_list.py
+++ fillmaster_session_list.py
@@ -462,9 +462,11 @@ def scan_selected_flag_slice_num(URI_SCAN,download_dir):
                 subprocess.call("echo " + "I URI_SCAN AT ::{}  >> /workingoutput/error.txt".format(URI_SCAN) ,shell=True )
                 each_file_df=pd.read_csv(os.path.join(download_dir,each_file))
                 scan_uri_derived=str(each_file_df.at[0,"URI"]).split("/resource")[0]
+                subprocess.call("echo " + "I URI_SCAN AT ::{}  >> /workingoutput/error.txt".format(URI_SCAN) ,shell=True )
+                subprocess.call("echo " + "I URI_SCAN AT ::{}  >> /workingoutput/error.txt".format(scan_uri_derived) ,shell=True )
                 if scan_uri_derived==URI_SCAN:
                 # URI_SCAN_count=each_file_df.loc[each_file_df["URI"].str.split("/resources").str[0] == URI_SCAN, 'URI'].count()
-                # subprocess.call("echo " + "I URI_SCAN AT ::{}  >> /workingoutput/error.txt".format(URI_SCAN) ,shell=True )
+                    subprocess.call("echo " + "I URI_SCAN AT ::{}  >> /workingoutput/error.txt".format(URI_SCAN) ,shell=True )
                 # if URI_SCAN_count == 1 :
                 #     subprocess.call("echo " + "I URI_SCAN_count AT ::{}  >> /workingoutput/error.txt".format(URI_SCAN_count) ,shell=True )
                     # URI_SCAN_df=f_listfile[f_listfile['URI']==URI_SCAN]

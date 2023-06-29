30003578ae4129f7a4144f2da473858fa5061323
dharlabwustl <atulkumar@wustl.edu>
2023-06-29 15:56:26 -0500

    Commit Feb 23 2023

--- fillmaster_session_list.py
+++ fillmaster_session_list.py
@@ -459,7 +459,9 @@ def scan_selected_flag_slice_num(URI_SCAN,download_dir):
         for each_file in filenames:
             each_file_df=pd.read_csv(each_file)
             URI_SCAN_count=each_file_df.loc[each_file_df.URI == URI_SCAN, 'URI'].count()
+            subprocess.call("echo " + "I URI_SCAN AT ::{}  >> /workingoutput/error.txt".format(URI_SCAN) ,shell=True )
             if URI_SCAN_count == 1 :
+                subprocess.call("echo " + "I URI_SCAN_count AT ::{}  >> /workingoutput/error.txt".format(URI_SCAN_count) ,shell=True )
                 URI_SCAN_df=f_listfile[f_listfile['URI']==URI_SCAN]
                 URI_SCAN_SLICE_COUNT=URI_SCAN_df['NUMBEROFSLICES']
                 returnvalue=[1,URI_SCAN_SLICE_COUNT]

aed4f4f481af46c3ca0ea953cbf0f82793f58b85
dharlabwustl <atulkumar@wustl.edu>
2023-06-30 17:16:17 -0500

    Commit Feb 23 2023

--- SNIPR_ANALYTICS_SCANROW.sh
+++ SNIPR_ANALYTICS_SCANROW.sh
@@ -45,7 +45,8 @@ call_fill_sniprsession_list_arguments=('call_fill_sniprsession_list' ${sessions_
 # ${working_dir}/${project_ID}_SNIPER_ANALYTICS.csv  ${project_ID} ${output_directory} )
 outputfiles_present=$(python3 fillmaster_session_list.py "${call_fill_sniprsession_list_arguments[@]}")
 counter=$((counter + 1))
-if [ $counter -eq 7 ] ; then
+#if [ $counter -eq 7 ] ; then
+if [ ${array[1]} == "SNIPR01_E00894" ]  ; then
   break
 fi
 done < <(tail -n +2 "${sessions_list}")

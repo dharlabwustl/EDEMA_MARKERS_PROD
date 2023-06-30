81e3840e5c0350690d1e36c0394d749b889a0186
dharlabwustl <atulkumar@wustl.edu>
2023-06-30 18:09:31 -0500

    Commit Feb 23 2023

--- SNIPR_ANALYTICS_SCANROW.sh
+++ SNIPR_ANALYTICS_SCANROW.sh
@@ -37,8 +37,8 @@ function call_get_resourcefiles_metadata_saveascsv_args() {
 sessions_list=${working_dir}/'sessions.csv'
 time_now=$(date -dnow +%Y%m%d%H%M%S)
 copy_session=${sessions_list%.csv}_ANALYTICS_${time_now}.csv
-scan_analytics=SCAN_ANALYTICS_${time_now}.csv
-scan_analytics_nofilename=SCAN_ANALYTICS_NOFILENAME${time_now}.csv
+scan_analytics=${sessions_list%sessions.csv}SCAN_ANALYTICS_${time_now}.csv
+scan_analytics_nofilename=${sessions_list%sessions.csv}SCAN_ANALYTICS_NOFILENAME${time_now}.csv
 curl -u $XNAT_USER:$XNAT_PASS -X GET $XNAT_HOST/data/projects/${project_ID}/experiments/?format=csv >${sessions_list}
 cp ${sessions_list} ${copy_session}
 counter=0

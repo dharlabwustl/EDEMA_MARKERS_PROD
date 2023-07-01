888eeec7e8c8d7017dfa4b71941f5d583707d44d
dharlabwustl <atulkumar@wustl.edu>
2023-07-01 18:02:38 -0500

    Commit Feb 23 2023

--- fillmaster_session_list.py
+++ fillmaster_session_list.py
@@ -830,14 +830,14 @@ def creat_analytics_onesessionscanasID(sessionId,sessionLabel,csvfilename,csvfil
 
     try:
         subprocess.call("echo  " + "I PASSED AT ::{}:{} >> /workingoutput/error.txt".format(sessionId,sessionLabel) ,shell=True )
-        command="rm " + os.path.dirname(csvfilename) +"/*.pdf"
-        subprocess.call(command,shell=True)
-
-        this_session_metadata=get_metadata_session(sessionId)
-        jsonStr = json.dumps(this_session_metadata)
-
-        each_session_metadata_df = pd.read_json(jsonStr)
-        subprocess.call("echo  " + "I PASSED AT ::{}:{} >> /workingoutput/error.txt".format(sessionId,sessionLabel) ,shell=True )
+        # command="rm " + os.path.dirname(csvfilename) +"/*.pdf"
+        # subprocess.call(command,shell=True)
+        #
+        # this_session_metadata=get_metadata_session(sessionId)
+        # jsonStr = json.dumps(this_session_metadata)
+        #
+        # each_session_metadata_df = pd.read_json(jsonStr)
+        # subprocess.call("echo  " + "I PASSED AT ::{}:{} >> /workingoutput/error.txt".format(sessionId,sessionLabel) ,shell=True )
         # nifti_file_list=list_niftilocation(sessionId,os.path.dirname(csvfilename))  #"SESSION_NOT_SELECTED"
 
         #

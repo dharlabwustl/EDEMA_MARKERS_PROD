cf456465bd6f6c936d0c5b958c78ec256fc5e05c
dharlabwustl <atulkumar@wustl.edu>
2023-07-02 14:30:03 -0500

    Commit Feb 23 2023

--- fillmaster_session_list.py
+++ fillmaster_session_list.py
@@ -1148,15 +1148,18 @@ def fill_sniprsession_list(sessionlist_filename,session_id):
         # check_available_file_and_document(row_identifier,extension_to_find_list,SCAN_URI,resource_dir,columnname,csvfilename)
                 if len(_infarct_auto_removesmall_path)>1:
                     csv_file_num=csv_file_num+1
+                    subprocess.call("echo " + "csv_file_num::{}  >> /workingoutput/error.txt".format(csv_file_num) ,shell=True )
                 resource_dir="MASKS"
                 extension_to_find_list="_infarct_auto_removesmall.nii.gz"
                 _infarct_auto_removesmall_path=get_filepath_withfileext_from_metadata(SCAN_URI,resource_dir,extension_to_find_list)
                 if len(_infarct_auto_removesmall_path)>1:
                     infarct_file_num=infarct_file_num+1
+                    subprocess.call("echo " + "infarct_file_num::{}  >> /workingoutput/error.txt".format(infarct_file_num) ,shell=True )
                 extension_to_find_list="_csf_unet.nii.gz"
                 _infarct_auto_removesmall_path=get_filepath_withfileext_from_metadata(SCAN_URI,resource_dir,extension_to_find_list)
                 if len(_infarct_auto_removesmall_path)>1:
                     csf_file_num=csf_file_num+1
+                    subprocess.call("echo " + "csf_file_num::{}  >> /workingoutput/error.txt".format(csf_file_num) ,shell=True )
         ### DICOM TO NIFTI STEP
         niftifiles_num=count_niftifiles_insession(session_id,os.path.dirname(sessionlist_filename))
         columnname="NUMBER_NIFTIFILES"

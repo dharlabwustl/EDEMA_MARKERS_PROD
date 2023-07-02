4c6512ac87132f93d0f56f4c468f516ba9ecd4a1
dharlabwustl <atulkumar@wustl.edu>
2023-07-01 22:21:55 -0500

    Commit Feb 23 2023

--- fillmaster_session_list.py
+++ fillmaster_session_list.py
@@ -875,7 +875,8 @@ def creat_analytics_onesessionscanasID(sessionId,sessionLabel,csvfilename,csvfil
 
                 print(nifti_file_list)
                 nifti_file_list.to_csv(os.path.join(os.path.dirname(csvfilename),'nifti_file_list.csv'),index=False)
-                SCAN_SELECTED_DF=nifti_file_list[nifti_file_list["URI"].str.split("/resources").str[0] == SCAN_URI]
+                nifti_file_list["URI_1"]=nifti_file_list["URI"].str.split("/resources").str[0]
+                SCAN_SELECTED_DF=nifti_file_list[nifti_file_list["URI_1"] == SCAN_URI]
                 if SCAN_SELECTED_DF.shape[0] > 0 :
                     fill_single_datapoint_each_scan_1(each_session_metadata_df_row["URI"],"SCAN_SELECTED",1,tempfile)
                     fill_single_datapoint_each_scan_1(each_session_metadata_df_row["URI"],"SLICE_COUNT",SCAN_SELECTED_DF["NUMBEROFSLICES"],tempfile)

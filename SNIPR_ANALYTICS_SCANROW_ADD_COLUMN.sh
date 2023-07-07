795c3987e13a22dbe3671626491c25084b3d2624
dharlabwustl <atulkumar@wustl.edu>
2023-07-07 11:30:55 -0500

    Commit Feb 23 2023

--- SNIPR_ANALYTICS_SCANROW_ADD_COLUMN.sh
+++ SNIPR_ANALYTICS_SCANROW_ADD_COLUMN.sh
@@ -58,10 +58,10 @@ while IFS="," read -ra array; do
 
   if [[ ${array[-1]} == *"SCAN_ANALYTICS_"* ]] && [[ ${array[-1]} != *"NOFILENAME"* ]]; then
     echo ${array[-3]}
-    url=${array[-3]}
+    uri=${array[-3]}
     filename=${array[-1]}
     dir_to_save=${working_dir}
-    call_download_a_singlefile_with_URIString_arguments=('call_download_a_singlefile_with_URIString' ${URI} ${resource_dir} ${dir_to_receive_the_data} ${output_csvfile})
+    call_download_a_singlefile_with_URIString_arguments=('call_download_a_singlefile_with_URIString' ${uri} ${resource_dir} ${dir_to_receive_the_data} ${output_csvfile})
     outputfiles_present=$(python3 download_with_session_ID.py "${call_download_a_singlefile_with_URIString_arguments[@]}")
 
   fi

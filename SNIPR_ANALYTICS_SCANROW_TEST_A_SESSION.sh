de69ad141aee89d1939ee7dccd63c7fe1f3ff741
dharlabwustl <atulkumar@wustl.edu>
2023-07-13 08:35:18 -0500

    no comment

--- SNIPR_ANALYTICS_SCANROW_TEST_A_SESSION.sh
+++ SNIPR_ANALYTICS_SCANROW_TEST_A_SESSION.sh
@@ -67,7 +67,7 @@ if [ ${array[4]} == "xnat:ctSessionData" ] ; then  ######### && [ ${array[1]} ==
     echo "${array[5]}"
 call_fill_sniprsession_list_arguments=('call_fill_sniprsession_list' ${copy_session} ${array[1]} ) ##
 ## ${working_dir}/${project_ID}_SNIPER_ANALYTICS.csv  ${project_ID} ${output_directory} )
-outputfiles_present=$(python3 fillmaster_session_list.py "${call_fill_sniprsession_list_arguments[@]}")
+#outputfiles_present=$(python3 fillmaster_session_list.py "${call_fill_sniprsession_list_arguments[@]}")
 call_creat_analytics_onesessionscanasID_arguments=('call_creat_analytics_onesessionscanasID' ${array[1]} ${array[5]} ${scan_analytics}  ${scan_analytics_nofilename})
 outputfiles_present=$(python3 fillmaster_session_list.py "${call_creat_analytics_onesessionscanasID_arguments[@]}")
 

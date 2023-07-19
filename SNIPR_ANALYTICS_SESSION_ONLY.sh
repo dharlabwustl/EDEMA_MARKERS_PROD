ca8e63e303963c6919560db7de5535aa44ed9807
dharlabwustl <atulkumar@wustl.edu>
2023-07-19 11:53:26 -0500

    no comment

--- SNIPR_ANALYTICS_SESSION_ONLY.sh
+++ SNIPR_ANALYTICS_SESSION_ONLY.sh
@@ -57,25 +57,25 @@ curl -u $XNAT_USER:$XNAT_PASS -X GET $XNAT_HOST/data/projects/${project_ID}/expe
 cp ${sessions_list} ${copy_session}
 counter=0
 while IFS=',' read -ra array; do
-xx=0
+  xx=0
 
-#if [ ${array[1]} == "SNIPR01_E00894" ]  ; then
-#  echo "${array[1]}"
-#  echo "${array[5]}"
-if [ ${array[4]} == "xnat:ctSessionData" ] ; then
+  #if [ ${array[1]} == "SNIPR01_E00894" ]  ; then
+  #  echo "${array[1]}"
+  #  echo "${array[5]}"
+  if [ ${array[4]} == "xnat:ctSessionData" ]; then
     echo "${array[1]}"
     echo "${array[5]}"
-call_fill_sniprsession_list_arguments=('call_fill_sniprsession_list' ${copy_session} ${array[1]} ) ##
-## ${working_dir}/${project_ID}_SNIPER_ANALYTICS.csv  ${project_ID} ${output_directory} )
-outputfiles_present=$(python3 fillmaster_session_list.py "${call_fill_sniprsession_list_arguments[@]}")
-#call_creat_analytics_onesessionscanasID_arguments=('call_creat_analytics_onesessionscanasID' ${array[1]} ${array[5]} ${scan_analytics}  ${scan_analytics_nofilename})
-#outputfiles_present=$(python3 fillmaster_session_list.py "${call_creat_analytics_onesessionscanasID_arguments[@]}")
-#def creat_analytics_onesessionscanasID(sessionId,sessionLabel,csvfilename,csvfilename_withoutfilename)
-#counter=$((counter + 1))
-fi
-#if [ $counter -eq 7 ] ; then
-#  break
-#fi
+    call_fill_sniprsession_list_arguments=('call_fill_sniprsession_list' ${copy_session} ${array[1]}) ##
+    ## ${working_dir}/${project_ID}_SNIPER_ANALYTICS.csv  ${project_ID} ${output_directory} )
+    outputfiles_present=$(python3 fillmaster_session_list.py "${call_fill_sniprsession_list_arguments[@]}")
+  #call_creat_analytics_onesessionscanasID_arguments=('call_creat_analytics_onesessionscanasID' ${array[1]} ${array[5]} ${scan_analytics}  ${scan_analytics_nofilename})
+  #outputfiles_present=$(python3 fillmaster_session_list.py "${call_creat_analytics_onesessionscanasID_arguments[@]}")
+  #def creat_analytics_onesessionscanasID(sessionId,sessionLabel,csvfilename,csvfilename_withoutfilename)
+  #counter=$((counter + 1))
+  fi
+  #if [ $counter -eq 7 ] ; then
+  #  break
+  #fi
 done < <(tail -n +2 "${sessions_list}")
 
 #call_edit_scan_analytics_file_arguments=('call_edit_scan_analytics_file'  ${scan_analytics}  ${scan_analytics_nofilename})
@@ -93,9 +93,9 @@ dir_to_save=${working_dir}
 #
 #copysinglefile_to_sniprproject  ${project_ID}  "${dir_to_save}"  ${resource_dirname_at_snipr}  ${outputfilename}
 resource_dirname_at_snipr="SNIPR_ANALYTICS_TEST"
-call_edit_session_analytics_file_arguments=('call_edit_session_analytics_file'   ${copy_session} )
+call_edit_session_analytics_file_arguments=('call_edit_session_analytics_file' ${copy_session})
 outputfiles_present=$(python3 fillmaster_session_list.py "${call_edit_session_analytics_file_arguments[@]}")
 
 #copysinglefile_to_sniprproject  ${project_ID}  "${dir_to_save}"  ${resource_dirname_at_snipr}  $(basename ${scan_analytics} )
 #copysinglefile_to_sniprproject  ${project_ID}  "${dir_to_save}"  ${resource_dirname_at_snipr}  $(basename ${scan_analytics_nofilename} )
-copysinglefile_to_sniprproject  ${project_ID}  "${dir_to_save}"  ${resource_dirname_at_snipr}  $(basename ${copy_session} )
+copysinglefile_to_sniprproject ${project_ID} "${dir_to_save}" ${resource_dirname_at_snipr} $(basename ${copy_session})

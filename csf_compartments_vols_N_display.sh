3a69d31592d675d80d56b1bb6d500e48088238cd
dharlabwustl <atulkumar@wustl.edu>
2023-07-31 16:40:30 -0500

    no comment

--- csf_compartments_vols_N_display.sh
+++ csf_compartments_vols_N_display.sh
@@ -625,7 +625,7 @@ while IFS=',' read -ra array; do
               echo "It's there!"
               echo "${array2[6]}"
               filename2=$(basename ${url2})
-              call_download_a_singlefile_with_URIString_arguments=('call_download_a_singlefile_with_URIString' ${url2} ${filename2} ${dir_to_save})
+              call_download_a_singlefile_with_URIString_arguments=('call_download_a_singlefile_with_URIString' ${url2} ${filename2} ${output_directory})
               outputfiles_present=$(python3 download_with_session_ID.py "${call_download_a_singlefile_with_URIString_arguments[@]}")
               csffile=${dir_to_save}/${filename2}
               echo "${csffile}"

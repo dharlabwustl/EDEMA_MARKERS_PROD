f8f5257d7a7ac4bcbd9af9d9ece4c4fd5c44dce3
dharlabwustl <atulkumar@wustl.edu>
2023-07-31 10:37:43 -0500

    no comment

--- csf_compartments_vols_N_display.sh
+++ csf_compartments_vols_N_display.sh
@@ -315,6 +315,24 @@ while IFS=',' read -ra array; do
         csffile=${dir_to_save}/${filename2}
         echo "${csffile}"
       fi
+      if [[ ${url2} == *"_sulci_"* ]]; then #  || [[ ${url2} == *"_levelset_bet"* ]]  || [[ ${url2} == *"csf_unet"* ]]  ; then ##[[ $string == *"My long"* ]]; then
+        echo "It's there!"
+        echo "${array2[6]}"
+        filename2=$(basename ${url2})
+        call_download_a_singlefile_with_URIString_arguments=('call_download_a_singlefile_with_URIString' ${url2} ${filename2} ${dir_to_save})
+        outputfiles_present=$(python3 download_with_session_ID.py "${call_download_a_singlefile_with_URIString_arguments[@]}")
+        csffile=${dir_to_save}/${filename2}
+        echo "${csffile}"
+      fi
+      if [[ ${url2} == *"_ventricle_"* ]]; then #  || [[ ${url2} == *"_levelset_bet"* ]]  || [[ ${url2} == *"csf_unet"* ]]  ; then ##[[ $string == *"My long"* ]]; then
+        echo "It's there!"
+        echo "${array2[6]}"
+        filename2=$(basename ${url2})
+        call_download_a_singlefile_with_URIString_arguments=('call_download_a_singlefile_with_URIString' ${url2} ${filename2} ${dir_to_save})
+        outputfiles_present=$(python3 download_with_session_ID.py "${call_download_a_singlefile_with_URIString_arguments[@]}")
+        csffile=${dir_to_save}/${filename2}
+        echo "${csffile}"
+      fi
     done < <(tail -n +2 "${working_dir}/${output_csvfile_1}")
 
   done < <(tail -n +2 "${dir_to_save}/${filename}")

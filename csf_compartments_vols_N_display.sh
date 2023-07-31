f699d9c1f3f1c23cff1ab652e66f06d668fc447f
dharlabwustl <atulkumar@wustl.edu>
2023-07-31 14:08:18 -0500

    no comment

--- csf_compartments_vols_N_display.sh
+++ csf_compartments_vols_N_display.sh
@@ -60,6 +60,58 @@ get_relevantfile_in_A_DIRECTORY()" ${sessionID} ${dir_to_receive_the_data} ${res
 
 }
 
+run_IML() {
+  this_filename=${1}
+  this_betfilename=${2}
+  #  this_csfmaskfilename=${3}
+  #  this_infarctmaskfilename=${4}
+  echo "BET USING LEVELSET MASK"
+
+  /software/bet_withlevelset.sh $this_filename ${this_betfilename} #${output_directory} #Helsinki2000_1019_10132014_1048_Head_2.0_ax_Tilt_1_levelset # ${3} # Helsinki2000_702_12172013_2318_Head_2.0_ax_levelset.nii.gz #${3} # $6 $7 $8 $9 ${10}
+
+  echo "bet_withlevelset successful" >${output_directory}/success.txt
+  this_filename_brain=${this_filename%.nii*}_brain_f.nii.gz
+  # cp ${this_filename_brain} ${output_directory}/ #  ${final_output_directory}/
+  echo "LINEAR REGISTRATION TO TEMPLATE"
+  /software/linear_rigid_registration.sh ${this_filename_brain} #${templatefilename} #$3 ${6} WUSTL_233_11122015_0840__levelset_brain_f.nii.gz
+  echo "linear_rigid_registration successful" >>${output_directory}/success.txt
+  echo "RUNNING IML FSL PART"
+  /software/ideal_midline_fslpart.sh ${this_filename} # ${templatefilename} ${mask_on_template}  #$9 #${10} #$8
+  echo "ideal_midline_fslpart successful" >>${output_directory}/success.txt
+  echo "RUNNING IML PYTHON PART"
+
+  /software/ideal_midline_pythonpart.sh ${this_filename} #${templatefilename}  #$3 #$8 $9 ${10}
+  echo "ideal_midline_pythonpart successful" >>${output_directory}/success.txt
+
+  #  echo "RUNNING NWU AND CSF VOLUME CALCULATION "
+  #
+  #  /software/nwu_csf_volume.sh ${this_filename} ${this_betfilename} ${this_csfmaskfilename} ${this_infarctmaskfilename} ${lower_threshold} ${upper_threshold}
+  #  echo "nwu_csf_volume successful" >>${output_directory}/success.txt
+  #  thisfile_basename=$(basename $this_filename)
+  #  # for texfile in $(/usr/lib/fsl/5.0/remove_ext ${output_directory}/$thisfile_basename)*.tex ;
+  #  for texfile in ${output_directory}/*.tex; do
+  #    pdflatex -halt-on-error -interaction=nonstopmode -output-directory=${output_directory} $texfile ##${output_directory}/$(/usr/lib/fsl/5.0/remove_ext $this_filename)*.tex
+  #    rm ${output_directory}/*.aux
+  #    rm ${output_directory}/*.log
+  #  done
+  #
+  #  for filetocopy in $(/usr/lib/fsl/5.0/remove_ext ${output_directory}/$thisfile_basename)*_brain_f.nii.gz; do
+  #    cp ${filetocopy} ${final_output_directory}/
+  #  done
+  #
+  #  for filetocopy in $(/usr/lib/fsl/5.0/remove_ext ${output_directory}/$thisfile_basename)*.mat; do
+  #    cp ${filetocopy} ${final_output_directory}/
+  #  done
+  #
+  #  for filetocopy in ${output_directory}/*.pdf; do
+  #    cp ${filetocopy} ${final_output_directory}/
+  #  done
+  #  for filetocopy in ${output_directory}/*.csv; do
+  #    cp ${filetocopy} ${final_output_directory}/
+  #  done
+
+}
+
 run_IML_NWU_CSF_CALC() {
   this_filename=${1}
   this_betfilename=${2}
@@ -109,6 +161,89 @@ run_IML_NWU_CSF_CALC() {
   for filetocopy in ${output_directory}/*.csv; do
     cp ${filetocopy} ${final_output_directory}/
   done
+
+}
+midlineonly_each_scan() {
+  local niftifilename_ext=${1}
+
+  eachfile_basename_noext=''
+  originalfile_basename=''
+  original_ct_file=''
+  #  for eachfile in ${working_dir}/*.nii*; do
+  for eachfile in ${working_dir_1}/*${niftifilename_ext}; do
+    original_ct_file=${eachfile}
+    eachfile_basename=$(basename ${eachfile})
+    originalfile_basename=${eachfile_basename}
+    eachfile_basename_noext=${eachfile_basename%.nii*}
+
+    ############## files basename ##################################
+    grayfilename=${eachfile_basename_noext}_resaved_levelset.nii
+    if [[ "$eachfile_basename" == *".nii.gz"* ]]; then #"$STR" == *"$SUB"*
+      grayfilename=${eachfile_basename_noext}_resaved_levelset.nii.gz
+    fi
+    betfilename=${eachfile_basename_noext}_resaved_levelset_bet.nii.gz
+    #    csffilename=${eachfile_basename_noext}_resaved_csf_unet.nii.gz
+    #    infarctfilename=${eachfile_basename_noext}_resaved_infarct_auto_removesmall.nii.gz
+    ################################################
+    ############## copy those files to the docker image ##################################
+    cp ${working_dir}/${betfilename} ${output_directory}/
+    #    cp ${working_dir}/${csffilename} ${output_directory}/
+    #    cp ${working_dir}/${infarctfilename} ${output_directory}/
+    ####################################################################################
+    source /software/bash_functions_forhost.sh
+
+    cp ${original_ct_file} ${output_directory}/${grayfilename}
+    grayimage=${output_directory}/${grayfilename} #${gray_output_subdir}/${eachfile_basename_noext}_resaved_levelset.nii
+    ###########################################################################
+
+    #    #### originalfiel: .nii
+    #    #### betfile: *bet.nii.gz
+    #
+    #    # original_ct_file=$original_CT_directory_names/
+    #    levelset_infarct_mask_file=${output_directory}/${infarctfilename}
+    #    echo "levelset_infarct_mask_file:${levelset_infarct_mask_file}"
+    #    ## preprocessing infarct mask:
+    #    python3 -c "
+    #import sys ;
+    #sys.path.append('/software/') ;
+    #from utilities_simple_trimmed import * ;  levelset2originalRF_new_flip()" "${original_ct_file}" "${levelset_infarct_mask_file}" "${output_directory}"
+
+    ## preprocessing bet mask:
+    levelset_bet_mask_file=${output_directory}/${betfilename}
+    echo "levelset_bet_mask_file:${levelset_bet_mask_file}"
+    python3 -c "
+
+import sys ;
+sys.path.append('/software/') ;
+from utilities_simple_trimmed import * ;  levelset2originalRF_new_flip()" "${original_ct_file}" "${levelset_bet_mask_file}" "${output_directory}"
+
+    #    #### preprocessing csf mask:
+    #    levelset_csf_mask_file=${output_directory}/${csffilename}
+    #    echo "levelset_csf_mask_file:${levelset_csf_mask_file}"
+    #    python3 -c "
+    #import sys ;
+    #sys.path.append('/software/') ;
+    #from utilities_simple_trimmed import * ;   levelset2originalRF_new_flip()" "${original_ct_file}" "${levelset_csf_mask_file}" "${output_directory}"
+
+    #    lower_threshold=0
+    #    upper_threshold=20
+    #    templatefilename=scct_strippedResampled1.nii.gz
+    #    mask_on_template=midlinecssfResampled1.nii.gz
+
+    x=$grayimage
+    bet_mask_filename=${output_directory}/${betfilename}
+    #    infarct_mask_filename=${output_directory}/${infarctfilename}
+    #    csf_mask_filename=${output_directory}/${csffilename}
+    run_IML $x ${bet_mask_filename} #${csf_mask_filename} ${infarct_mask_filename}
+
+  done
+
+  # for f in ${output_directory}/*; do
+  #     # if [ -d "$f" ]; then
+  #         # $f is a directory
+  #         rm -r $f
+  #     # fi
+  # done
 
 }
 
@@ -292,6 +427,9 @@ while IFS=',' read -ra array; do
   while IFS=',' read -ra array1; do
     #      echo "${array1[0]}"
     url1=${array1[0]}
+    filename_nifti=$(basename ${url1})
+    call_download_a_singlefile_with_URIString_arguments=('call_download_a_singlefile_with_URIString' ${url1} ${filename_nifti} ${dir_to_save})
+    outputfiles_present=$(python3 download_with_session_ID.py "${call_download_a_singlefile_with_URIString_arguments[@]}")
     #      URI=/data/experiments/${sessionID}
     resource_dir="MASKS"
     output_csvfile_1=${sessionID}_MASK_METADATA.csv
@@ -396,65 +534,67 @@ done < <(tail -n +2 "${working_dir}/${output_csvfile}")
 #outputfiles_present=$(python3 download_with_session_ID.py "${call_download_files_in_a_resource_in_a_session_arguments[@]}")
 #echo '$outputfiles_present'::$outputfiles_present
 #########################################
-#for niftifile_csvfilename in ${working_dir}/*NIFTILOCATION.csv; do
-#  rm ${final_output_directory}/*.*
-#  rm ${output_directory}/*.*
-#  outputfiles_present=0
-#  echo $niftifile_csvfilename
-#  while IFS=',' read -ra array; do
-#    scanID=${array[2]}
-#    echo sessionId::${sessionID}
-#    echo scanId::${scanID}
-#    snipr_output_foldername="EDEMA_BIOMARKER"
-#    ### check if the file exists:
-#    call_check_if_a_file_exist_in_snipr_arguments=('call_check_if_a_file_exist_in_snipr' ${sessionID} ${scanID} ${snipr_output_foldername} .pdf .csv)
-#    outputfiles_present=$(python3 download_with_session_ID.py "${call_check_if_a_file_exist_in_snipr_arguments[@]}")
-#
-#    ################################################
-#    outputfiles_present=0
-#    echo "outputfiles_present:: "${outputfiles_present: -1}"::outputfiles_present"
-#    #echo "outputfiles_present::ATUL${outputfiles_present}::outputfiles_present"
-#    if [[ "${outputfiles_present: -1}" -eq 1 ]]; then
-#      echo " I AM THE ONE"
-#    fi
-#    if [[ "${outputfiles_present: -1}" -eq 0 ]]; then ##[[ 1 -gt 0 ]]  ; then #
-#
-#      echo "outputfiles_present:: "${outputfiles_present: -1}"::outputfiles_present"
-#
-#      copy_scan_data ${niftifile_csvfilename} ${working_dir_1} #${working_dir}
-#
-#      ##############################################################################################################
-#
-#      ## GET THE RESPECTIVS MASKS NIFTI FILE NAME AND COPY IT TO THE WORKING_DIR
-#
-#      #####################################################################################
-#      resource_dirname='MASKS'
-#      output_dirname=${working_dir}
-#      while IFS=',' read -ra array; do
-#        scanID=${array[2]}
-#        echo sessionId::${sessionID}
-#        echo scanId::${scanID}
-#      done < <(tail -n +2 "${niftifile_csvfilename}")
-#      echo working_dir::${working_dir}
-#      echo output_dirname::${output_dirname}
-#      copy_masks_data ${sessionID} ${scanID} ${resource_dirname} ${output_dirname}
-#      ######################################################################################################################
-#      ## CALCULATE EDEMA BIOMARKERS
-#      nwucalculation_each_scan
-#      ######################################################################################################################
-#      ## COPY IT TO THE SNIPR RESPECTIVE SCAN RESOURCES
-#      snipr_output_foldername="EDEMA_BIOMARKER"
-#      file_suffixes=(.pdf .mat .csv) #sys.argv[5]
-#      for file_suffix in ${file_suffixes[@]}; do
-#        copyoutput_to_snipr ${sessionID} ${scanID} "${final_output_directory}" ${snipr_output_foldername} ${file_suffix}
-#      done
-#      ######################################################################################################################
-#      echo " FILES NOT PRESENT I AM WORKING ON IT"
-#    else
-#      echo " FILES ARE PRESENT "
-#    ######################################################################################################################
-#    fi
-#    ##
-#
-#  done < <(tail -n +2 "${niftifile_csvfilename}")
-#done
+for niftifile_csvfilename in ${working_dir}/*NIFTILOCATION.csv; do
+  rm ${final_output_directory}/*.*
+  rm ${output_directory}/*.*
+  outputfiles_present=0
+  echo $niftifile_csvfilename
+  while IFS=',' read -ra array; do
+    scanID=${array[2]}
+    echo sessionId::${sessionID}
+    echo scanId::${scanID}
+    snipr_output_foldername="EDEMA_BIOMARKER"
+    ### check if the file exists:
+    call_check_if_a_file_exist_in_snipr_arguments=('call_check_if_a_file_exist_in_snipr' ${sessionID} ${scanID} ${snipr_output_foldername} .pdf .csv)
+    outputfiles_present=$(python3 download_with_session_ID.py "${call_check_if_a_file_exist_in_snipr_arguments[@]}")
+
+    ################################################
+    outputfiles_present=0
+    echo "outputfiles_present:: "${outputfiles_present: -1}"::outputfiles_present"
+    #echo "outputfiles_present::ATUL${outputfiles_present}::outputfiles_present"
+    if [[ "${outputfiles_present: -1}" -eq 1 ]]; then
+      echo " I AM THE ONE"
+    fi
+    if [[ "${outputfiles_present: -1}" -eq 0 ]]; then ##[[ 1 -gt 0 ]]  ; then #
+
+      echo "outputfiles_present:: "${outputfiles_present: -1}"::outputfiles_present"
+
+      copy_scan_data ${niftifile_csvfilename} ${working_dir_1} #${working_dir}
+
+      ##############################################################################################################
+
+      ## GET THE RESPECTIVS MASKS NIFTI FILE NAME AND COPY IT TO THE WORKING_DIR
+
+      #####################################################################################
+      resource_dirname='MASKS'
+      output_dirname=${working_dir}
+      while IFS=',' read -ra array; do
+        scanID=${array[2]}
+        echo sessionId::${sessionID}
+        echo scanId::${scanID}
+      done < <(tail -n +2 "${niftifile_csvfilename}")
+      echo working_dir::${working_dir}
+      echo output_dirname::${output_dirname}
+      copy_masks_data ${sessionID} ${scanID} ${resource_dirname} ${output_dirname}
+      ######################################################################################################################
+      ## CALCULATE EDEMA BIOMARKERS
+      #      nwucalculation_each_scan
+      midlineonly_each_scan
+      #########################
+      # ############################################################################################
+      ## COPY IT TO THE SNIPR RESPECTIVE SCAN RESOURCES
+      snipr_output_foldername="EDEMA_BIOMARKER"
+      file_suffixes=(.pdf .mat .csv) #sys.argv[5]
+      for file_suffix in ${file_suffixes[@]}; do
+        copyoutput_to_snipr ${sessionID} ${scanID} "${final_output_directory}" ${snipr_output_foldername} ${file_suffix}
+      done
+      ######################################################################################################################
+      echo " FILES NOT PRESENT I AM WORKING ON IT"
+    else
+      echo " FILES ARE PRESENT "
+    ######################################################################################################################
+    fi
+    ##
+
+  done < <(tail -n +2 "${niftifile_csvfilename}")
+done

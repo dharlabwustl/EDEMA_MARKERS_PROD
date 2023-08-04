fb669c5d3a50a927c8d760e190f87bf9f49af7b2
dharlabwustl <atulkumar@wustl.edu>
2023-08-04 14:10:53 -0500

    no comment

--- EDIT_COMBINED_CSV_OF_EDEMABIOMARKERS.sh
+++ EDIT_COMBINED_CSV_OF_EDEMABIOMARKERS.sh
@@ -105,6 +105,15 @@ splitter='_'
 splitter_idx='-1'
 call_make_a_column_with_substring_from_othercolumn_arguments=('call_make_a_column_with_substring_from_othercolumn' ${csvfilename_input} ${csvfilename_output} ${column_name_forstring} ${new_column_name} ${splitter} ${splitter_idx})
 outputfiles_present=$(python3 fillmaster_session_list.py "${call_make_a_column_with_substring_from_othercolumn_arguments[@]}")
+
+csvfilename_input=${SCAN_ANALYTICS_NOFILENAME_FILE}
+csvfilename_output=${SCAN_ANALYTICS_NOFILENAME_FILE}
+column_name_forstring='URI'
+new_column_name='SCAN_NUMBER'
+splitter='_'
+splitter_idx='-1'
+call_make_a_column_with_substring_from_othercolumn_arguments=('call_make_a_column_with_substring_from_othercolumn' ${csvfilename_input} ${csvfilename_output} ${column_name_forstring} ${new_column_name} ${splitter} ${splitter_idx})
+outputfiles_present=$(python3 fillmaster_session_list.py "${call_make_a_column_with_substring_from_othercolumn_arguments[@]}")
 #URI=/data/projects/${project_ID}
 #resource_dir='EDEMA_BIOMARKER_TEST'
 ##final_output_directory=${working_dir}

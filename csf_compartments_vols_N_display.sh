1a39377da50bd27979e8ecba06eb64c97807a6c4
dharlabwustl <atulkumar@wustl.edu>
2023-08-11 13:55:39 -0500

    no comment

--- csf_compartments_vols_N_display.sh
+++ csf_compartments_vols_N_display.sh
@@ -137,7 +137,7 @@ run_IML() {
 
   echo "RUNNING IML PYTHON PART"
 
-#  /software/ideal_midline_pythonpart.sh ${this_filename} #${templatefilename}  #$3 #$8 $9 ${10}
+  /software/ideal_midline_pythonpart.sh ${this_filename} #${templatefilename}  #$3 #$8 $9 ${10}
   echo "ideal_midline_pythonpart successful" >>${output_directory}/success.txt
   /software/ideal_midline_pythonpart_V2.sh ${this_filename} #${templatefilename}  #$3 #$8 $9 ${10}
   #    echo "RUNNING NWU AND CSF VOLUME CALCULATION "

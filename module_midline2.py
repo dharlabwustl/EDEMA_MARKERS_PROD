6c82628068035692a71ddca68089949be26cd35e
dharlabwustl <atulkumar@wustl.edu>
2023-08-11 12:47:16 -0500

    no comment

--- module_midline2.py
+++ module_midline2.py
@@ -34,28 +34,15 @@ def fit_line_to_midlinepixels_ORF_sh():
      gray_file=sys.argv[1] #"/media/atul/AC0095E80095BA32/WASHU_WORK/PROJECTS/MIDLINE/DATA/CTs_SP_Pineal/CTs/Helsinki2000_1225_1_05012015_1250_Head_2.0_ax_Tilt_1.nii"
      midline_nifti_file=sys.argv[2] #"/media/atul/AC0095E80095BA32/WASHU_WORK/PROJECTS/MIDLINE/SOFTWARE/pyscripts/IDEALML/midlinecssfResampled1InverseT.nii.gz"
      SAVE_DIRECTORY=sys.argv[3]
-     methodType=sys.argv[4]
-     method_name=sys.argv[5]
-     # infarctfile_present=int(float(sys.argv[6]))
-     # latexfilename=os.path.join(SAVE_DIRECTORY,os.path.basename(gray_file.split(".nii")[0].replace('.','_')) + "_IML.tex")
-     # print("latexfilename")
-     # print(latexfilename)
-     # latex_start(latexfilename)
-     # latex_begin_document(latexfilename)
-     
+     # methodType=sys.argv[4]
+     method_name=sys.argv[4]
      midline_nifti=nib.load(midline_nifti_file)
      midline_nifti_np=midline_nifti.get_fdata()
-     # if infarctfile_present==1:
-     # midline_nifti_np=resizeinto_512by512(midline_nifti_np)
      gray_nifti=nib.load(gray_file)
      gray_nifti_np=gray_nifti.get_fdata()  
-     # if infarctfile_present==1: 
-     # gray_nifti_np=resizeinto_512by512(gray_nifti_np)
+
      gray_nifti_np_im=contrast_stretch(gray_nifti_np,1)*255
-#     if "_levelset" in os.path.basename(gray_file):
-#         gray_nifti_np_im=contrast_stretch(gray_nifti_np,2)*255
-#     if "_header_copied" in os.path.basename(gray_file):
-#         gray_nifti_np_im=contrast_stretch(gray_nifti_np,1)*255  
+
      numberofslices=midline_nifti_np.shape[2]
      for xx in range(numberofslices): 
          this_slice=midline_nifti_np[:,:,xx]
@@ -78,18 +65,12 @@ def fit_line_to_midlinepixels_ORF_sh():
              min_samples=X.shape[0]
              print("min_samples")
              print(min_samples)
- #            z = np.abs(stats.zscore(boston_df))
- #            threshold = 3
- #            aa=np.where(z > threshold)        
+
              reg = RANSACRegressor(random_state=0,min_samples=min_samples, max_trials=1000,residual_threshold =1).fit(X,y)
- #            score_diff_from_1= 1-reg.score(X, y)
+
              x_axis_1=np.arange(0,512).reshape(-1, 1)
              y_axis_1 = reg.predict(x_axis_1)            
-             #############################################       
- #            coefficients = np.polyfit(non_zero_cord[1], non_zero_cord[0], 1)
- #            polynomial = np.poly1d(coefficients)
- #            x_axis_1 = np.linspace(0,511,512)
- #            y_axis_1 = polynomial(x_axis_1)
+
              font = cv2.FONT_HERSHEY_SIMPLEX
              cv2.putText(slice_3_layer, os.path.basename(gray_file).split(".nii")[0]+ "_SLICE_"+str(xx), (1,500), font, 0.5, (0, 255, 0), 1, cv2.LINE_AA)
              cv2.putText(slice_3_layer,"Ideal Midline", (200,20), font, 0.5, (0, 255, 0), 1, cv2.LINE_AA)
@@ -105,10 +86,4 @@ def fit_line_to_midlinepixels_ORF_sh():
              # latex_start_tableNc_noboundary(latexfilename,1)
              image_list=[]
              image_list.append(imagefilename)
-             # latex_insertimage_tableNc(latexfilename,image_list,1, caption=os.path.basename(gray_file).split(".nii")[0],imagescale=0.5, angle=90,space=1)
-             
-             # latex_start_table1c(latexfilename)
-             # latex_insertimage_table1c(latexfilename,image1=imagefilename,caption= os.path.basename(gray_file).split(".nii")[0],imagescale=0.5)
-             # latex_end_table2c(latexfilename)
-             # latex_insert_line_nodate(latexfilename, os.path.basename(gray_file).split(".nii")[0] + "_SLICE_"+str(xx) )
-     # latex_end(latexfilename)
+

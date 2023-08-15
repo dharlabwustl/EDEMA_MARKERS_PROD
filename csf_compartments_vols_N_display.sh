d7db2df230a17479171d38724fb929b6b8bc774d
dharlabwustl <atulkumar@wustl.edu>
2023-08-15 12:15:16 -0500

    no comment

--- csf_compartments_vols_N_display.sh
+++ csf_compartments_vols_N_display.sh
@@ -877,7 +877,7 @@ for grayscale_filename in ${working_dir_1}/*.nii*; do
       suffix=${y##*_}
       images[$i]=${x} ##{output_directory}/SAH_1_01052014_2003_2_GRAY_031.jpg
       i=$(($i + 1))
-      images[$i]=${output_directory}/${grayscale_filename_basename_noext}_resaved_levelset_GRAY_${suffix}.jpg
+      images[$i]=${output_directory}/${grayscale_filename_basename_noext}_resaved_levelset_GRAY_CSF_${suffix}.jpg
       i=$(($i + 1))
       images[$i]=${output_directory}/${grayscale_filename_basename_noext}_resaved_levelset_GRAY_${suffix}.jpg
       i=$(($i + 1))

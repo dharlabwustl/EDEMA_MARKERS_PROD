475269a9f40d18b007b77766154aff8237e0393e
dharlabwustl <atulkumar@wustl.edu>
2023-08-15 18:46:15 -0500

    no comment

--- utilities_simple_trimmed.py
+++ utilities_simple_trimmed.py
@@ -800,13 +800,12 @@ def write_panda_df(latexfilename,table_df):
     return
 def call_write_panda_df(args):
     table_df=pd.read_csv(args.stuff[1])
-    table_df['FILENAME']=os.path.basename(args.stuff[1])
+    table_df['FILENAME']=os.path.basename(args.stuff[1]).split('.csv')[0]
     column_to_move = table_df.pop('FILENAME')
     table_df.insert(0, 'FILENAME', column_to_move)
     table_df1=table_df.unstack().reset_index()
     table_df1.columns=["Region","IDX","Volume"]
-    table_df1.drop(["IDX"],axis=1)
-
+    table_df1=table_df1.drop(["IDX"],axis=1)
     latexfilename=args.stuff[2]
     write_panda_df(latexfilename,table_df1)
     return

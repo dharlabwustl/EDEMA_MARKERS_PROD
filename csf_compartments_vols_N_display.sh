ad30aab64d04250ace8858ef43e3921a41519995
dharlabwustl <atulkumar@wustl.edu>
2023-08-23 09:29:26 -0500

    no comment

--- csf_compartments_vols_N_display.sh
+++ csf_compartments_vols_N_display.sh
@@ -1050,7 +1050,7 @@ while IFS=',' read -ra array; do
     for x in ${working_dir}/${grayscale_filename_basename_noext}*.jpg; do #_resaved_levelset_GRAY
 
       #              filename=args.stuff[1]
-      imagescale='0.25' #float(args.stuff[2])
+      imagescale='0.18' #float(args.stuff[2])
       angle='90'        #float(args.stuff[3])
       space='1'         #float(args.stuff[4])
       i=0

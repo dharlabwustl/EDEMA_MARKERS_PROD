11fcd6338d59270d931ecd36fc8ac288a7f39574
dharlabwustl <atulkumar@wustl.edu>
2023-09-14 17:04:50 -0500

    no comment

--- utilities_simple_trimmed.py
+++ utilities_simple_trimmed.py
@@ -1326,16 +1326,16 @@ def latex_insertimage_tableNc(filename,images,N, caption="ATUL",imagescale=0.5, 
     file1.writelines("\\vspace{" + str(space)+"em}\n")
     return file1
 
-def latex_inserttext_tableNc_colored(filename,texts,N,space=1):
+def latex_inserttext_tableNc_colored(filename,texts,text_colors,N,space=1):
     file1 = open(filename,"a")
     # file1.writelines("\\vspace{-2em}\n")
 
     for x in range(N):
         if x < N-1:
-            file1.writelines("\\textbf{\\textcolor{blue}{" + texts[x] + "}}\n")
+            file1.writelines("\\textbf{\\textcolor{"+ text_colors[x]+"}{" + texts[x] + "}}\n")
             file1.writelines("&")
         else:
-            file1.writelines("\\textbf{\\textcolor{blue}{" + texts[x] + "}}\n")
+            file1.writelines("\\textbf{\\textcolor{"+ text_colors[x]+"}{" + texts[x] + "}}\n")
 
     #    file1.writelines("\\includegraphics[width=" + str(imagescale) + "\\textwidth]{"+  image2 + "}\n")
     file1.writelines("\\vspace{" + str(space)+"em}\n")
@@ -1369,12 +1369,13 @@ def call_latex_inserttext_tableNc(args):
         # imagescale=float(args.stuff[2])
         # angle=float(args.stuff[3])
         # space=float(args.stuff[4])
-        texts=args.stuff[2:]
+        text_color_list=args.stuff[2].split('_')
+        texts=args.stuff[3:]
         N=len(texts)
         colsize=(1/N)
         # latex_start_tableNc_noboundary(filename,N)
         latex_start_tableNc_noboundary_withcolsize(filename,N,colsize=colsize)
-        latex_inserttext_tableNc_colored(filename,texts,N,space=1)
+        latex_inserttext_tableNc_colored(filename,texts,text_color_list,N,space=1)
         # latex_inserttext_tableNc(filename,texts,N, caption="NONE",imagescale=imagescale, angle=angle,space=space)
         latex_end_table2c(filename)
         command="echo successful at :: {}::maskfilename::{} >> /software/error.txt".format(inspect.stack()[0][3],'call_latex_inserttext_tableNc')

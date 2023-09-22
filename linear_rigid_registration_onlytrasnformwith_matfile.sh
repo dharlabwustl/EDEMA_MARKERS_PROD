468cfd98f02759075552d956d9e2db15ec259d27
dharlabwustl <atulkumar@wustl.edu>
2023-09-22 11:45:24 -0500

    no comment

--- linear_rigid_registration_onlytrasnformwith_matfile.sh
+++ linear_rigid_registration_onlytrasnformwith_matfile.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 templatefilename=scct_strippedResampled1.nii.gz 
 mask_on_template=midlinecssfResampled1.nii.gz
+template_ventricle_filename=scct_strippedResampled1_onlyventricle.nii.gz
 intensity=0.01
 filename=$1 
 echo " I AM WORKING IN DOCKER"
@@ -10,11 +11,13 @@ output_directory=${output_directory} ###/output #/LINEAR_REGISTRATION_OUTPUT
 echo " I am in LINEAR_REGISTRATION"
 # mkdir -p $output_directory
 template_directory=/templatenifti
+template_ventricle_directory=/templateventricle
 template_file_name=$templatefilename
 # x=$input_directory/$1 
 
 echo $filename
-template_image=$template_directory/$template_file_name 
+template_image=$template_directory/$template_file_name
+template_image_ventricle=$template_ventricle_directory/$template_ventricle_filename
 template_basename="$(basename -- $template_image)"
 exten="${template_basename%%.nii*}"
 echo $template_basename 
@@ -27,7 +30,7 @@ echo $output_filename
 
 /usr/lib/fsl/5.0/flirt -in ${template_image} -ref ${img} -out "${output_filename}${exten}lin1_1" -init ${output_filename}_${exten}lin1_1.mat  -applyxfm
 
-/usr/lib/fsl/5.0/flirt -in ${template_image%.nii*}_onlyventricle.nii.gz -ref ${img} -out "${output_filename}${template_image%.nii*}_onlyventricle_lin1_1" -init ${output_filename}_${exten}lin1_1.mat  -applyxfm
+/usr/lib/fsl/5.0/flirt -in ${template_image_ventricle} -ref ${img} -out "${output_filename}${exten}_onlyventricle_lin1_1" -init ${output_filename}_${exten}lin1_1.mat  -applyxfm
 #/usr/lib/fsl/5.0/flirt -ref  "${img}"  -in "${template_image}"  -dof 12 -out "${output_filename}${exten}lin1_1" -omat ${output_filename}_${exten}lin1_1.mat
 
 echo "IMAGE REGISTERED"

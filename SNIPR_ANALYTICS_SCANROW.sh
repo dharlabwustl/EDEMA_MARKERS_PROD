64452245266a6f09181b104fc460e18b2f4aed7b
dharlabwustl <atulkumar@wustl.edu>
2023-10-09 10:50:59 -0500

    no comment

--- SNIPR_ANALYTICS_SCANROW.sh
+++ SNIPR_ANALYTICS_SCANROW.sh
@@ -71,11 +71,11 @@ outputfiles_present=$(python3 fillmaster_session_list.py "${call_fill_sniprsessi
 call_creat_analytics_onesessionscanasID_arguments=('call_creat_analytics_onesessionscanasID' ${array[1]} ${array[5]} ${scan_analytics}  ${scan_analytics_nofilename})
 outputfiles_present=$(python3 fillmaster_session_list.py "${call_creat_analytics_onesessionscanasID_arguments[@]}")
 #def creat_analytics_onesessionscanasID(sessionId,sessionLabel,csvfilename,csvfilename_withoutfilename)
-#counter=$((counter + 1))
+counter=$((counter + 1))
 fi
-#if [ $counter -eq 7 ] ; then
-#  break
-#fi
+if [ $counter -eq 7 ] ; then
+  break
+fi
 done < <(tail -n +2 "${sessions_list}")
 
 call_edit_scan_analytics_file_arguments=('call_edit_scan_analytics_file'  ${scan_analytics}  ${scan_analytics_nofilename})

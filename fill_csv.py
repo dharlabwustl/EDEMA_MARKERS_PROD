638704ffe64fbf08011ceba6f7077ef31d9ed5be
dharlabwustl <atulkumar@wustl.edu>
2023-10-18 13:58:54 -0500

    no comment

--- fill_csv.py
+++ fill_csv.py
@@ -13,6 +13,20 @@ import argparse
 # sys.path.append('/media/atul/WDJan2022/WASHU_WORKS/PROJECTS/DOCKERIZE/NWU/PYCHARM/EDEMA_MARKERS_PROD');
 from utilities_simple import *
 from download_with_session_ID import *
+def make_identifier_column(csvfilename_input,csvfilename_output,output_column_name):
+    returnvalue=0
+    try:
+        csvfilename_input_df=pd.read_csv(csvfilename_input)
+        csvfilename_input_df[output_column_name]="ATUL" #csvfilename_input_df[columns_list_tocombine[0]].astype(str)
+        # for x in range(len(columns_list_tocombine)):
+        #     if x>0:
+        #         csvfilename_input_df[output_column_name]=csvfilename_input_df[output_column_name].astype(str) + "_"+ csvfilename_input_df[columns_list_tocombine[x]].astype(str)
+        csvfilename_input_df.to_csv(csvfilename_output,index=False)
+        subprocess.call("echo " + "passed at ::{}  >> /workingoutput/error.txt".format(inspect.stack()[0][3]) ,shell=True )
+        returnvalue=1
+    except:
+        subprocess.call("echo " + "passed at ::{}  >> /workingoutput/error.txt".format(inspect.stack()[0][3]) ,shell=True )
+    return returnvalue
 def create_empty_csvfile(csvfilename):
     returnvalue=0
     # print("I AM AT ::{}".format(inspect.stack()[0][3]))
@@ -36,7 +50,20 @@ def call_create_empty_csvfile(args):
         print("I FAILED AT ::{}".format(inspect.stack()[0][3]))
         pass
     return  returnvalue
+def call_make_identifier_column(args):
 
+    returnvalue=0
+    # print("I AM AT ::{}".format(inspect.stack()[0][3]))
+    try:
+        csvfilename_input=args.stuff[1]
+        csvfilename_output=args.stuff[2]
+        output_column_name=args.stuff[3]
+        make_identifier_column(csvfilename_input,csvfilename_output,output_column_name)
+        print("I PASSED AT ::{}".format(inspect.stack()[0][3]))
+    except:
+        print("I FAILED AT ::{}".format(inspect.stack()[0][3]))
+        pass
+    return  returnvalue
 
 def main():
     parser = argparse.ArgumentParser()

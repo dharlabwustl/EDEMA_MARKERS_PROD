664b3adad06fa9e05e0b05f388440f232f865d29
dharlabwustl <atulkumar@wustl.edu>
2023-10-19 15:06:12 -0500

    no comment

--- ALL_PROJECTS_COMBINED_ANALYTICS.sh
+++ ALL_PROJECTS_COMBINED_ANALYTICS.sh
@@ -84,6 +84,7 @@ for x in $(seq 0 1 $((arguments_count - 1))); do
       file_path_csv=${dir_to_receive_the_data}/${project_ID}"_${resource_dir}_resultfilepath.csv"
       get_latest_filepath_from_metadata_arguments=('get_latest_filepath_from_metadata' ${URI} ${resource_dir} ".csv" "sessions_ANALYTICS_20231003124834" ${file_path_csv})
       outputfiles_present=$(python3 fill_csv.py "${get_latest_filepath_from_metadata_arguments[@]}")
+      download_a_single_file ${file_path_csv} ${dir_to_receive_the_data}
 
     elif [ ${project_ID} == "MGBBMC" ]; then
       resource_dir="EDEMA_BIOMARKER_TEST"
@@ -95,6 +96,7 @@ for x in $(seq 0 1 $((arguments_count - 1))); do
       file_path_csv=${dir_to_receive_the_data}/${project_ID}"_${resource_dir}_resultfilepath.csv"
       get_latest_filepath_from_metadata_arguments=('get_latest_filepath_from_metadata' ${URI} ${resource_dir} ".csv" "sessions_ANALYTICS_20231009173614" ${file_path_csv})
       outputfiles_present=$(python3 fill_csv.py "${get_latest_filepath_from_metadata_arguments[@]}")
+      download_a_single_file ${file_path_csv} ${dir_to_receive_the_data}
     elif [ ${project_ID} == "ICH" ]; then
       resource_dir="ICH_QUANTIFICATION"
       file_path_csv=${dir_to_receive_the_data}/${project_ID}"_${resource_dir}_resultfilepath.csv"
@@ -105,7 +107,7 @@ for x in $(seq 0 1 $((arguments_count - 1))); do
       file_path_csv=${dir_to_receive_the_data}/${project_ID}"_${resource_dir}_resultfilepath.csv"
       get_latest_filepath_from_metadata_arguments=('get_latest_filepath_from_metadata' ${URI} ${resource_dir} ".csv" "ICH_CTSESSIONS_202305222109" ${file_path_csv})
       outputfiles_present=$(python3 fill_csv.py "${get_latest_filepath_from_metadata_arguments[@]}")
-
+      download_a_single_file ${file_path_csv} ${dir_to_receive_the_data}
     fi
 
     #    resource_dir="SNIPR_ANALYTICS_TEST"

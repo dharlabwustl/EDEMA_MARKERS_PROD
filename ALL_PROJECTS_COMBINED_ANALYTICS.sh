b36ac586b981285c26cc4982cf33bc21d8b6c5b1
dharlabwustl <atulkumar@wustl.edu>
2023-10-20 12:43:56 -0500

    no comment

--- ALL_PROJECTS_COMBINED_ANALYTICS.sh
+++ ALL_PROJECTS_COMBINED_ANALYTICS.sh
@@ -74,22 +74,23 @@ for x in $(seq 0 1 $((arguments_count - 1))); do
     time_now=$(date -dnow +%Y%m%d%H%M%S)
     URI="/data/projects/"${project_ID}
     dir_to_receive_the_data=${output_directory}
+    analytics_file_dir=${working_dir}
     if [ ${project_ID} == "COLI" ]; then
       resource_dir="EDEMA_BIOMARKER_TEST"
-      file_path_csv=${dir_to_receive_the_data}/${project_ID}"_${resource_dir}_resultfilepath.csv"
+      file_path_csv=${analytics_file_dir}/${project_ID}"_${resource_dir}_resultfilepath.csv"
       get_latest_filepath_from_metadata_arguments=('get_latest_filepath_from_metadata' ${URI} ${resource_dir} ".csv" "COLI_EDEMA_BIOMARKERS_COMBINED_20231003124834" ${file_path_csv})
       outputfiles_present=$(python3 system_analysis.py "${get_latest_filepath_from_metadata_arguments[@]}")
       echo ${outputfiles_present}
       download_a_single_file ${file_path_csv} ${dir_to_receive_the_data}
       resource_dir="SNIPR_ANALYTICS_TEST"
-      file_path_csv=${dir_to_receive_the_data}/${project_ID}"_${resource_dir}_resultfilepath.csv"
+      file_path_csv=${analytics_file_dir}/${project_ID}"_${resource_dir}_resultfilepath.csv"
       get_latest_filepath_from_metadata_arguments=('get_latest_filepath_from_metadata' ${URI} ${resource_dir} ".csv" "sessions_ANALYTICS_20231003124834" ${file_path_csv})
       outputfiles_present=$(python3 system_analysis.py "${get_latest_filepath_from_metadata_arguments[@]}")
-      download_a_single_file ${file_path_csv} ${dir_to_receive_the_data} ${project_ID}
+      download_a_single_file ${file_path_csv} ${analytics_file_dir} ${project_ID}
 
     elif [ ${project_ID} == "MGBBMC" ]; then
       resource_dir="EDEMA_BIOMARKER_TEST"
-      file_path_csv=${dir_to_receive_the_data}/${project_ID}"_${resource_dir}_resultfilepath.csv"
+      file_path_csv=${analytics_file_dir}/${project_ID}"_${resource_dir}_resultfilepath.csv"
       get_latest_filepath_from_metadata_arguments=('get_latest_filepath_from_metadata' ${URI} ${resource_dir} ".csv" "MGBBMC_EDEMA_BIOMARKERS_COMBINED_20231009173614_modified_20231016200619" ${file_path_csv})
       outputfiles_present=$(python3 system_analysis.py "${get_latest_filepath_from_metadata_arguments[@]}")
       download_a_single_file ${file_path_csv} ${dir_to_receive_the_data}
@@ -97,7 +98,7 @@ for x in $(seq 0 1 $((arguments_count - 1))); do
       file_path_csv=${dir_to_receive_the_data}/${project_ID}"_${resource_dir}_resultfilepath.csv"
       get_latest_filepath_from_metadata_arguments=('get_latest_filepath_from_metadata' ${URI} ${resource_dir} ".csv" "sessions_ANALYTICS_20231009173614" ${file_path_csv})
       outputfiles_present=$(python3 system_analysis.py "${get_latest_filepath_from_metadata_arguments[@]}")
-      download_a_single_file ${file_path_csv} ${dir_to_receive_the_data} ${project_ID}
+      download_a_single_file ${file_path_csv} ${analytics_file_dir} ${project_ID}
     elif [ ${project_ID} == "ICH" ]; then
       resource_dir="ICH_QUANTIFICATION"
       file_path_csv=${dir_to_receive_the_data}/${project_ID}"_${resource_dir}_resultfilepath.csv"
@@ -108,7 +109,7 @@ for x in $(seq 0 1 $((arguments_count - 1))); do
       file_path_csv=${dir_to_receive_the_data}/${project_ID}"_${resource_dir}_resultfilepath.csv"
       get_latest_filepath_from_metadata_arguments=('get_latest_filepath_from_metadata' ${URI} ${resource_dir} ".csv" "ICH_CTSESSIONS_202305222109" ${file_path_csv})
       outputfiles_present=$(python3 system_analysis.py "${get_latest_filepath_from_metadata_arguments[@]}")
-      download_a_single_file ${file_path_csv} ${dir_to_receive_the_data} ${project_ID}
+      download_a_single_file ${file_path_csv} ${analytics_file_dir} ${project_ID}
     fi
 
 

7d2633a9e9dad680135c6e536b0b7e9733041abf
dharlabwustl <atulkumar@wustl.edu>
2023-10-20 11:21:06 -0500

    no comment

--- SNIPR_ANALYTICS_SCANROW_SAH.sh
+++ SNIPR_ANALYTICS_SCANROW_SAH.sh
@@ -73,9 +73,10 @@ copy_session=${sessions_list%.csv}_ANALYTICS_${time_now}.csv
 download_a_single_file ${file_path_csv} ${dir_to_receive_the_data} ${project_ID} ${copy_session}
   while IFS=',' read -ra array; do
     echo array::${array[3]}
-
-#    local get_latest_filepath_from_metadata_arguments=('download_a_singlefile_with_URIString' ${array[0]} ${output_filename} ${dir_to_save})
-#    local outputfiles_present=$(python3 system_analysis.py "${get_latest_filepath_from_metadata_arguments[@]}")
+    n=${#${array[3]}}
+    echo ${n}
+     get_latest_filepath_from_metadata_arguments=('download_a_singlefile_with_URIString' ${array[0]} ${output_filename} ${dir_to_save})
+     outputfiles_present=$(python3 system_analysis.py "${get_latest_filepath_from_metadata_arguments[@]}")
   done < <(tail -n +2 "${copy_session}")
 #scan_analytics=${sessions_list%sessions.csv}SCAN_ANALYTICS_${time_now}.csv
 #scan_analytics_nofilename=${sessions_list%sessions.csv}SCAN_ANALYTICS_NOFILENAME${time_now}.csv

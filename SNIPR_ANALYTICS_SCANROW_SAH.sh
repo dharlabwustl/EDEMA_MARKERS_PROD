b1a27273bb50a8ef6014e4a001c28be39886aac2
dharlabwustl <atulkumar@wustl.edu>
2023-10-20 11:44:20 -0500

    no comment

--- SNIPR_ANALYTICS_SCANROW_SAH.sh
+++ SNIPR_ANALYTICS_SCANROW_SAH.sh
@@ -91,8 +91,8 @@ while IFS=',' read -ra array; do
   n_csvfilename_length=${#csv_file_location}
   echo ${n_csvfilename_length}
   if [ ${n_csvfilename_length} -gt 1 ]; then
-    output_filename=$(basename ${csv_file_location})
-    get_latest_filepath_from_metadata_arguments=('download_a_singlefile_with_URIString' ${csv_file_location} ${output_filename} ${dir_to_save})
+    csv_output_filename=$(basename ${csv_file_location})
+    get_latest_filepath_from_metadata_arguments=('download_a_singlefile_with_URIString' ${csv_file_location} ${csv_output_filename} ${dir_to_save})
     outputfiles_present=$(python3 system_analysis.py "${get_latest_filepath_from_metadata_arguments[@]}")
 #    copysinglefile_to_sniprproject  ${project_ID}  "${dir_to_save}"  ${resource_dirname_at_snipr}  ${output_filename}
 #    counter=$((counter + 1))

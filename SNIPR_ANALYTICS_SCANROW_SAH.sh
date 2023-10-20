5825354cf182dd73ba6d9f49d4dc862cebf02cc5
dharlabwustl <atulkumar@wustl.edu>
2023-10-20 12:04:26 -0500

    no comment

--- SNIPR_ANALYTICS_SCANROW_SAH.sh
+++ SNIPR_ANALYTICS_SCANROW_SAH.sh
@@ -107,7 +107,7 @@ csvfile_list="${working_dir}/CSV_FILENAMES_LIST.csv"
 echo "CSV_FILENAMES" > ${csvfile_list}
 for eachfilename in ${dir_to_save}/*.csv ; do echo $eachfilename >> ${csvfile_list} ; done
 combined_metrics_results="${working_dir}/COMBINED_SESSIONS_SAH_METRICS_${time_now}.csv"
-combinecsvsfiles_from_a_csv_containing_its_list_arguments=('combinecsvsfiles_from_a_csv_containing_its_list' ${csvfile_list} ${csv_output_filename} ${dir_to_save})
+combinecsvsfiles_from_a_csv_containing_its_list_arguments=('combinecsvsfiles_from_a_csv_containing_its_list' ${csvfile_list} ${combined_metrics_results} )
 outputfiles_present=$(python3 system_analysis.py "${combinecsvsfiles_from_a_csv_containing_its_list_arguments[@]}")
 ##scan_analytics=${sessions_list%sessions.csv}SCAN_ANALYTICS_${time_now}.csv
 ##scan_analytics_nofilename=${sessions_list%sessions.csv}SCAN_ANALYTICS_NOFILENAME${time_now}.csv

f0677ebf42b0a1573983341d316de76e6559f315
dharlabwustl <atulkumar@wustl.edu>
2023-10-20 13:32:13 -0500

    no comment

--- system_analysis.py
+++ system_analysis.py
@@ -49,7 +49,7 @@ def histogram_column_ina_csvfile(args):
     output_image_name=args.stuff[3]
     df=pd.read_csv(csvfilename)
     ###################
-    df.hist(column=column_name)
+    df.hist(column=column_name, bins=25, grid=False, figsize=(12,8), color='#86bf91', zorder=2, rwidth=0.9)
     # ax = df.plot().hist(column=column_name, bins=25, grid=False, figsize=(12,8), color='#86bf91', zorder=2, rwidth=0.9)
     # ax.get_figure().savefig(output_image_name)
     # ax = ax[0]

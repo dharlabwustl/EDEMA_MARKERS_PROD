ce8348fb35d5f7836e6744b960bd1b4d544ba594
dharlabwustl <atulkumar@wustl.edu>
2023-10-20 12:28:09 -0500

    no comment

--- SNIPR_ANALYTICS_SCANROW_SAH.sh
+++ SNIPR_ANALYTICS_SCANROW_SAH.sh
@@ -95,8 +95,6 @@ while IFS=',' read -ra array; do
     csv_output_filename=$(basename ${csv_file_location})
     get_latest_filepath_from_metadata_arguments=('download_a_singlefile_with_URIString' ${csv_file_location} ${csv_output_filename} ${dir_to_save})
     outputfiles_present=$(python3 system_analysis.py "${get_latest_filepath_from_metadata_arguments[@]}")
-#    copysinglefile_to_sniprproject  ${project_ID}  "${dir_to_save}"  ${resource_dirname_at_snipr}  ${output_filename}
-#    counter=$((counter + 1))
   fi
 
   if [ $counter -eq 2 ]; then
@@ -112,60 +110,3 @@ combinecsvsfiles_from_a_csv_containing_its_list_arguments=('combinecsvsfiles_fro
 outputfiles_present=$(python3 system_analysis.py "${combinecsvsfiles_from_a_csv_containing_its_list_arguments[@]}")
 resource_dirname_at_snipr='SAH_RESULTS_CSV'
 copysinglefile_to_sniprproject  ${project_ID}  "${working_dir}"  ${resource_dirname_at_snipr}  $(basename ${combined_metrics_results})
-##scan_analytics=${sessions_list%sessions.csv}SCAN_ANALYTICS_${time_now}.csv
-##scan_analytics_nofilename=${sessions_list%sessions.csv}SCAN_ANALYTICS_NOFILENAME${time_now}.csv
-##curl -u $XNAT_USER:$XNAT_PASS -X GET $XNAT_HOST/data/projects/${project_ID}/experiments/?format=csv >${sessions_list}
-##cp ${sessions_list} ${copy_session}
-##counter=0
-##while IFS=',' read -ra array; do
-##xx=0
-##
-###if [ ${array[1]} == "SNIPR01_E00894" ]  ; then
-###  echo "${array[1]}"
-###  echo "${array[5]}"
-##if [ ${array[4]} == "xnat:ctSessionData" ] ; then
-##    echo "${array[1]}"
-##    echo "${array[5]}"
-##call_fill_sniprsession_list_arguments=('call_fill_sniprsession_list' ${copy_session} ${array[1]} ) ##
-#### ${working_dir}/${project_ID}_SNIPER_ANALYTICS.csv  ${project_ID} ${output_directory} )
-##outputfiles_present=$(python3 fillmaster_session_list.py "${call_fill_sniprsession_list_arguments[@]}")
-##call_creat_analytics_onesessionscanasID_arguments=('call_creat_analytics_onesessionscanasID' ${array[1]} ${array[5]} ${scan_analytics}  ${scan_analytics_nofilename})
-##outputfiles_present=$(python3 fillmaster_session_list.py "${call_creat_analytics_onesessionscanasID_arguments[@]}")
-###def creat_analytics_onesessionscanasID(sessionId,sessionLabel,csvfilename,csvfilename_withoutfilename)
-##counter=$((counter + 1))
-##fi
-##if [ $counter -eq 7 ] ; then
-##  break
-##fi
-##done < <(tail -n +2 "${sessions_list}")
-#
-##call_edit_scan_analytics_file_arguments=('call_edit_scan_analytics_file'  ${scan_analytics}  ${scan_analytics_nofilename})
-##outputfiles_present=$(python3 fillmaster_session_list.py "${call_edit_scan_analytics_file_arguments[@]}")
-#
-#X_level="projects"
-#level_name=${project_ID}
-#dir_to_save=${working_dir}
-#resource_dirname_at_snipr="SAH_RESULTS_PDF"
-##call_upload_pdfs_arguments=('call_upload_pdfs'  ${scan_analytics}  ${X_level} ${level_name} ${dir_to_save} ${resource_dirname_at_snipr} )
-##outputfiles_present=$(python3 fillmaster_session_list.py "${call_upload_pdfs_arguments[@]}")
-#
-#masterfile_scans=${copy_session}     #args.stuff[1]
-#column_name_for_url="PDF_FILE_PATH"  #args.stuff[2]
-#column_name_for_session_name='label' #args.stuff[3]
-#file_extension='.pdf'                #args.stuff[4]
-##
-##call_upload_pdfs_arguments=('download_then_upload_files_withurl_from_a_csvfile'  ${masterfile_scans}  ${column_name_for_url} ${column_name_for_session_name} ${file_extension} ${X_level} ${level_name} ${dir_to_save} ${resource_dirname_at_snipr} )
-##outputfiles_present=$(python3 system_analysis.py "${call_upload_pdfs_arguments[@]}")
-#############################
-##outputfilename=${project_ID}_EDEMA_BIOMARKERS_COMBINED_${time_now}.csv
-##call_download_csvs_combine_upload_v1_arguments=('call_download_csvs_combine_upload_v1'  ${scan_analytics}  ${sessions_list} ${dir_to_save} ${outputfilename} )
-##outputfiles_present=$(python3 fillmaster_session_list.py "${call_download_csvs_combine_upload_v1_arguments[@]}")
-##
-##copysinglefile_to_sniprproject  ${project_ID}  "${dir_to_save}"  ${resource_dirname_at_snipr}  ${outputfilename}
-##resource_dirname_at_snipr="SNIPR_ANALYTICS_TEST"
-##call_edit_session_analytics_file_arguments=('call_edit_session_analytics_file'   ${copy_session} )
-##outputfiles_present=$(python3 fillmaster_session_list.py "${call_edit_session_analytics_file_arguments[@]}")
-##
-##copysinglefile_to_sniprproject  ${project_ID}  "${dir_to_save}"  ${resource_dirname_at_snipr}  $(basename ${scan_analytics} )
-##copysinglefile_to_sniprproject  ${project_ID}  "${dir_to_save}"  ${resource_dirname_at_snipr}  $(basename ${scan_analytics_nofilename} )
-##copysinglefile_to_sniprproject  ${project_ID}  "${dir_to_save}"  ${resource_dirname_at_snipr}  $(basename ${copy_session} )

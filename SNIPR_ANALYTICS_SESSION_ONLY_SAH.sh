4de8e70b812ac219aeba7f2548e849cd860c3b6e
dharlabwustl <atulkumar@wustl.edu>
2023-10-23 16:20:07 -0500

    no comment

--- SNIPR_ANALYTICS_SESSION_ONLY_SAH.sh
+++ SNIPR_ANALYTICS_SESSION_ONLY_SAH.sh
@@ -71,11 +71,11 @@ while IFS=',' read -ra array; do
     #call_creat_analytics_onesessionscanasID_arguments=('call_creat_analytics_onesessionscanasID' ${array[1]} ${array[5]} ${scan_analytics}  ${scan_analytics_nofilename})
     #outputfiles_present=$(python3 fillmaster_session_list.py "${call_creat_analytics_onesessionscanasID_arguments[@]}")
     #def creat_analytics_onesessionscanasID(sessionId,sessionLabel,csvfilename,csvfilename_withoutfilename)
-#    counter=$((counter + 1))
+    counter=$((counter + 1))
   fi
-#  if [ $counter -eq 2 ]; then
-#    break
-#  fi
+  if [ $counter -eq 2 ]; then
+    break
+  fi
 done < <(tail -n +2 "${sessions_list}")
 
 dir_to_save=${working_dir}
@@ -86,22 +86,22 @@ outputfiles_present=$(python3 fillmaster_session_list.py "${call_edit_session_an
 ##############################
 #def add_file_size(args):
 time_now=$(date -dnow +%Y%m%d%H%M%S)
-copy_session_1=${sessions_list%.csv}_${project_ID}_ANALYTICS_${time_now}.csv
+#copy_session_1=${sessions_list%.csv}_${project_ID}_ANALYTICS_${time_now}.csv
 #cp ${copy_session} ${copy_session_1}
-while IFS=',' read -ra array; do
-  file_url=${array[3]}
-  if [[ ${file_url} == *".pdf"* ]]; then
-    session_ID=${array[0]}
-    echo session_ID::${session_ID}
-    echo file_url::${file_url}
-    csvfilename=${copy_session}
-    echo csvfilename::${csvfilename}
-    temp_dir=${working_dir}
-    echo temp_dir::${temp_dir}
-    call_edit_session_analytics_file_arguments=('add_file_size' ${session_ID} ${file_url} ${csvfilename} "PDF_FILE_SIZE" ${temp_dir})
-    outputfiles_present=$(python3 fillmaster_session_list.py "${call_edit_session_analytics_file_arguments[@]}")
-  fi
-done < <(tail -n +2 "${copy_session}")
+#while IFS=',' read -ra array; do
+#  file_url=${array[3]}
+#  if [[ ${file_url} == *".pdf"* ]]; then
+#    session_ID=${array[0]}
+#    echo session_ID::${session_ID}
+#    echo file_url::${file_url}
+#    csvfilename=${copy_session}
+#    echo csvfilename::${csvfilename}
+#    temp_dir=${working_dir}
+#    echo temp_dir::${temp_dir}
+#    call_edit_session_analytics_file_arguments=('add_file_size' ${session_ID} ${file_url} ${csvfilename} "PDF_FILE_SIZE" ${temp_dir})
+#    outputfiles_present=$(python3 fillmaster_session_list.py "${call_edit_session_analytics_file_arguments[@]}")
+#  fi
+#done < <(tail -n +2 "${copy_session}")
 ##############################
 
 copysinglefile_to_sniprproject ${project_ID} "${dir_to_save}" ${resource_dirname_at_snipr} $(basename ${copy_session})

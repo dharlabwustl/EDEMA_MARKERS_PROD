df32432b2518dde97cb79d3a530bb61ed0a07c6d
dharlabwustl <atulkumar@wustl.edu>
2023-10-24 13:10:10 -0500

    no comment

--- system_analysis.py
+++ system_analysis.py
@@ -51,7 +51,7 @@ def histogram_column_ina_csvfile(args):
     df.columns=df.columns.str.replace(' ','_')
     non_zero_items=df[df[str(column_name)]>=df[str(column_name)].min()]
     ###################
-    ax = df[str(column_name)].plot.hist(bins=12, alpha=0.5)
+    ax = df[str(column_name)].plot.hist(bins=12, alpha=0.5,color = "skyblue", lw=0)
     # ax = df.hist(column=column_name, bins=25, grid=False, figsize=(12,8), color='#86bf91', zorder=2, rwidth=0.9)
     # # ax = s.hist()  # s is an instance of Series
     ax.set_xlabel(str(column_name).replace("_"," "))

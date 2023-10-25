9cf76e8e4a8950ff4e63acb89dcf9af1ab59fe13
dharlabwustl <atulkumar@wustl.edu>
2023-10-24 21:05:45 -0500

    no comment

--- system_analysis.py
+++ system_analysis.py
@@ -51,7 +51,7 @@ def histogram_column_ina_csvfile(args):
         df=pd.read_csv(csvfilename)
         df.columns=df.columns.str.strip() #(' ','')
         df.columns=df.columns.str.replace(' ','_')
-        # non_zero_items=df[df[str(column_name)]>=df[str(column_name)].min()]
+        non_zero_items=df[df[str(column_name)]>=df[str(column_name)].min()]
         # ##################
         # ax = df[str(column_name)].plot.hist(bins=12, alpha=0.5,color = "blueviolet", lw=0)
         # # # ax = df.hist(column=column_name, bins=25, grid=False, figsize=(12,8), color='#86bf91', zorder=2, rwidth=0.9)

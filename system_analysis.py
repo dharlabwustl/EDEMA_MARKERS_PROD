bd6ae0be38b33731f712c6343b4e28dfe4ebc22b
dharlabwustl <atulkumar@wustl.edu>
2023-10-27 13:36:00 -0500

    no comment

--- system_analysis.py
+++ system_analysis.py
@@ -26,9 +26,10 @@ def combinecsvs_with_a_given_suffix(args):
             merged_df.loc[merged_df[list(merged_df.columns)[0]]==df[list(df.columns)[0]][0], list(df.columns)[1]] = df[list(df.columns)[1]][0]
         else:
             merged_df=pd.concat([merged_df,df ], ignore_index=True)
-    combined_csv = merged_df.drop_duplicates().T
+    combined_csv = merged_df.drop_duplicates()
     combined_csv.loc[-1] = combined_csv.columns
     combined_csv = combined_csv.sort_index().reset_index(drop=True)
+    combined_csv = merged_df.drop_duplicates().T
     # combined_csv.columns = combined_csv.iloc[0]
     # combined_csv = combined_csv[1:]
     # combined_csv.set_index=list(combined_csv[list(combined_csv.columns)[0]])

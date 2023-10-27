0a6b7f2cbfb73d3a714fda87b3f00d7c32f4d5b2
dharlabwustl <atulkumar@wustl.edu>
2023-10-27 10:13:36 -0500

    no comment

--- system_analysis.py
+++ system_analysis.py
@@ -20,7 +20,10 @@ def combinecsvs_with_a_given_suffix(args):
     all_filenames = [i for i in glob.glob(os.path.join(inputdirectory,'*{}'.format(suffix)))]
     #    os.chdir(inputdirectory)
     #combine all files in the list
-    combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames ], ignore_index=False)
+    combined_csv=pd.read_csv(all_filenames[0])
+    for each_file in all_filenames:
+        combined_csv = pd.merge(combined_csv,pd.read_csv(each_file), how = 'inner', on = 'Key')
+    # combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames ], ignore_index=True)
     combined_csv = combined_csv.drop_duplicates()
     #export to csv
     combined_csv.to_csv(outputfilename, index=False, encoding='utf-8-sig')

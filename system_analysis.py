3ae631c0f3d63ac89c1aeb2c47dd8358e4aa8970
dharlabwustl <atulkumar@wustl.edu>
2023-10-28 15:48:02 -0500

    no comment

--- system_analysis.py
+++ system_analysis.py
@@ -64,11 +64,12 @@ def transpose_a_table(args):
     combined_csv=combined_csv.T
     combined_csv.set_index=list(column_names)
     combined_csv.columns = combined_csv.iloc[0]
+    combined_csv['COMBINED']=combined_csv.sum(axis=1)
     combined_csv = combined_csv[1:]
     combined_csv.index.name = 'DATA TYPE'
     #export to csv
     combined_csv.replace(np.nan, '')
-    combined_csv['COMBINED']=combined_csv.sum(axis=1)
+
     combined_csv.to_csv(outputfilename, index=True, encoding='utf-8-sig')
 def rename_one_column(args):
     csvfilename=args.stuff[1]

e398332691ab4eb470b4272d4fe37ba7120bef94
dharlabwustl <atulkumar@wustl.edu>
2023-11-01 11:33:12 -0500

    no comment

--- system_analysis.py
+++ system_analysis.py
@@ -13,6 +13,49 @@ import argparse
 # sys.path.append('/media/atul/WDJan2022/WASHU_WORKS/PROJECTS/DOCKERIZE/NWU/PYCHARM/EDEMA_MARKERS_PROD');
 from utilities_simple import *
 from download_with_session_ID import *
+def scatter_hist(args): #x, y,output_image_filename): #, ax, ax_histx, ax_histy):
+    csvfilename=args.stuff[1]
+    column_name_1=args.stuff[2]
+    column_name_2=args.stuff[3]
+    output_image_filename=args.stuff[4]
+    df=pd.read_csv(csvfilename)
+    df.columns=df.columns.str.strip() #(' ','')
+    df.columns=df.columns.str.replace(' ','_')
+    x=df[str(column_name_1)]
+    y=df[str(column_name_2)]
+    fig = plt.figure(figsize=(6, 6))
+
+    # Add a gridspec with two rows and two columns and a ratio of 1 to 4 between
+    # the size of the marginal axes and the main axes in both directions.
+    # Also adjust the subplot parameters for a square plot.
+    gs = fig.add_gridspec(2, 2, width_ratios=(4, 1), height_ratios=(1, 4),
+                          left=0.1, right=0.9, bottom=0.1, top=0.9,
+                          wspace=0.05, hspace=0.05)
+    # Create the Axes.
+    ax = fig.add_subplot(gs[1, 0])
+    ax_histx = fig.add_subplot(gs[0, 0], sharex=ax)
+    ax_histy = fig.add_subplot(gs[1, 1], sharey=ax)
+    # Draw the scatter plot and marginals.
+    # scatter_hist(x, y, ax, ax_histx, ax_histy)
+
+    # no labels
+    ax_histx.tick_params(axis="x", labelbottom=False)
+    ax_histy.tick_params(axis="y", labelleft=False)
+
+    # the scatter plot:
+    ax.scatter(x, y,color = '#88c999')
+
+    # now determine nice limits by hand:
+    binwidth = 0.25
+    xymax = max(np.max(np.abs(x)), np.max(np.abs(y)))
+    lim = (int(xymax / binwidth) + 1) * binwidth
+
+    bins = np.arange(-lim, lim + binwidth, binwidth)
+    ax_histx.hist(x, bins=bins, alpha=0.5,color = "blueviolet")
+    ax_histy.hist(y, bins=bins, orientation='horizontal',alpha=0.5,color = "magenta")
+    fig = ax.get_figure()
+
+    fig.savefig(output_image_filename)
 def bar_chart_a_table(args):
     csvfilename=args.stuff[1]
     output_image_name=args.stuff[2]
@@ -202,6 +245,8 @@ def combinecsvsfiles_from_a_csv_containing_its_list(args): #listofcsvfiles_filen
 #     with pd.ExcelWriter('output.xlsx') as writer:
 #         df1.to_excel(writer, sheet_name='Sheet_name_1')
 #         df2.to_excel(writer, sheet_name='Sheet_name_2')
+
+
 def histogram_column_ina_csvfile(args):
     try:
         csvfilename=args.stuff[1]

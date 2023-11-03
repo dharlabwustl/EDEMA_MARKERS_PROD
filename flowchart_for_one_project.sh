861a8dff7557e3e028e8a2ee8a7879fc9687c477
dharlabwustl <atulkumar@wustl.edu>
2023-11-03 12:49:36 -0500

    Flowchart commit

--- flowchart_for_one_project.sh
+++ flowchart_for_one_project.sh
@@ -215,14 +215,14 @@ done
 #outputfiles_present=$(python3 system_analysis.py "${get_sessions_scans_for_pipepline_image_arguments[@]}")
 
 
-#while IFS=',' read -ra array; do
-#sessionId=${array[1]}
-#resource_dirname='DICOM'
-#dir_to_save=${workingoutput}
-#echo ${sessionId}
-#create_images_for_cluster_arguments=('create_images_for_cluster' ${sessionId}  ${dir_to_save} ${sessions_list} )
-#outputfiles_present=$(python3 system_analysis.py "${create_images_for_cluster_arguments[@]}")
-#done < <(tail -n +2 "${eachfilename%.csv}_top10.csv")
+while IFS=',' read -ra array; do
+sessionId=${array[1]}
+resource_dirname='DICOM'
+dir_to_save=${workingoutput}
+echo ${sessionId}
+create_images_for_cluster_arguments=('create_images_for_cluster' ${sessionId}  ${dir_to_save} ${sessions_list} )
+outputfiles_present=$(python3 system_analysis.py "${create_images_for_cluster_arguments[@]}")
+done < <(tail -n +2 "${eachfilename%.csv}_top10.csv")
 
 #time_now=$(date -dnow +%Y%m%d%H%M%S)
 #csvfile_list="${working_dir}/CSV_FILENAMES_LIST.csv"

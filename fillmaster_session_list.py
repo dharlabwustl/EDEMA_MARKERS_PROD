3c742f427dc6aee30d6cb8dc46b322e5aafb1d07
dharlabwustl <atulkumar@wustl.edu>
2023-11-14 15:19:40 -0600

    Flowchart commit

--- fillmaster_session_list.py
+++ fillmaster_session_list.py
@@ -1035,11 +1035,11 @@ def append_sessionxmlinfo_to_analytics(args):
         with open(xmlfile) as fd:
             xmlfile_dict = xmltodict.parse(fd.read())
 
-        # xmlfile_dict = xmltodict.parse(xmlfile)
-        # columnname='scanner'
-        # # columnvalue=""
-        # columnvalue=xmlfile_dict['xnat:CTSession']['xnat:scanner']
-        # fill_datapoint_each_sessionn_1(identifier,columnname,columnvalue,csvfilename)
+        xmlfile_dict = xmltodict.parse(xmlfile)
+        columnname='scanner'
+        # columnvalue=""
+        columnvalue=xmlfile_dict['xnat:CTSession']['xnat:scanner']
+        fill_datapoint_each_sessionn_1(identifier,columnname,columnvalue,csvfilename)
         # columnname='subject_id'
         # columnvalue=""
         # columnvalue_1=subj_listfile_df[subj_listfile_df['ID']==str(xmlfile_dict['xnat:CTSession']['xnat:subject_ID'])].reset_index()

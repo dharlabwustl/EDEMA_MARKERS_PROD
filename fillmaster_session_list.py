f8594336f7224c2891cefcecf61f6aaf56dd7adf
dharlabwustl <atulkumar@wustl.edu>
2023-11-16 13:06:07 -0600

    Flowchart commit

--- fillmaster_session_list.py
+++ fillmaster_session_list.py
@@ -1047,16 +1047,18 @@ def sort_data_first_col_date(args):
         csvfilename_output=args.stuff[2]
         sorting_columns=args.stuff[3:]
         df=pd.read_csv(csvfilename)
-        df[sorting_columns[0]+'_1'] = pd.to_datetime(df[sorting_columns[0]], format='%m/%d/%Y %H:%M')
-        df=df.sort_values(by=[sorting_columns[0]+'_1'])
-        if len(sorting_columns)>2:
-
-            df=df.sort_values(by=[sorting_columns[1:]])
-        else:
-            df=df.sort_values(by=[sorting_columns[1]])
-
-        # df=df.drop(['acquisition_datetime_1'], axis=1)
-        df.to_csv(csvfilename_output,index=False)
+        df_agg = df.groupby(['subject_id'])
+        res = df_agg.apply(lambda x: x.sort_values(by=['acquisition_datetime'],ascending=False))
+        # df[sorting_columns[0]+'_1'] = pd.to_datetime(df[sorting_columns[0]], format='%m/%d/%Y %H:%M')
+        # df=df.sort_values(by=[sorting_columns[0]+'_1'])
+        # if len(sorting_columns)>2:
+        #
+        #     df=df.sort_values(by=[sorting_columns[1:]])
+        # else:
+        #     df=df.sort_values(by=[sorting_columns[1]])
+        #
+        # # df=df.drop(['acquisition_datetime_1'], axis=1)
+        res.to_csv(csvfilename_output,index=False)
         subprocess.call("echo " + "I PASSED AT ::{}  >> /workingoutput/error.txt".format(inspect.stack()[0][3]) ,shell=True )
     except:
         print("I FAILED AT ::{}".format(inspect.stack()[0][3]))

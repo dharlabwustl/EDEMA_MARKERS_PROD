a764bdd236353d42fa1746027371990110cdc8b5
dharlabwustl <atulkumar@wustl.edu>
2023-11-17 10:58:52 -0600

    Flowchart commit

--- fillmaster_session_list.py
+++ fillmaster_session_list.py
@@ -1079,7 +1079,7 @@ def create_subject_id_from_snipr(args):
         for each_session_row_index, each_session_row_row in df_session.iterrows():
             matched_session=session_list_file_df[session_list_file_df["ID"].astype(str)==str(each_session_row_row["ID"])]
             for matched_session_index, matched_session_row in matched_session.iterrows():
-                session_list_file_df[matched_session_index,"subject_id"]=each_subject_row_row["label"]
+                session_list_file_df.loc[matched_session_index,"subject_id"]=each_subject_row_row["label"]
     session_list_file_df.to_csv(session_list_file_output,index=False)
 def create_subject_id(args):
     csvfilename=args.stuff[1]

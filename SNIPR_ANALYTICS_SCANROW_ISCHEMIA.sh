6ca7b9173e51cd31a772f401572490a1c2d1a619
dharlabwustl <atulkumar@wustl.edu>
2023-11-16 20:46:13 -0600

    Flowchart commit

--- SNIPR_ANALYTICS_SCANROW_ISCHEMIA.sh
+++ SNIPR_ANALYTICS_SCANROW_ISCHEMIA.sh
@@ -122,9 +122,9 @@ while IFS=',' read -ra array; do
     #    counter=$((counter + 1))
   fi
 
-  #  if [ $counter -gt 0 ]; then
-  #    break
-  #  fi
+    if [ $counter -gt 0 ]; then
+      break
+    fi
 done < <(tail -n +2 "${copy_session}")
 
 new_analytics_file_prefix=${working_dir}/${project_ID}'_SESSIONS_RESULTS_METRICS'

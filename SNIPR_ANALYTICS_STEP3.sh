c2de0ddc516141cbda225a14dbbfa83b8f11c212
dharlabwustl <atulkumar@wustl.edu>
2023-11-18 20:57:41 -0600

    Flowchart commit

--- SNIPR_ANALYTICS_STEP3.sh
+++ SNIPR_ANALYTICS_STEP3.sh
@@ -85,28 +85,28 @@ while IFS=',' read -ra array; do
   this_session_id=${array[0]}
   n_pdffilename_length=${#pdf_file_location}
   echo ${n_pdffilename_length}
-#  xml_filename=${working_dir}/${this_session_id}.xml
-#  #  def download_an_xmlfile_with_URIString(args): #url,filename,dir_to_save):
-#  #  url_xml='/app/action/XDATActionRouter/xdataction/xml_file/search_element/xnat%3ActSessionData/search_field/xnat%3ActSessionData.ID/search_value/'${this_session_id} #args.stuff[1]
-#  filename_xml=$(basename ${xml_filename})   #args.stuff[2]
-#  dir_to_save_xml=$(dirname ${xml_filename}) #args args.stuff[3]
-#  download_an_xmlfile_with_URIString_arguments=('download_an_xmlfile_with_URIString' ${this_session_id} ${filename_xml} ${dir_to_save_xml})
-#  outputfiles_present=$(python3 download_with_session_ID.py "${download_an_xmlfile_with_URIString_arguments[@]}")
-#  #  curl -u $XNAT_USER:$XNAT_PASS -X GET 'https://snipr.wustl.edu/app/action/XDATActionRouter/xdataction/xml_file/search_element/xnat%3ActSessionData/search_field/xnat%3ActSessionData.ID/search_value/'${this_session_id} >${xml_filename}
-#  session_id=${this_session_id}
-#  xmlfile=${xml_filename}
-#  csvfilename=${copy_session}
-#  subj_listfile=${subject_list}
-#  append_sessionxmlinfo_to_analytics_arguments=('append_sessionxmlinfo_to_analytics' ${session_id} ${xmlfile} ${csvfilename} ${subj_listfile})
-#  outputfiles_present=$(python3 fillmaster_session_list.py "${append_sessionxmlinfo_to_analytics_arguments[@]}")
-#  #  if [ ${n_pdffilename_length} -gt 1 ]; then
-#  #    resource_dirname_at_snipr=${project_ID}'_RESULTS_PDF'
-#  #    output_filename=$(basename ${pdf_file_location})
-#  #    get_latest_filepath_from_metadata_arguments=('download_a_singlefile_with_URIString' ${pdf_file_location} ${output_filename} ${dir_to_save})
-#  #    outputfiles_present=$(python3 system_analysis.py "${get_latest_filepath_from_metadata_arguments[@]}")
-#  #    copysinglefile_to_sniprproject ${project_ID} "${dir_to_save}" ${resource_dirname_at_snipr} ${output_filename}
-#  #    counter=$((counter + 1))
-#  #  fi
+  #  xml_filename=${working_dir}/${this_session_id}.xml
+  #  #  def download_an_xmlfile_with_URIString(args): #url,filename,dir_to_save):
+  #  #  url_xml='/app/action/XDATActionRouter/xdataction/xml_file/search_element/xnat%3ActSessionData/search_field/xnat%3ActSessionData.ID/search_value/'${this_session_id} #args.stuff[1]
+  #  filename_xml=$(basename ${xml_filename})   #args.stuff[2]
+  #  dir_to_save_xml=$(dirname ${xml_filename}) #args args.stuff[3]
+  #  download_an_xmlfile_with_URIString_arguments=('download_an_xmlfile_with_URIString' ${this_session_id} ${filename_xml} ${dir_to_save_xml})
+  #  outputfiles_present=$(python3 download_with_session_ID.py "${download_an_xmlfile_with_URIString_arguments[@]}")
+  #  #  curl -u $XNAT_USER:$XNAT_PASS -X GET 'https://snipr.wustl.edu/app/action/XDATActionRouter/xdataction/xml_file/search_element/xnat%3ActSessionData/search_field/xnat%3ActSessionData.ID/search_value/'${this_session_id} >${xml_filename}
+  #  session_id=${this_session_id}
+  #  xmlfile=${xml_filename}
+  #  csvfilename=${copy_session}
+  #  subj_listfile=${subject_list}
+  #  append_sessionxmlinfo_to_analytics_arguments=('append_sessionxmlinfo_to_analytics' ${session_id} ${xmlfile} ${csvfilename} ${subj_listfile})
+  #  outputfiles_present=$(python3 fillmaster_session_list.py "${append_sessionxmlinfo_to_analytics_arguments[@]}")
+  #  #  if [ ${n_pdffilename_length} -gt 1 ]; then
+  #  #    resource_dirname_at_snipr=${project_ID}'_RESULTS_PDF'
+  #  #    output_filename=$(basename ${pdf_file_location})
+  #  #    get_latest_filepath_from_metadata_arguments=('download_a_singlefile_with_URIString' ${pdf_file_location} ${output_filename} ${dir_to_save})
+  #  #    outputfiles_present=$(python3 system_analysis.py "${get_latest_filepath_from_metadata_arguments[@]}")
+  #  #    copysinglefile_to_sniprproject ${project_ID} "${dir_to_save}" ${resource_dirname_at_snipr} ${output_filename}
+  #  #    counter=$((counter + 1))
+  #  #  fi
   n_csvfilename_length=${#csv_file_location}
   echo ${n_csvfilename_length}
   if [ ${n_csvfilename_length} -gt 1 ]; then
@@ -127,11 +127,10 @@ while IFS=',' read -ra array; do
   fi
 done < <(tail -n +2 "${copy_session}")
 
-
 new_analytics_file_prefix=${working_dir}/${project_ID}'_SESSIONS_RESULTS_METRICS'
 time_now=$(date -dnow +%Y%m%d%H%M%S)
 new_analytics_file=${new_analytics_file_prefix}_${time_now}.csv
-cp ${copy_session}  ${new_analytics_file}
+cp ${copy_session} ${new_analytics_file}
 ##############################EDITING################################
 call_edit_session_analytics_file_arguments=('call_edit_session_analytics_file' ${new_analytics_file})
 outputfiles_present=$(python3 fillmaster_session_list.py "${call_edit_session_analytics_file_arguments[@]}")

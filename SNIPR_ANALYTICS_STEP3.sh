fc607114c2b477191fadd72ca2a49c4843905607
dharlabwustl <atulkumar@wustl.edu>
2023-12-05 11:42:18 -0600

    Flowchart commit

--- SNIPR_ANALYTICS_STEP3.sh
+++ SNIPR_ANALYTICS_STEP3.sh
@@ -82,17 +82,17 @@ while IFS=',' read -ra array; do
   echo ${n_pdffilename_length}
   n_csvfilename_length=${#csv_file_location}
   echo ${n_csvfilename_length}
-  if [ ${n_csvfilename_length} -gt 1 ]; then
-    csv_output_filename=$(basename ${csv_file_location})
-    get_latest_filepath_from_metadata_arguments=('download_a_singlefile_with_URIString' ${csv_file_location} ${csv_output_filename} ${dir_to_save})
-    outputfiles_present=$(python3 system_analysis.py "${get_latest_filepath_from_metadata_arguments[@]}")
-    append_results_to_analytics_arguments=('append_results_to_analytics' ${copy_session} ${dir_to_save}/${csv_output_filename} ${this_session_id} ${copy_session})
-    outputfiles_present=$(python3 fillmaster_session_list.py "${append_results_to_analytics_arguments[@]}")
-    counter=$((counter + 1))
-  fi
-  if [ $counter -gt 10 ]; then
-    break
-  fi
+#  if [ ${n_csvfilename_length} -gt 1 ]; then
+#    csv_output_filename=$(basename ${csv_file_location})
+#    get_latest_filepath_from_metadata_arguments=('download_a_singlefile_with_URIString' ${csv_file_location} ${csv_output_filename} ${dir_to_save})
+#    outputfiles_present=$(python3 system_analysis.py "${get_latest_filepath_from_metadata_arguments[@]}")
+#    append_results_to_analytics_arguments=('append_results_to_analytics' ${copy_session} ${dir_to_save}/${csv_output_filename} ${this_session_id} ${copy_session})
+#    outputfiles_present=$(python3 fillmaster_session_list.py "${append_results_to_analytics_arguments[@]}")
+#    counter=$((counter + 1))
+#  fi
+#  if [ $counter -gt 10 ]; then
+#    break
+#  fi
 done < <(tail -n +2 "${copy_session}")
 #
 #new_analytics_file_prefix=${working_dir}/${project_ID}'_SESSIONS_RESULTS_METRICS'

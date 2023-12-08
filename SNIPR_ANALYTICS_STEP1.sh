13763d01f6de92ec603730b60a787763791f2df6
dharlabwustl <atulkumar@wustl.edu>
2023-12-08 14:42:53 -0600

    Flowchart commit

--- SNIPR_ANALYTICS_STEP1.sh
+++ SNIPR_ANALYTICS_STEP1.sh
@@ -78,7 +78,7 @@ while IFS=',' read -ra array; do
   echo 'add_axial_thin_num'::${session_id}::${csvfilename}::${csvfilename}
   outputfiles_present=$(python3 fillmaster_session_list.py "${add_axial_thin_num_arguments[@]}")
   counter=$((counter + 1))
-    if [ $counter -gt 2 ]; then
+    if [ $counter -gt 1 ]; then
       break
     fi
 done < <(tail -n +2 "${copy_session}")

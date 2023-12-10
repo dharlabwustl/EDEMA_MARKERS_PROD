9f57081ae74ebcfe87b00a98c64c8cc0063c09c9
dharlabwustl <atulkumar@wustl.edu>
2023-12-10 15:52:05 -0600

    Flowchart commit

--- download_with_session_ID.py
+++ download_with_session_ID.py
@@ -1054,7 +1054,7 @@ def uploadfilesfromlistinacsv(urllistfilename,X_level,projectId,resource_dirname
             # print("eachniftifile_ROW::{}".format(row))
             uploadsinglefile_X_level(X_level,projectId,eachniftifile,resource_dirname)
             print("I SUCCEED AT ::{}".format(inspect.stack()[0][3]))
-        return print("ROW::{}".format(row['LOCAL_FILENAME'])) #print("X_level::{}::projectId::{}::eachniftifile::{}::resource_dirname::{}".format(X_level,projectId,eachniftifile,resource_dirname))
+        return 1 #print("ROW::{}".format(row['LOCAL_FILENAME'])) #print("X_level::{}::projectId::{}::eachniftifile::{}::resource_dirname::{}".format(X_level,projectId,eachniftifile,resource_dirname))
     except:
         print("I FAILED AT ::{}".format(inspect.stack()[0][3]))
         pass

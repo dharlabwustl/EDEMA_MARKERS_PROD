a76e6e6047cecb1ee8d587744b2284c7374c6488
dharlabwustl <atulkumar@wustl.edu>
2023-12-12 19:36:56 -0600

    Flowchart commit

--- SNIPR_ANALYTICS_STEP3_REDCAP.py
+++ SNIPR_ANALYTICS_STEP3_REDCAP.py
@@ -206,7 +206,7 @@ for row_id, row in new_analytics_file_df.iterrows():
     if each_col != "subject":
       field_id=each_col
       field_value=row[each_col]
-      if "date_time" in each_col:
+      if "date_time" in each_col and len(str(row[each_col])>6):
         field_value=datetime.datetime.strptime(row[each_col], '%m/%d/%Y %H:%M').strftime('%Y-%m-%d %H:%M')
       record = {
         # 'redcap_repeat_instrument':str(df_scan_sample.loc[0,'redcap_repeat_instrument']),

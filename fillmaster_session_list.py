ad638ab97989157d80975067fa6ccbdc88ec71cd
dharlabwustl <atulkumar@wustl.edu>
2023-12-15 11:48:45 -0600

    Flowchart commit

--- fillmaster_session_list.py
+++ fillmaster_session_list.py
@@ -1279,7 +1279,7 @@ def append_dicominfo_to_analytics(session_id,scan_id,csvfilename,dir_to_save="./
         # fill_datapoint_each_sessionn_1(identifier,'slice_thickness',res_z,csvfilename)
         fill_datapoint_each_sessionn_1(identifier,'scanner',str(scanner_manufacturer) +' '+str(scanner_model),csvfilename)
         # fill_datapoint_each_sessionn_1(identifier,'scanner_model',scanner_model,csvfilename)
-        fill_datapoint_each_sessionn_1(identifier,'acquisition_datetime',dateandtime,csvfilename)
+        # fill_datapoint_each_sessionn_1(identifier,'acquisition_datetime',dateandtime,csvfilename)
         fill_datapoint_each_sessionn_1(identifier,'body_part',bodypart,csvfilename)
         fill_datapoint_each_sessionn_1(identifier,'kvp',kvp,csvfilename)
 

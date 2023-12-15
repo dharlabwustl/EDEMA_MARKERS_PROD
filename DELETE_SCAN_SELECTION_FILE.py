41eb1c45b82d91beb3dbec220869454d4278b5ab
dharlabwustl <atulkumar@wustl.edu>
2023-12-15 11:48:10 -0600

    Flowchart commit

--- DELETE_SCAN_SELECTION_FILE.py
+++ DELETE_SCAN_SELECTION_FILE.py
@@ -7,14 +7,13 @@ from fillmaster_session_list import *
 from download_with_session_ID import *
 from system_analysis import *
 
-command='export XNAT_USER='+sys.argv[2] #${2}'
+command='export XNAT_USER='+sys.argv[2]
 subprocess.call(command,shell=True)
-command='export XNAT_PASS='+sys.argv[3] #${3}
+command='export XNAT_PASS='+sys.argv[3]
 subprocess.call(command,shell=True)
-# export XNAT_HOST=${4}
-command='export XNAT_HOST='+sys.argv[4] #${4}
+command='export XNAT_HOST='+sys.argv[4]
 subprocess.call(command,shell=True)
-project_ID=sys.argv[1] #${1}
+project_ID=sys.argv[1]
 working_dir="/workinginput"
 output_directory="/workingoutput"
 
@@ -22,9 +21,8 @@ final_output_directory="/outputinsidedocker"
 
 sessions_list=working_dir +'/'+'sessions.csv'
 time_now=datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-# $(date -dnow +%Y%m%d%H%M%S)
 print(project_ID+"_"+time_now)
-copy_session=sessions_list #sessions_list.split('.csv')[0]+'_'+ project_ID+ '_SESSIONS_'+time_now+'.csv'
+copy_session=sessions_list
 get_metadata_project_sessionlist(project_ID,sessions_list)
 copy_session_df=pd.read_csv(copy_session)
 counter=0

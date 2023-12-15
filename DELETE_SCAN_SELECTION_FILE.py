4de49e4131720b82df4a40701a8e46d8a007595a
dharlabwustl <atulkumar@wustl.edu>
2023-12-15 10:38:16 -0600

    Flowchart commit

--- DELETE_SCAN_SELECTION_FILE.py
+++ DELETE_SCAN_SELECTION_FILE.py
@@ -26,27 +26,6 @@ time_now=datetime.datetime.now().strftime("%Y%m%d%H%M%S")
 print(project_ID+"_"+time_now)
 copy_session=sessions_list #sessions_list.split('.csv')[0]+'_'+ project_ID+ '_SESSIONS_'+time_now+'.csv'
 get_metadata_project_sessionlist(project_ID,sessions_list)
-#
-# def download_a_single_file(file_path_csv,dir_to_save,projectid,output_filename): # {
-#     file_path_csv_df=pd.read_csv(file_path_csv)
-#     for row_id,row in file_path_csv_df.iterrows():
-#         filename=row['FILE_PATH']
-#         get_latest_filepath_from_metadata_arguments=arguments()
-#         get_latest_filepath_from_metadata_arguments.stuff=['download_a_singlefile_with_URIString',filename,output_filename,dir_to_save]
-#         download_a_singlefile_with_URIString(get_latest_filepath_from_metadata_arguments) #outputfiles_present=$(python3 system_analysis.py "${get_latest_filepath_from_metadata_arguments[@]}")
-#
-# URI="/data/projects/"+project_ID #${project_ID}
-# dir_to_receive_the_data=working_dir #${working_dir}
-# resource_dir=project_ID+"_SESSION_ANALYTICS_1" #${project_ID}_SESSION_ANALYTICS_1"
-# file_path_csv=os.path.join(dir_to_receive_the_data,project_ID+"_"+resource_dir+"_resultfilepath.csv") #${dir_to_receive_the_data}/${project_ID}"_${resource_dir}_resultfilepath.csv"
-# get_latest_filepath_from_metadata_arguments=arguments()
-# get_latest_filepath_from_metadata_arguments.stuff=['get_latest_filepath_from_metadata_for_analytics',URI,resource_dir,".csv", "sessions_"+project_ID+"_ANALYTICS_STEP1_", file_path_csv]
-# get_latest_filepath_from_metadata_for_analytics(get_latest_filepath_from_metadata_arguments)
-# sessions_list=os.path.join(working_dir,'sessions.csv')
-# time_now=datetime.datetime.now().strftime('%Y%m%d%H%M%S')
-# copy_session=sessions_list.split('.csv')[0]+project_ID+'_ANALYTICS_STEP2_'+time_now+'.csv'
-# download_a_single_file(file_path_csv,dir_to_receive_the_data,project_ID,copy_session)
-# #
 copy_session_df=pd.read_csv(copy_session)
 counter=0
 for row_id,row in copy_session_df.iterrows():

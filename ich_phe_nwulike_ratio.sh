5f5e9a13c124c27329a6c9529f8a9f11ca5b7ef9
dharlabwustl <atulkumar@wustl.edu>
2023-12-27 17:25:28 -0600

    Flowchart commi

--- ich_phe_nwulike_ratio.sh
+++ ich_phe_nwulike_ratio.sh
@@ -856,9 +856,14 @@ while IFS=',' read -ra array; do
       fi
 
     done < <(tail -n +2 "${working_dir}/${output_csvfile_midline}")
+    grayscale_filename=${working_dir_1}/${filename_nifti}
 
-    run_divide_mask_into_left_right ${grayimage} ${output_directory}/$(basename ${csffile}) ${output_directory} ${working_dir}
-    run_divide_mask_into_left_right ${grayimage} ${output_directory}/$(basename ${betfile}) ${output_directory} ${working_dir}
+    grayscale_filename_basename=$(basename ${grayscale_filename})
+    grayscale_filename_basename_noext=${grayscale_filename_basename%.nii*}
+    grayscale_filename_basename_ext=${grayscale_filename_basename##*.}
+    run_divide_mask_into_left_right ${grayscale_filename} ${output_directory}/$(basename ${csffile}) ${output_directory} ${working_dir}
+    calculate_left_right_ratio ${mask_filename3} ${mask_filename4} ${grayscale_filename_basename_noext}
+    run_divide_mask_into_left_right ${grayscale_filename} ${output_directory}/$(basename ${betfile}) ${output_directory} ${working_dir}
 #    latexfilename_prefix=${grayscale_filename%.nii*}_non_lin_reg
 #    #csv_file_tostore_latexfilename=${latexfilename_prefix}_latex.csv
 #    latexfilename=${latexfilename_prefix}_${outputfiles_suffix}.tex

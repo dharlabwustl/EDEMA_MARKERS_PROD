0bd759b61b903f548855159409af25553bd99b6d
dharlabwustl <atulkumar@wustl.edu>
2023-12-28 13:43:38 -0600

    Flowchart commi

--- ich_phe_nwulike_ratio.sh
+++ ich_phe_nwulike_ratio.sh
@@ -876,13 +876,16 @@ while IFS=',' read -ra array; do
      #${working_dir_1}/${filename_nifti}
 
     grayscale_filename_basename=$(basename ${grayscale_filename})
-    grayscale_filename_basename_noext=${grayscale_filename_basename%.nii*}
-    grayscale_filename_basename_ext=${grayscale_filename_basename##*.}
+    grayscale_filename_basename_noext=${grayscale_filename_basename%_resaved*}
+#    grayscale_filename_basename_ext=${grayscale_filename_basename##*.}
     call_divide_a_mask_into_left_right_submasks_arguments=('call_divide_a_mask_into_left_right_submasks_v1' ${grayscale_filename} ${output_directory}/$(basename ${csffile})  ${working_dir} ${output_directory})
     outputfiles_present=$(python3 dividemasks_into_left_right.py "${call_divide_a_mask_into_left_right_submasks_arguments[@]}")
     call_divide_a_mask_into_left_right_submasks_arguments=('call_divide_a_mask_into_left_right_submasks_v1' ${grayscale_filename} ${output_directory}/$(basename ${betfile})  ${working_dir} ${output_directory})
     outputfiles_present=$(python3 dividemasks_into_left_right.py "${call_divide_a_mask_into_left_right_submasks_arguments[@]}")
-
+    csf_left_half=${output_directory}/${grayscale_filename_basename_noext}_resaved_csf_unet_left_half_originalRF.nii.gz
+    csf_right_half=${output_directory}/${grayscale_filename_basename_noext}_resaved_csf_unet_right_half_originalRF.nii.gz
+    call_divide_a_mask_into_left_right_submasks_arguments=('call_csf_related_parameters' ${csf_left_half} ${csf_right_half}  ${csffile} ${working_dir_1}/${filename_nifti})
+    outputfiles_present=$(python3 dividemasks_into_left_right.py "${call_divide_a_mask_into_left_right_submasks_arguments[@]}")
     #    run_divide_mask_into_left_right ${grayscale_filename} ${output_directory}/$(basename ${csffile}) ${output_directory} ${working_dir}
 #    calculate_left_right_ratio ${mask_filename3} ${mask_filename4} ${grayscale_filename_basename_noext}
     #    run_divide_mask_into_left_right ${grayscale_filename} ${output_directory}/$(basename ${betfile}) ${output_directory} ${working_dir}

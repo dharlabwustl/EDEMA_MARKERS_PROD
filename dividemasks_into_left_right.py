3b53f62c3314e7246fa843b41fc469d89eea760e
dharlabwustl <atulkumar@wustl.edu>
2023-12-31 22:54:21 -0600

    Flowchart commi

--- dividemasks_into_left_right.py
+++ dividemasks_into_left_right.py
@@ -63,7 +63,7 @@ def call_begin_csvfile_with_session_name(args):
 def begin_csvfile_with_session_name(csvfilename,session_id,scan_name):
     try:
         session_name=get_session_label(session_id)
-        command="echo success at :: {}::{}::{}>> /software/error.txt".format(inspect.stack()[0][3],session_name,csvfilename,scan_name)
+        command="echo success at :: {}::{}::{}:{}>> /software/error.txt".format(inspect.stack()[0][3],session_name,csvfilename,scan_name)
         subprocess.call(command,shell=True)
         scanname_df=pd.DataFrame([session_id,session_name,scan_name])
         scanname_df.columns=['snipr_session_id','snipr_session','scan_name']

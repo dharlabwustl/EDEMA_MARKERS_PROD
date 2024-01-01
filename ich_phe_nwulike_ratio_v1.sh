f17117048a3e3b65b143ed3431a8387e1bebf3a1
dharlabwustl <atulkumar@wustl.edu>
2024-01-01 07:38:20 -0600

    Flowchart commi

--- ich_phe_nwulike_ratio_v1.sh
+++ ich_phe_nwulike_ratio_v1.sh
@@ -845,8 +845,8 @@ while IFS=',' read -ra array; do
         filename2=$(basename ${url2})
         call_download_a_singlefile_with_URIString_arguments=('call_download_a_singlefile_with_URIString' ${url2} ${filename2} ${output_directory})
         outputfiles_present=$(python3 download_with_session_ID.py "${call_download_a_singlefile_with_URIString_arguments[@]}")
-        csffile=${dir_to_save}/${filename2}
-        echo "${csffile}"
+        matfile=${dir_to_save}/${filename2}
+#        echo "${csffile}"
       fi
 
     done < <(tail -n +2 "${working_dir}/${output_csvfile_1}")

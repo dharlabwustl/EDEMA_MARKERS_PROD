ad8e16143f4dc769d70dceb2e7f8c7d011a9ba4c
dharlabwustl <atulkumar@wustl.edu>
2024-01-03 05:34:05 -0600

    Flowchart commi

--- ich_phe_nwulike_ratio_v1.sh
+++ ich_phe_nwulike_ratio_v1.sh
@@ -1130,12 +1130,12 @@ while IFS=',' read -ra array; do
     #  overlapped_mask_on_otherimage ${grayscale_filename_1} ${contrast_limits} ${outputfile_dir} ${outputfile_suffix} ${color_list} ${working_dir_1} mask_filename
     call_masks_on_grayscale_colored_arguments=('call_masks_on_grayscale_colored' ${grayscale_filename512x512} ${contrast_limits} ${outputfile_dir} ${outputfile_suffix} ${color_list} ${working_dir_1} ${mask_filename1} ${mask_filename2} ${mask_filename3} ${mask_filename4})
     outputfiles_present=$(python3 dividemasks_into_left_right.py "${call_masks_on_grayscale_colored_arguments[@]}")
-#    ### GRAY SCALE with all CSF
-#    outputfile_suffix="COMPLETE_CSF"
-#    color_list='blue_blue'
-#    #mask_filename=(${mask_filename3} ${mask_filename4})
-#    call_masks_on_grayscale_colored_arguments=('call_masks_on_grayscale_colored' ${grayscale_filename_1} ${contrast_limits} ${outputfile_dir} ${outputfile_suffix} ${color_list} ${working_dir_1} ${mask_filename3} ${mask_filename4})
-#    outputfiles_present=$(python3 dividemasks_into_left_right.py "${call_masks_on_grayscale_colored_arguments[@]}")
+    ### GRAY SCALE with all CSF
+    outputfile_suffix="COMPLETE_CSF"
+    color_list='blue_red'
+    #mask_filename=(${mask_filename3} ${mask_filename4})
+    call_masks_on_grayscale_colored_arguments=('call_masks_on_grayscale_colored' ${grayscale_filename_1} ${contrast_limits} ${outputfile_dir} ${outputfile_suffix} ${color_list} ${working_dir_1} ${mask_filename3} ${mask_filename4})
+    outputfiles_present=$(python3 dividemasks_into_left_right.py "${call_masks_on_grayscale_colored_arguments[@]}")
     #    #  overlapped_mask_on_otherimage ${grayscale_filename_1} ${contrast_limits} ${outputfile_dir} ${outputfile_suffix} ${color_list} ${working_dir_1} mask_filename
     #    echo outputfiles_present::${outputfiles_present}
     #

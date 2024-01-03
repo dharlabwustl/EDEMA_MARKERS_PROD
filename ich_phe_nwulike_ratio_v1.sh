ef1023039115aa5fb5b089b0e019ef50653de5c0
dharlabwustl <atulkumar@wustl.edu>
2024-01-03 08:34:17 -0600

    Flowchart commi

--- ich_phe_nwulike_ratio_v1.sh
+++ ich_phe_nwulike_ratio_v1.sh
@@ -1138,7 +1138,7 @@ while IFS=',' read -ra array; do
     contrast_limits='20_60'
     color_list='blue_red'
     #mask_filename=(${mask_filename3} ${mask_filename4})
-    call_masks_on_grayscale_colored_arguments=('masks_on_grayscale_colored_v1' ${levelset_gray_file} ${contrast_limits} ${outputfile_dir} ${outputfile_suffix} ${color_list} ${output_directory} ${mask_filename3} ${mask_filename4})
+    call_masks_on_grayscale_colored_arguments=('call_masks_on_grayscale_colored' ${levelset_gray_file} ${contrast_limits} ${outputfile_dir} ${outputfile_suffix} ${color_list} ${output_directory} ${mask_filename3} ${mask_filename4})
     outputfiles_present=$(python3 dividemasks_into_left_right.py "${call_masks_on_grayscale_colored_arguments[@]}")
     #    #  overlapped_mask_on_otherimage ${grayscale_filename_1} ${contrast_limits} ${outputfile_dir} ${outputfile_suffix} ${color_list} ${working_dir_1} mask_filename
     #    echo outputfiles_present::${outputfiles_present}

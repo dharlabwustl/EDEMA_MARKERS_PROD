28e935fa7977b37f0e62ae8a811859dc29f8d3d7
dharlabwustl <atulkumar@wustl.edu>
2024-01-03 06:07:48 -0600

    Flowchart commi

--- dividemasks_into_left_right.py
+++ dividemasks_into_left_right.py
@@ -145,7 +145,8 @@ def masks_on_grayscale_colored(grayscale_filename,contrast_limits,mask_filename_
         slice_3_layer= np.zeros([grayscale_filename_np.shape[0],grayscale_filename_np.shape[1],3])
         method_name="REGIS"
         # npyfiledirectory="/input"
-
+        command="echo successful at :: {}::grayscale_filename::{} >> /software/error.txt".format(inspect.stack()[0][3],grayscale_filename)
+        subprocess.call(command,shell=True)
         for i in range(grayscale_filename_np.shape[2]):
             slice_3_layer[:,:,0]= grayscale_filename_np[:,:,i] #imgray1
             slice_3_layer[:,:,1]= grayscale_filename_np[:,:,i] #imgray1
@@ -159,8 +160,7 @@ def masks_on_grayscale_colored(grayscale_filename,contrast_limits,mask_filename_
             slice_number="{0:0=3d}".format(i)
             # filename_tosave=re.sub('[^a-zA-Z0-9 \n\_]', '', os.path.basename(grayscale_filename).split(".nii")[0])
             # this_npyfile=os.path.join(npyfiledirectory,filename_tosave+method_name+"_"+str(slice_number)+  "_V2.npy")
-            command="echo successful at :: {}::maskfilename::{} >> /software/error.txt".format(inspect.stack()[0][3],'masks_on_grayscale_colored')
-            subprocess.call(command,shell=True)
+
             if os.path.exists(npyfiledirectory):
                 try:
                     slice_3_layer=draw_midline_on_a_slice(grayscale_filename,method_name,npyfiledirectory,slice_3_layer,slice_number)

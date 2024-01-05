f4789430cd3f3421deb491f8206c472b7c1fee64
dharlabwustl <atulkumar@wustl.edu>
2024-01-05 23:39:40 +0800

    Flowchart commi

--- script_to_call_main_program.sh
+++ script_to_call_main_program.sh
@@ -236,6 +236,6 @@ if [[ ${TYPE_OF_PROGRAM} == "PHE_NWULIKE_RATIO" ]]; then
   /software/nwu_with_ich_mask.sh ${SESSION_ID} $XNAT_USER $XNAT_PASS $XNAT_HOST
   /software/phe_nwu_calculation.sh   ${SESSION_ID} $XNAT_USER $XNAT_PASS $XNAT_HOST
 
-#  ich_phe_nwulike_ratio_v1.sh
+
 
 fi

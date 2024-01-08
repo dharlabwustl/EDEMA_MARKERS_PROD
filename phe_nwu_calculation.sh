8a5b59473303d156009b74762ea6954d453d2bc1
dharlabwustl <atulkumar@wustl.edu>
2024-01-08 17:25:16 +0800

    Flowchart commi

--- phe_nwu_calculation.sh
+++ phe_nwu_calculation.sh
@@ -345,9 +345,9 @@ for niftifile_csvfilename in ${working_dir}/*NIFTILOCATION.csv; do
 #      done
       ######################################################################################################################
       echo " FILES NOT PRESENT I AM WORKING ON IT"
-        cp /workingoutput/*.tex /working/
-        cp /workingoutput/*.csv /working/
-        cp /workingoutput/*.png /working/
+#        cp /workingoutput/*.tex /working/
+#        cp /workingoutput/*.csv /working/
+#        cp /workingoutput/*.png /working/
 
     else
       echo " FILES ARE PRESENT "

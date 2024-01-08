79470f4739fc9015714a0cada01793574c550dbe
dharlabwustl <atulkumar@wustl.edu>
2024-01-08 16:46:26 +0800

    Flowchart commi

--- phe_nwu_ich_calculation.sh
+++ phe_nwu_ich_calculation.sh
@@ -5,5 +5,11 @@
   XNAT_HOST=${4}
 
   /software/nwu_with_ich_mask.sh ${SESSION_ID} $XNAT_USER $XNAT_PASS $XNAT_HOST
-
+  cp /workingoutput/*.tex /working/
+  cp /workingoutput/*.csv /working/
+  cp /workingoutput/*.png /working/
   /software/phe_nwu_calculation.sh   ${SESSION_ID} $XNAT_USER $XNAT_PASS $XNAT_HOST
+  cp /workingoutput/*.tex /working/
+  cp /workingoutput/*.csv /working/
+  cp /workingoutput/*.png /working/
+

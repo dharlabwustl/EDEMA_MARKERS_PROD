ba5b2bb7e55d29158fd3cadfdd24e1c5de782b62
dharlabwustl <atulkumar@wustl.edu>
2024-01-16 23:18:19 +0800

    Flowchart commi

--- module_ICH_CSFCompartment_Calculations.py
+++ module_ICH_CSFCompartment_Calculations.py
@@ -1219,7 +1219,7 @@ def measure_compartments_with_reg_round5_one_file_sh_v1() : #niftifilenamedir,np
         grayfilename=niftifilename #os.path.join(niftifilenamedir,grayfilename)
         thisfilebasename=os.path.basename(grayfilename).split("_resaved")[0]
         # csvfile_with_vol_total=os.path.join(SLICE_OUTPUT_DIRECTORY,os.path.basename(grayfilename).split(".nii")[0] + "_threshold"+ str(lower_thresh) + "_" + str(upper_thresh) + "TOTAL.csv")
-        csvfile_with_vol_total=os.path.join(SLICE_OUTPUT_DIRECTORY,thisfilebasename + "_threshold"+ str(lower_thresh) + "_" + str(upper_thresh) + "TOTAL" +Version_Date+date_time + ".csv")
+        csvfile_with_vol_total=os.path.join(SLICE_OUTPUT_DIRECTORY,thisfilebasename + "_threshold"+ str(lower_thresh) + "_" + str(upper_thresh) + "_ICHCSF_TOTAL" +Version_Date+date_time + ".csv")
 
 
 

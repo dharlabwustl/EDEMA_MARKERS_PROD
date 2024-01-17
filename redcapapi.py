612eed779536979d08005871d07d94fec233cab3
dharlabwustl <atulkumar@wustl.edu>
2024-01-17 15:10:07 +0800

    REDCAP INPUT

--- redcapapi.py
+++ redcapapi.py
@@ -124,6 +124,7 @@ counter=0
 for each_unique_subject in unique_subjects:
     subject_df=copy_session_df[copy_session_df['subject_id']==each_unique_subject]
     for each_row_id,each_row in subject_df.iterrows():
+        print((each_row['subject_id']))
         print((each_row['acquisition_datetime']))
     counter=counter+1
     if counter >10:

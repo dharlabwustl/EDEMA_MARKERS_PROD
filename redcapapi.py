b5636a39b7580bf7c8c32ddabfb13af60cc44d4a
dharlabwustl <atulkumar@wustl.edu>
2024-01-17 10:31:42 +0800

    REDCAP INPUT

--- redcapapi.py
+++ redcapapi.py
@@ -34,8 +34,10 @@ dir_to_receive_the_data=working_dir #${working_dir}
 resource_dir=project_ID+"_SESSION_ANALYTICS_1" #${project_ID}_SESSION_ANALYTICS_1"
 file_path_csv=os.path.join(dir_to_receive_the_data,project_ID+"_"+resource_dir+"_resultfilepath.csv")
 get_latest_filepath_from_metadata_arguments=arguments()
+
 get_latest_filepath_from_metadata_arguments.stuff=['get_latest_filepath_from_metadata_for_analytics',URI,resource_dir,".csv", "sessions_"+project_ID+"_ANALYTICS_STEP1_", file_path_csv]
-get_latest_filepath_from_metadata_for_analytics(get_latest_filepath_from_metadata_arguments)
+print(get_latest_filepath_from_metadata_arguments.stuff)
+# get_latest_filepath_from_metadata_for_analytics(get_latest_filepath_from_metadata_arguments)
 
 ### fill each session with its projectname, subject name, instrument number, instance number
 

614e6596143f3f3a22d7e5bcc55aa82fc02f6344
dharlabwustl <atulkumar@wustl.edu>
2024-01-17 11:00:34 +0800

    REDCAP INPUT

--- redcapapi.py
+++ redcapapi.py
@@ -34,11 +34,13 @@ dir_to_receive_the_data=working_dir #${working_dir}
 resource_dir=project_ID+"_SESSION_ANALYTICS_1" #${project_ID}_SESSION_ANALYTICS_1"
 file_path_csv=os.path.join(dir_to_receive_the_data,project_ID+"_"+resource_dir+"_resultfilepath.csv")
 get_latest_filepath_from_metadata_arguments=arguments()
-
 get_latest_filepath_from_metadata_arguments.stuff=['get_latest_filepath_from_metadata_for_analytics',URI,resource_dir,".csv", "sessions_"+project_ID+"_ANALYTICS_STEP1_", file_path_csv]
 print(get_latest_filepath_from_metadata_arguments.stuff)
 get_latest_filepath_from_metadata_for_analytics(get_latest_filepath_from_metadata_arguments)
-
+sessions_list=os.path.join(working_dir,'sessions.csv')
+time_now=datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+copy_session=sessions_list.split('.csv')[0]+project_ID+'_ANALYTICS_STEP1_'+time_now+'.csv'
+download_a_single_file(file_path_csv,dir_to_receive_the_data,project_ID,copy_session)
 ### fill each session with its projectname, subject name, instrument number, instance number
 
 # df_scan=pd.read_csv('test.csv',index_col=False, dtype=object)

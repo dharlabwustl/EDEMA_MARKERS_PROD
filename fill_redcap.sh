8114d7d39807b6bf461fbf5fd24edcb338801982
dharlabwustl <atulkumar@wustl.edu>
2024-01-17 21:32:14 +0800

    REDCAP INPUT

--- fill_redcap.sh
+++ fill_redcap.sh
@@ -8,4 +8,4 @@ IFS='::::' read -ra ADDR <<< "${XNAT_HOST_TOKEN}"
 redcap_token='EC6A2206FF8C1D87D4035E61C99290FF' #${ADDR[2]}
 redcap_url='https://redcap.wustl.edu/redcap/api/' #${ADDR[1]} #'https://redcap.wustl.edu/redcap/api/'
 echo '${redcap_token} ${redcap_url}'::"${redcap_token} ${redcap_url}"
-python3 /software/redcapapi.py ${redcap_token} ${redcap_url} ${PROJECT_ID}
+#python3 /software/redcapapi.py ${redcap_token} ${redcap_url} ${PROJECT_ID}

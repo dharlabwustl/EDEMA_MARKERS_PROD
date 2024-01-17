da7cffe37071723ad3a5c5c48b9b0f7d714d62a6
dharlabwustl <atulkumar@wustl.edu>
2024-01-17 13:06:20 +0800

    REDCAP INPUT

--- redcapapi.py
+++ redcapapi.py
@@ -92,7 +92,9 @@ for each_row_id,each_row in copy_session_df.iterrows():
                 'redcap_repeat_instrument':this_redcap_repeat_instrument,
                 'redcap_repeat_instance':this_redcap_repeat_instance,
                 'record_id':this_record_id,
-                'subject':this_subject
+                'project':project_ID,
+                'subject':this_subject,
+                'subject_number':this_subject.split('_')[1]
                 # 'snipr_session':this_snipr_session
             }
             print(record)
@@ -113,6 +115,56 @@ for each_row_id,each_row in copy_session_df.iterrows():
             if counter>10:
                 break
 
+######################## FILL SESSION LABLE ############################################
+this_project_redcapfile_latest=project_ID+'_latest.csv'
+df_scan=download_latest_redcapfile(project_ID,api_token,this_project_redcapfile_latest)
+for each_row_id,each_row in copy_session_df.iterrows():
+    print(type(each_row['subject_id']))
+    # if str(each_row['subject_id']) not in df_scan['record_id'].tolist() :
+    this_project_redcapfile_latest=project_ID+'_latest1.csv'
+    df_scan_1=download_latest_redcapfile(project_ID,api_token,this_project_redcapfile_latest)
+    this_record_subject_df=df_scan[(df_scan['redcap_repeat_instance']=="2" ) & (df_scan['record_id']=='ATUL_001')].reset_index()
+    # # time.sleep(5)
+    # print(df_scan['record_id'].tolist())
+    # if str(each_row['subject_id']) not in df_scan_1['record_id'].tolist():
+    #     if str(each_row['subject_id']) not in df_scan_1['record_id'].tolist()
+    #     #     print('I AM NOT IN THE RECORD')
+    #     print(type(df_scan['record_id'].tolist()[0]))
+    #     print('I AM NOT IN THE RECORD')
+    #     # break
+    #     if  str(each_row['subject_id']) not in record_ids_done:
+    #         print('I AM NOT IN THE RECORD_ID_LIST')
+    #
+    #         #         # print(each_row['label']+':::::'+each_row['subject_id'])
+    #         this_record_id=str(each_row['subject_id'])
+    #         this_subject=str(each_row['subject_id'])
+    #         this_redcap_repeat_instance=str(1)
+    #         this_redcap_repeat_instrument='stroke_details'
+    #         this_snipr_session=str(each_row['label'])
+    #         record = {
+    #             'redcap_repeat_instrument':this_redcap_repeat_instrument,
+    #             'redcap_repeat_instance':this_redcap_repeat_instance,
+    #             'record_id':this_record_id,
+    #             'subject':this_subject
+    #             # 'snipr_session':this_snipr_session
+    #         }
+    #         print(record)
+    #         # break
+    #         data = json.dumps([record])
+    #         fields = {
+    #             'token': api_token,
+    #             'content': 'record',
+    #             'format': 'json',
+    #             'type': 'flat',
+    #             'data': data,
+    #         }
+    #         r = requests.post(api_url,data=fields)
+    #         print('HTTP Status: ' + str(r.status_code))
+    #         print(r.text)
+    #         record_ids_done.append(this_record_id)
+    #         counter=counter+1
+    #         if counter>10:
+    #             break
 
 # # df_scan_sample=df_scan[(df_scan['redcap_repeat_instance']=="2" ) & (df_scan['record_id']=='ATUL_001')].reset_index()
 # df_scan_sample=df_scan[(df_scan['record_id']=="1" )].reset_index()

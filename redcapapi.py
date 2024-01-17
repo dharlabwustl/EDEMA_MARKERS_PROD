01ca6e8a37c5a230511c343050bfef802bf0c8b4
dharlabwustl <atulkumar@wustl.edu>
2024-01-17 11:54:24 +0800

    REDCAP INPUT

--- redcapapi.py
+++ redcapapi.py
@@ -55,7 +55,9 @@ print(df_scan['record_id'].tolist())
 
 for each_row_id,each_row in copy_session_df.iterrows():
     if str(each_row['subject_id']) not in df_scan['record_id'].tolist():
+        print('I AM NOT IN THE RECORD')
         if  str(each_row['subject_id']) not in record_ids_done:
+            print('I AM NOT IN THE RECORD_ID_LIST')
             # print(each_row['label']+':::::'+each_row['subject_id'])
             this_record_id=str(each_row['subject_id'])
             this_subject=str(each_row['subject_id'])

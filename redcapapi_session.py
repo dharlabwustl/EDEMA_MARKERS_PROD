ecf9ee96e0ef98bf17a73bdd5a3582a26a4e5112
dharlabwustl <atulkumar@wustl.edu>
2024-01-17 21:47:26 +0800

    REDCAP INPUT

--- redcapapi_session.py
+++ redcapapi_session.py
@@ -113,11 +113,14 @@ subject_col_name='subject_id'
 datetime_col_name='acquisition_datetime'
 subject_df=sorted_subj_list(subject_df,subject_col_name,datetime_col_name)
 subject_df['redcap_repeat_instance']=''
-print(subject_df)
+
 # # subject_df=subject_df[subject_df['axial_number']>0 | subject_df['axial_thin_number']>0]
 this_redcap_repeat_instance=1
 for each_row_id in range(subject_df.shape[0]):
-    print(subject_df.iloc[each_row_id])
+    subject_df.at[each_row_id,'redcap_repeat_instance']=this_redcap_repeat_instance
+    this_redcap_repeat_instance=this_redcap_repeat_instance+1
+print(subject_df)
+# print(subject_df.iloc[each_row_id,'sub'])
 
 # for each_row_id,each_row in subject_df.iterrows():
 #     print((each_row['subject_id']))

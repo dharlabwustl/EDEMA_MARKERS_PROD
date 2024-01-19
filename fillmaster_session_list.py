67a9535ec2d7da91e8945cd5fe9a607e814d9fd1
dharlabwustl <atulkumar@wustl.edu>
2024-01-20 00:46:07 +0800

    REDCAP INPUT

--- fillmaster_session_list.py
+++ fillmaster_session_list.py
@@ -1142,7 +1142,7 @@ def append_sessionxmlinfo_to_analytics(args):
         subprocess.call("echo " + "I identifier AT ::{}  >> /workingoutput/error.txt".format(identifier) ,shell=True )
         with open(xmlfile) as fd:
             subprocess.call("echo " + "I xmlfile AT ::{}  >> /workingoutput/error.txt".format(xmlfile) ,shell=True )
-            subprocess.call("cat " + xmlfile,shell=True )
+            subprocess.call("cat " + "  {}  >> /workingoutput/error.txt".format(xmlfile) ,shell=True )
             xmlfile_dict = xmltodict.parse(fd.read())
             subprocess.call("echo " + "I xmlfile AT ::{}  >> /workingoutput/error.txt".format(xmlfile) ,shell=True )
         ## subject id:

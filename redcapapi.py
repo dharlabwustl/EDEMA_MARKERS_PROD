fee68e15bbcc9a26a7c51b9301c7a5f0c8ad1a82
dharlabwustl <atulkumar@wustl.edu>
2024-02-10 01:38:35 +0800

    New Commit

--- redcapapi.py
+++ redcapapi.py
@@ -117,7 +117,7 @@ counter=0
 # for each_row_id,each_row in df_scan.iterrows():
    # delete_record(each_row['record_id'])
 ##############FILL RECORD ID AND SUBJECT ID , project and subject number , VERY FIRST TIME ##############################
-fill_subjects_records(copy_session,counter_ul=20)
+# fill_subjects_records(copy_session,counter_ul=20)
 ######################## FILL SESSION LABEL in IMAGING INSTRUMENT ############################################
 unique_subjects=sorted(list(set(copy_session_df['subject_id'].tolist()))) #.sort()
 print(unique_subjects)
@@ -160,72 +160,80 @@ def add_one_data_to_redcap(this_record_id,this_redcap_repeat_instrument,this_red
     except:
         pass
     return
-for each_unique_subject in unique_subjects:
-    copy_session_df['redcap_repeat_instance'] = copy_session_df.groupby('subject_id').cumcount() + 1
-    subject_df=copy_session_df[copy_session_df['subject_id']==each_unique_subject]
-    subject_col_name='subject_id'
-    datetime_col_name='acquisition_datetime'
-    subject_df=sorted_subj_list(subject_df,subject_col_name,datetime_col_name)
-    # subject_df=subject_df[subject_df['axial_number']>0 | subject_df['axial_thin_number']>0]
-    # this_redcap_repeat_instance=1
-
-    for each_row_id,each_row in subject_df.iterrows():
-        print((each_row['subject_id']))
-        print((each_row['acquisition_datetime']))
-        this_date_time=str(each_row['acquisition_datetime'])
-        try:
-            this_date_time=datetime.datetime.strptime(str(each_row['acquisition_datetime']),'%m/%d/%Y %H:%M').strftime('%Y-%m-%d %H:%M')
-            print(this_date_time)
-        except:
-            pass
-        nifti_file_present=0
-        scan_selection_complete=0
-        if len(str(each_row['FileName_slice']))>3:
-            nifti_file_present=1
-            scan_selection_complete=1
-
-        # add_one_data_to_redcap(each_row['subject_id'],'imaging_data',this_redcap_repeat_instance,'nifti_file_present',str(nifti_file_present))
-        # add_one_data_to_redcap(each_row['subject_id'],'imaging_data',this_redcap_repeat_instance,'scan_selection_complete',str(scan_selection_complete))
-        add_one_data_to_redcap(each_row['subject_id'],each_row['redcap_repeat_instance'],'imaging_data','snipr_session',str(each_row['label']))
-        add_one_data_to_redcap(each_row['subject_id'],each_row['redcap_repeat_instance'],'imaging_data','scan_stem',str(each_row['NIFTIFILES_PREFIX']))
-        add_one_data_to_redcap(each_row['subject_id'],each_row['redcap_repeat_instance'],'imaging_data','scan_name',str(each_row['FileName_slice']))
-        add_one_data_to_redcap(each_row['subject_id'],each_row['redcap_repeat_instance'],'imaging_data','scan_date_time',this_date_time)
-        add_one_data_to_redcap(each_row['subject_id'],each_row['redcap_repeat_instance'],'imaging_data','scan_kernel',str(each_row['SCAN_DESCRIPTION']))
-        # add_one_data_to_redcap(each_row['subject_id'],each_row['redcap_repeat_instance'],'imaging_data','stroke_side',str(each_row['ICH SIDE']).upper()[0])
-        add_one_data_to_redcap(each_row['subject_id'],each_row['redcap_repeat_instance'],'imaging_data','axial_number',each_row['axial_number'])
-        add_one_data_to_redcap(each_row['subject_id'],each_row['redcap_repeat_instance'],'imaging_data','axial_thin_number',each_row['axial_thin_number'])
-        add_one_data_to_redcap(each_row['subject_id'],each_row['redcap_repeat_instance'],'imaging_data','kvp',each_row['kvp'])
-        add_one_data_to_redcap(each_row['subject_id'],each_row['redcap_repeat_instance'],'imaging_data','body_site',each_row['body_part'])
-        add_one_data_to_redcap(each_row['subject_id'],each_row['redcap_repeat_instance'],'imaging_data','scanner_name',each_row['scanner'])
-        add_one_data_to_redcap(each_row['subject_id'],each_row['redcap_repeat_instance'],'imaging_data','scan_selected',each_row['SELECTED_SCAN_ID'])
-        pdf_created=0
-        try:
-            if int(str(each_row['PDF_FILE_NUM'])) >0:
-                pdf_created=1
-        except:
-            pass
-        add_one_data_to_redcap(each_row['subject_id'],each_row['redcap_repeat_instance'],'imaging_data','pdf_created',pdf_created)
-        add_one_data_to_redcap(each_row['subject_id'],each_row['redcap_repeat_instance'],'imaging_data','csf_total',each_row['TOTAL CSF VOLUME'])
-        # add_one_data_to_redcap(each_row['subject_id'],each_row['redcap_repeat_instance'],'imaging_data','csf_left',each_row['LEFT CSF VOLUME AFTER EDEMA SUBT'])
-        # add_one_data_to_redcap(each_row['subject_id'],each_row['redcap_repeat_instance'],'imaging_data','csf_right',each_row['RIGHT CSF VOLUME AFTER EDEMA SUBT'])
-        # add_one_data_to_redcap(each_row['subject_id'],each_row['redcap_repeat_instance'],'imaging_data','csf_ratio',each_row['CSF RATIO AFTER EDEMA SUBT'])
-        add_one_data_to_redcap(each_row['subject_id'],each_row['redcap_repeat_instance'],'imaging_data','brain_left',each_row['LEFT BRAIN VOLUME without CSF'])
-        add_one_data_to_redcap(each_row['subject_id'],each_row['redcap_repeat_instance'],'imaging_data','brain_right',each_row['RIGHT BRAIN VOLUME without CSF'])
-        add_one_data_to_redcap(each_row['subject_id'],each_row['redcap_repeat_instance'],'imaging_data','cranial',each_row['BET VOLUME'])
-
-        # add_one_data_to_redcap(each_row['subject_id'],'imaging_data','brain_ratio',each_row['label'])
-        # add_one_data_to_redcap(each_row['subject_id'],'imaging_data','infarct_volume',each_row['ICH EDEMA VOLUME'])
-        # add_one_data_to_redcap(each_row['subject_id'],'imaging_data','nwu',each_row['label'])
-        add_one_data_to_redcap(each_row['subject_id'],each_row['redcap_repeat_instance'],'imaging_data','px',each_row['px'])
-        add_one_data_to_redcap(each_row['subject_id'],each_row['redcap_repeat_instance'],'imaging_data','pz',each_row['pz'])
-        add_one_data_to_redcap(each_row['subject_id'],each_row['redcap_repeat_instance'],'imaging_data','slices',each_row['SLICE_NUM'])
-        add_one_data_to_redcap(each_row['subject_id'],each_row['redcap_repeat_instance'],'imaging_data','software_version',each_row['label'])
-        add_one_data_to_redcap(each_row['subject_id'],each_row['redcap_repeat_instance'],'imaging_data','software_application_date',each_row['label'])
-        # this_redcap_repeat_instance=this_redcap_repeat_instance+1
+copy_session_df['redcap_repeat_instance'] = copy_session_df.groupby('subject_id').cumcount() + 1
+counter=0
+for each_row_id,each_row in  copy_session_df.iterrows():
+    add_one_data_to_redcap(each_row['subject_id'],each_row['redcap_repeat_instance'],'imaging_data','snipr_session',str(each_row['label']))
     counter=counter+1
-    if counter >10:
+    if counter>10:
         break
-    # break
+#
+# for each_unique_subject in unique_subjects:
+#     # copy_session_df['redcap_repeat_instance'] = copy_session_df.groupby('subject_id').cumcount() + 1
+#     subject_df=copy_session_df[copy_session_df['subject_id']==each_unique_subject]
+#     subject_col_name='subject_id'
+#     datetime_col_name='acquisition_datetime'
+#     subject_df=sorted_subj_list(subject_df,subject_col_name,datetime_col_name)
+#     # subject_df=subject_df[subject_df['axial_number']>0 | subject_df['axial_thin_number']>0]
+#     # this_redcap_repeat_instance=1
+#
+#     for each_row_id,each_row in subject_df.iterrows():
+#         print((each_row['subject_id']))
+#         print((each_row['acquisition_datetime']))
+#         this_date_time=str(each_row['acquisition_datetime'])
+#         try:
+#             this_date_time=datetime.datetime.strptime(str(each_row['acquisition_datetime']),'%m/%d/%Y %H:%M').strftime('%Y-%m-%d %H:%M')
+#             print(this_date_time)
+#         except:
+#             pass
+#         nifti_file_present=0
+#         scan_selection_complete=0
+#         if len(str(each_row['FileName_slice']))>3:
+#             nifti_file_present=1
+#             scan_selection_complete=1
+#
+#         # add_one_data_to_redcap(each_row['subject_id'],'imaging_data',this_redcap_repeat_instance,'nifti_file_present',str(nifti_file_present))
+#         # add_one_data_to_redcap(each_row['subject_id'],'imaging_data',this_redcap_repeat_instance,'scan_selection_complete',str(scan_selection_complete))
+#         add_one_data_to_redcap(each_row['subject_id'],each_row['redcap_repeat_instance'],'imaging_data','snipr_session',str(each_row['label']))
+#         add_one_data_to_redcap(each_row['subject_id'],each_row['redcap_repeat_instance'],'imaging_data','scan_stem',str(each_row['NIFTIFILES_PREFIX']))
+#         add_one_data_to_redcap(each_row['subject_id'],each_row['redcap_repeat_instance'],'imaging_data','scan_name',str(each_row['FileName_slice']))
+#         add_one_data_to_redcap(each_row['subject_id'],each_row['redcap_repeat_instance'],'imaging_data','scan_date_time',this_date_time)
+#         add_one_data_to_redcap(each_row['subject_id'],each_row['redcap_repeat_instance'],'imaging_data','scan_kernel',str(each_row['SCAN_DESCRIPTION']))
+#         # add_one_data_to_redcap(each_row['subject_id'],each_row['redcap_repeat_instance'],'imaging_data','stroke_side',str(each_row['ICH SIDE']).upper()[0])
+#         add_one_data_to_redcap(each_row['subject_id'],each_row['redcap_repeat_instance'],'imaging_data','axial_number',each_row['axial_number'])
+#         add_one_data_to_redcap(each_row['subject_id'],each_row['redcap_repeat_instance'],'imaging_data','axial_thin_number',each_row['axial_thin_number'])
+#         add_one_data_to_redcap(each_row['subject_id'],each_row['redcap_repeat_instance'],'imaging_data','kvp',each_row['kvp'])
+#         add_one_data_to_redcap(each_row['subject_id'],each_row['redcap_repeat_instance'],'imaging_data','body_site',each_row['body_part'])
+#         add_one_data_to_redcap(each_row['subject_id'],each_row['redcap_repeat_instance'],'imaging_data','scanner_name',each_row['scanner'])
+#         add_one_data_to_redcap(each_row['subject_id'],each_row['redcap_repeat_instance'],'imaging_data','scan_selected',each_row['SELECTED_SCAN_ID'])
+#         pdf_created=0
+#         try:
+#             if int(str(each_row['PDF_FILE_NUM'])) >0:
+#                 pdf_created=1
+#         except:
+#             pass
+#         add_one_data_to_redcap(each_row['subject_id'],each_row['redcap_repeat_instance'],'imaging_data','pdf_created',pdf_created)
+#         add_one_data_to_redcap(each_row['subject_id'],each_row['redcap_repeat_instance'],'imaging_data','csf_total',each_row['TOTAL CSF VOLUME'])
+#         # add_one_data_to_redcap(each_row['subject_id'],each_row['redcap_repeat_instance'],'imaging_data','csf_left',each_row['LEFT CSF VOLUME AFTER EDEMA SUBT'])
+#         # add_one_data_to_redcap(each_row['subject_id'],each_row['redcap_repeat_instance'],'imaging_data','csf_right',each_row['RIGHT CSF VOLUME AFTER EDEMA SUBT'])
+#         # add_one_data_to_redcap(each_row['subject_id'],each_row['redcap_repeat_instance'],'imaging_data','csf_ratio',each_row['CSF RATIO AFTER EDEMA SUBT'])
+#         add_one_data_to_redcap(each_row['subject_id'],each_row['redcap_repeat_instance'],'imaging_data','brain_left',each_row['LEFT BRAIN VOLUME without CSF'])
+#         add_one_data_to_redcap(each_row['subject_id'],each_row['redcap_repeat_instance'],'imaging_data','brain_right',each_row['RIGHT BRAIN VOLUME without CSF'])
+#         add_one_data_to_redcap(each_row['subject_id'],each_row['redcap_repeat_instance'],'imaging_data','cranial',each_row['BET VOLUME'])
+#
+#         # add_one_data_to_redcap(each_row['subject_id'],'imaging_data','brain_ratio',each_row['label'])
+#         # add_one_data_to_redcap(each_row['subject_id'],'imaging_data','infarct_volume',each_row['ICH EDEMA VOLUME'])
+#         # add_one_data_to_redcap(each_row['subject_id'],'imaging_data','nwu',each_row['label'])
+#         add_one_data_to_redcap(each_row['subject_id'],each_row['redcap_repeat_instance'],'imaging_data','px',each_row['px'])
+#         add_one_data_to_redcap(each_row['subject_id'],each_row['redcap_repeat_instance'],'imaging_data','pz',each_row['pz'])
+#         add_one_data_to_redcap(each_row['subject_id'],each_row['redcap_repeat_instance'],'imaging_data','slices',each_row['SLICE_NUM'])
+#         add_one_data_to_redcap(each_row['subject_id'],each_row['redcap_repeat_instance'],'imaging_data','software_version',each_row['label'])
+#         add_one_data_to_redcap(each_row['subject_id'],each_row['redcap_repeat_instance'],'imaging_data','software_application_date',each_row['label'])
+#         # this_redcap_repeat_instance=this_redcap_repeat_instance+1
+#     counter=counter+1
+#     if counter >10:
+#         break
+#     # break
 #     print('I AM NOT IN THE RECORD')
 #     # break
 #     if  str(each_row['subject_id']) not in record_ids_done or str(each_row['subject_id'])  in record_ids_done:

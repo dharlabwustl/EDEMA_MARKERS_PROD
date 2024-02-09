a38a548b4ba6cc026ddcbbe710fbd26f948384b9
dharlabwustl <atulkumar@wustl.edu>
2024-02-09 22:20:30 +0800

    New Commit

--- scan_selection_fill_REDCap_Feb09_2024.sh
+++ scan_selection_fill_REDCap_Feb09_2024.sh
@@ -255,7 +255,7 @@ outputfiles_present=$(python3 download_with_session_ID.py "${download_an_xmlfile
 #session_id=${this_session_id}
 #xmlfile=${xml_filename}
 ##subj_listfile=${subject_list}
-fill_redcap_for_selected_scan_arguments=('fill_redcap_for_selected_scan' ${this_session_id} ${filename_xml}  ) #${csvfilename} ${subj_listfile})
+fill_redcap_for_selected_scan_arguments=('fill_redcap_for_selected_scan' ${this_session_id} ${xml_filename}  ) #${csvfilename} ${subj_listfile})
 outputfiles_present=$(python3 download_with_session_ID.py "${fill_redcap_for_selected_scan_arguments[@]}")
 #
 #niftifile_csvfilename=${working_dir}/'this_session_final_ct.csv'

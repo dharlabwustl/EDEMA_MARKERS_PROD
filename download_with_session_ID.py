43bd206e555e457d80c1fb4ae2b0fc6cd221eea6
dharlabwustl <atulkumar@wustl.edu>
2024-02-13 18:55:01 +0800

    New Commit

--- download_with_session_ID.py
+++ download_with_session_ID.py
@@ -747,8 +747,8 @@ def fill_redcap_for_selected_scan(args):
         df_scan_latest=download_latest_redcapfile(api_token,this_project_redcapfile_latest)
         this_session_redcap_repeat_instance_df=df_scan_latest[df_scan_latest['snipr_session']==session_label]
         this_session_redcap_repeat_instance=str(this_session_redcap_repeat_instance_df['redcap_repeat_instance'])
-        for each_colname in csv_file_df.columns:
-            add_one_data_to_redcap(subject_name,'imaging_data',this_session_redcap_repeat_instance,each_colname,csv_file_df[each_colname])
+        # for each_colname in csv_file_df.columns:
+        #     add_one_data_to_redcap(subject_name,'imaging_data',this_session_redcap_repeat_instance,each_colname,csv_file_df[each_colname])
 
         #fill scan base
         ## fill scan complete name

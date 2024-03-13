42b48f7c4272fea55d8aacef1861c4d97e0e04b9
dharlabwustl <atulkumar@wustl.edu>
2024-03-13 11:26:44 -0500

    New Commit

--- download_with_session_ID.py
+++ download_with_session_ID.py
@@ -824,8 +824,10 @@ def fill_redcap_for_selected_scan(args):
                 subprocess.call("echo " + "I PASSED AT session_label::{}  >> /workingoutput/error.txt".format(session_label) ,shell=True )
                 subprocess.call("echo " + "I PASSED AT each_colname::{}  >> /workingoutput/error.txt".format(each_colname) ,shell=True )
                 subprocess.call("echo " + "I PASSED AT each_colname_value::{}  >> /workingoutput/error.txt".format(csv_file_df[each_colname][0]) ,shell=True )
-                add_one_data_to_redcap(subject_name,'imaging_data',this_session_redcap_repeat_instance,str(each_colname),csv_file_df[each_colname].item())
-
+                try:
+                    add_one_data_to_redcap(subject_name,'imaging_data',this_session_redcap_repeat_instance,str(each_colname),csv_file_df[each_colname].item())
+                except:
+                    pass
         #fill scan base
         ## fill scan complete name
         ## fill number of slices,kvp,px,pz, scanner detail

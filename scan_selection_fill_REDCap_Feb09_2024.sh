7992b0329a47d13314a03fee3ae3a667590cb29e
dharlabwustl <atulkumar@wustl.edu>
2024-03-13 12:14:28 -0500

    New Commit

--- scan_selection_fill_REDCap_Feb09_2024.sh
+++ scan_selection_fill_REDCap_Feb09_2024.sh
@@ -533,17 +533,17 @@ echo '$outputfiles_present'::$outputfiles_present
   csvfile_scan_selection_for_redcap_call=('csvfile_scan_selection_for_redcap' ${this_csvfilename} ${csvfile_for_redcap} ${sessionID}) # 'Name' scan_name)
   outputfiles_present=$(python3 fillmaster_session_list.py "${csvfile_scan_selection_for_redcap_call[@]}")
 
-  #call_edit_session_analytics_file_arguments=('rename_columns' ${this_csvfilename} ${this_csvfilename} 'Name' scan_name)
-  #outputfiles_present=$(python3 fillmaster_session_list.py "${call_edit_session_analytics_file_arguments[@]}")
-  this_session_id=${sessionID}
-  xml_filename=${working_dir}/${this_session_id}.xml
-  filename_xml=$(basename ${xml_filename})   #args.stuff[2]
-  dir_to_save_xml=$(dirname ${xml_filename}) #args args.stuff[3]
-  download_an_xmlfile_with_URIString_arguments=('download_an_xmlfile_with_URIString' ${this_session_id} ${filename_xml} ${dir_to_save_xml})
-  outputfiles_present=$(python3 download_with_session_ID.py "${download_an_xmlfile_with_URIString_arguments[@]}")
-
-  fill_redcap_for_selected_scan_arguments=('fill_redcap_for_selected_scan' ${xml_filename} ${csvfile_for_redcap} ) #${subj_listfile})
-  outputfiles_present=$(python3 download_with_session_ID.py "${fill_redcap_for_selected_scan_arguments[@]}")
+#  #call_edit_session_analytics_file_arguments=('rename_columns' ${this_csvfilename} ${this_csvfilename} 'Name' scan_name)
+#  #outputfiles_present=$(python3 fillmaster_session_list.py "${call_edit_session_analytics_file_arguments[@]}")
+#  this_session_id=${sessionID}
+#  xml_filename=${working_dir}/${this_session_id}.xml
+#  filename_xml=$(basename ${xml_filename})   #args.stuff[2]
+#  dir_to_save_xml=$(dirname ${xml_filename}) #args args.stuff[3]
+#  download_an_xmlfile_with_URIString_arguments=('download_an_xmlfile_with_URIString' ${this_session_id} ${filename_xml} ${dir_to_save_xml})
+#  outputfiles_present=$(python3 download_with_session_ID.py "${download_an_xmlfile_with_URIString_arguments[@]}")
+#
+#  fill_redcap_for_selected_scan_arguments=('fill_redcap_for_selected_scan' ${xml_filename} ${csvfile_for_redcap} ) #${subj_listfile})
+#  outputfiles_present=$(python3 download_with_session_ID.py "${fill_redcap_for_selected_scan_arguments[@]}")
 #fi
 #########################################
 #outputfiles_present=0

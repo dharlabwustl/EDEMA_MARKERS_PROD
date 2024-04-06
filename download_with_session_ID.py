cce21bcc980c88b2c0cf44c52c950778a8f57fbb
dharlabwustl <atulkumar@wustl.edu>
2024-04-05 23:42:46 -0500

    New Commit

--- download_with_session_ID.py
+++ download_with_session_ID.py
@@ -18,7 +18,7 @@ XNAT_HOST_URL='https://snipr.wustl.edu'
 XNAT_HOST = XNAT_HOST_URL # os.environ['XNAT_HOST'] #
 XNAT_USER = os.environ['XNAT_USER']#
 XNAT_PASS =os.environ['XNAT_PASS'] #
-
+api_token=os.environ['REDCAP_API']
 class arguments:
     def __init__(self,stuff=[]):
         self.stuff=stuff
@@ -706,7 +706,7 @@ def fill_single_val_redcap(args):
     csvfilename=args.stuff[2]
     project_name,subject_name, session_label,acquisition_site_xml,acquisition_datetime_xml,scanner_from_xml,body_part_xml,kvp_xml=get_info_from_xml(xmlfile)
     this_project_redcapfile_latest=project_name+'_latest.csv'
-    api_token='EC6A2206FF8C1D87D4035E61C99290FF'
+    # api_token='EC6A2206FF8C1D87D4035E61C99290FF'
     df_scan_latest=download_latest_redcapfile(api_token,this_project_redcapfile_latest)
     this_session_redcap_repeat_instance_df=df_scan_latest[df_scan_latest['snipr_session']==session_label]
     this_session_redcap_repeat_instance=str(this_session_redcap_repeat_instance_df['redcap_repeat_instance'])
@@ -810,7 +810,7 @@ def fill_redcap_for_selected_scan(args):
         csv_file_df=pd.read_csv(args.stuff[2])
         project_name,subject_name, session_label,acquisition_site_xml,acquisition_datetime_xml,scanner_from_xml,body_part_xml,kvp_xml=get_info_from_xml(xmlfile)
         this_project_redcapfile_latest=project_name+'_latest.csv'
-        api_token='EC6A2206FF8C1D87D4035E61C99290FF'
+        # api_token='EC6A2206FF8C1D87D4035E61C99290FF'
         df_scan_latest=download_latest_redcapfile(api_token,this_project_redcapfile_latest)
         this_session_redcap_repeat_instance_df=df_scan_latest[df_scan_latest['snipr_session']==session_label]
         this_session_redcap_repeat_instance=str(this_session_redcap_repeat_instance_df['redcap_repeat_instance'].item())
@@ -854,7 +854,7 @@ def fill_redcap_for_pdffile(args):
         file_name=args.stuff[2]
         project_name,subject_name, session_label,acquisition_site_xml,acquisition_datetime_xml,scanner_from_xml,body_part_xml,kvp_xml=get_info_from_xml(xmlfile)
         this_project_redcapfile_latest=project_name+'_latest.csv'
-        api_token='EC6A2206FF8C1D87D4035E61C99290FF'
+        # api_token='EC6A2206FF8C1D87D4035E61C99290FF'
         df_scan_latest=download_latest_redcapfile(api_token,this_project_redcapfile_latest)
         this_session_redcap_repeat_instance_df=df_scan_latest[df_scan_latest['snipr_session']==session_label]
         this_session_redcap_repeat_instance=str(this_session_redcap_repeat_instance_df['redcap_repeat_instance'].item())

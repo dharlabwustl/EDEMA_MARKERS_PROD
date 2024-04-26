228f33744cb30ed3b5c15a93eaecb5267c7bd1a3
dharlabwustl <atulkumar@wustl.edu>
2024-04-26 00:15:11 -0400

    New Commit

--- phe_nwu_ich_calculation.sh
+++ phe_nwu_ich_calculation.sh
@@ -40,11 +40,18 @@ if [[ "${outputfiles_present: -1}" -eq 0 ]]; then                  ##[[ 1 -gt 0 
   cp /workingoutput/*_ICHCSF*.csv /working/
   cp /workingoutput/*.png /working/
 
+    cp /workingoutput/*_ICHCSF*.csv /outputinsidedocker/
+    cp /workingoutput/*_class1.png /outputinsidedocker/
+
   /software/phe_nwu_calculation.sh ${SESSION_ID} $XNAT_USER $XNAT_PASS $XNAT_HOST
   #  cp /workingoutput/*.tex /working/
   cp /workinginput/*NIFTILOCATION*.csv /working/
   cp /workingoutput/*_PHENWU*.csv /working/
   cp /workingoutput/*.png /working/
+  ##########
+    cp /workinginput/*NIFTILOCATION*.csv /outputinsidedocker/
+    cp /workingoutput/*_PHENWU*.csv /outputinsidedocker/
+    cp /workingoutput/*.png /outputinsidedocker/
 
   #echo "HELLO" > /working/atulTOTAL.csv
   counter=0

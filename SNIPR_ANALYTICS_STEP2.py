30be1de9cc5ee3b4cd80ee28968922a07abb66e1
DHAR LAB <106468137+dharlabwustl@users.noreply.github.com>
2024-04-30 17:36:16 -0500

    commit

--- SNIPR_ANALYTICS_STEP2.py
+++ SNIPR_ANALYTICS_STEP2.py
@@ -83,6 +83,7 @@ for row_id,row in copy_session_df.iterrows():
     # columnname=args.stuff[2]
     # neighboring_col=args.stuff[3]
     # csvfilename_edited=args.stuff[4]
+call_edit_session_analytics_file_arguments=arguments()
 call_edit_session_analytics_file_arguments.stuff=['move_one_column_after_another' , copy_session, 'PDF_FILE_NUM', "REGISTRATION_SUCCESS",copy_session]
 rename_columns(call_edit_session_analytics_file_arguments)
 uploadsinglefile_projectlevel_args_arguments=arguments()

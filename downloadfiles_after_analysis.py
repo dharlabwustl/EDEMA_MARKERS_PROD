5678417c8cddedb4820e8eba1332056b2b47e945
DHAR LAB <106468137+dharlabwustl@users.noreply.github.com>
2024-05-16 18:15:52 -0500

    sgfdf

--- downloadfiles_after_analysis.py
+++ downloadfiles_after_analysis.py
@@ -65,9 +65,10 @@ for row_id,row in copy_session_df.iterrows():
         df_scan = pd.read_json(json.dumps(metadata_masks))
         pd.DataFrame(df_scan).to_csv(os.path.join(dir_to_receive_the_data,output_csvfile),index=False)
         if '.pdf' in extension or '.csv' in extension:
-            latest_file_df=get_latest_file(df_scan)
-            print('{}::{}::{}'.format('latest_file_df',latest_file_df,latest_file_df.shape[0] ))
-            latest_file_df.to_csv('latest_file_df.csv',index=False)
+            if df_scan.shape[0] >0:
+                latest_file_df=get_latest_file(df_scan)
+                print('{}::{}::{}'.format('latest_file_df',latest_file_df,latest_file_df.shape[0] ))
+                latest_file_df.to_csv('latest_file_df.csv',index=False)
             # if latest_file_df.shape[0] >0:
             counter=counter+1
                 # get_latest_filepath_from_metadata_arguments.stuff=['download_a_singlefile_with_URIString',latest_file_df['URI'],latest_file_df['Name'],dir_to_save]
@@ -82,6 +83,7 @@ for row_id,row in copy_session_df.iterrows():
                     get_latest_filepath_from_metadata_arguments=arguments()
                     get_latest_filepath_from_metadata_arguments.stuff=['download_a_singlefile_with_URIString',item['URI'],item['Name'],dir_to_save]
                     download_a_singlefile_with_URIString(get_latest_filepath_from_metadata_arguments)
+                    
     except:
         pass
 

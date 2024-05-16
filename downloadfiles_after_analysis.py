895c971075b70e878422f35bffd2248f70be6d84
DHAR LAB <106468137+dharlabwustl@users.noreply.github.com>
2024-05-16 17:32:38 -0500

    asdfdsf

--- downloadfiles_after_analysis.py
+++ downloadfiles_after_analysis.py
@@ -54,9 +54,7 @@ def convert_if_numeric(s):
         return s
 for row_id,row in copy_session_df.iterrows():
     try:
-        counter=counter+1
-        if counter > 10 : #; then
-            break
+
 
 
         URI=row['URI']+'/scans/' +str(convert_if_numeric(str(row['SELECTED_SCAN_ID']).split('.')[0]))
@@ -72,6 +70,10 @@ for row_id,row in copy_session_df.iterrows():
             latest_file_df.to_csv('latest_file_df.csv',index=False)
             # get_latest_filepath_from_metadata_arguments.stuff=['download_a_singlefile_with_URIString',latest_file_df['URI'],latest_file_df['Name'],dir_to_save]
             # download_a_singlefile_with_URIString(get_latest_filepath_from_metadata_arguments)
+            if latest_file_df.shape[0] >0:
+                counter=counter+1
+            if counter > 1 : #; then
+                break
         else:
             for id,item in df_scan.iterrows():
                 if extension in item['Name']:

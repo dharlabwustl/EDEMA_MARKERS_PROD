e50909ad059ffa2b65d8ef9384b80756c8872cea
DHAR LAB <106468137+dharlabwustl@users.noreply.github.com>
2024-05-16 14:47:07 -0500

    jghgjghj

--- downloadfiles_after_analysis.py
+++ downloadfiles_after_analysis.py
@@ -43,12 +43,15 @@ dir_to_save=output_directory
 resource_dir='MASKS'
 for row_id,row in copy_session_df.iterrows():
     URI=row['URI']+'/scans/' +row['SELECTED_SCAN_ID']
-    output_csvfile=os.path.basename(row['URI'])+ '_'+ row['SELECTED_SCAN_ID'] + '.csv'
-    print("{}::{}::{}::{}".format('get_resourcefiles_metadata_saveascsv',URI,resource_dir,dir_to_receive_the_data,output_csvfile))
-    # get_resourcefiles_metadata_saveascsv(get_latest_filepath_from_metadata_arguments)))
-    # get_latest_filepath_from_metadata_arguments=arguments()
-    # get_latest_filepath_from_metadata_arguments.stuff=['get_resourcefiles_metadata_saveascsv',URI,resource_dir,dir_to_receive_the_data,output_csvfile]
-    # get_resourcefiles_metadata_saveascsv(get_latest_filepath_from_metadata_arguments)
+    try:
+        output_csvfile=row['ID']+ '_'+ row['SELECTED_SCAN_ID'] + '.csv'
+        print("{}::{}::{}::{}".format('get_resourcefiles_metadata_saveascsv',URI,resource_dir,dir_to_receive_the_data,output_csvfile))
+        # get_resourcefiles_metadata_saveascsv(get_latest_filepath_from_metadata_arguments)))
+        get_latest_filepath_from_metadata_arguments=arguments()
+        get_latest_filepath_from_metadata_arguments.stuff=['get_resourcefiles_metadata_saveascsv',URI,resource_dir,dir_to_receive_the_data,output_csvfile]
+        get_resourcefiles_metadata_saveascsv(get_latest_filepath_from_metadata_arguments)
+    except:
+        pass
     # get_resourcefiles_metadata_saveascsv(URI,resource_dir,dir_to_receive_the_data,output_csvfile)
     # # while IFS=',' read -ra array; do
     # #   echo array::${array[22]}

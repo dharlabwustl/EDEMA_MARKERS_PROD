ae0b4f7f935f7cc939d4aca4ec57938b19ec7037
DHAR LAB <106468137+dharlabwustl@users.noreply.github.com>
2024-05-16 20:13:13 -0500

    dfasdfdsaf

--- downloadfiles_after_analysis.py
+++ downloadfiles_after_analysis.py
@@ -88,8 +88,8 @@ for row_id,row in copy_session_df.iterrows():
                     download_a_singlefile_with_URIString(get_latest_filepath_from_metadata_arguments)
             counter=counter+1
             print('{}::{}'.format('counter',counter))
-            if counter > 1 : #; then
-                break
+            # if counter > 1 : #; then
+            #     break
     except:
         pass
 

8734d814c9cca0cbdbb9b7e75d484fd46d2c2f8f
DHAR LAB <106468137+dharlabwustl@users.noreply.github.com>
2024-06-16 10:37:52 -0500

    asdfdf

--- SNIPR_ANALYTICS_STEP2_SUBSEQUENT_RUNS.py
+++ SNIPR_ANALYTICS_STEP2_SUBSEQUENT_RUNS.py
@@ -21,24 +21,29 @@ output_directory="/workingoutput"
 final_output_directory="/outputinsidedocker"
 
 def download_a_single_file(file_path_csv,dir_to_save,projectid,output_filename): # {
-    file_path_csv_df=pd.read_csv(file_path_csv)
-    for row_id,row in file_path_csv_df.iterrows():
-        filename=row['FILE_PATH']
-        get_latest_filepath_from_metadata_arguments=arguments()
-        get_latest_filepath_from_metadata_arguments.stuff=['download_a_singlefile_with_URIString',filename,output_filename,dir_to_save]
-        download_a_singlefile_with_URIString(get_latest_filepath_from_metadata_arguments) #outputfiles_present=$(python3 system_analysis.py "${get_latest_filepath_from_metadata_arguments[@]}")
+  file_path_csv_df=pd.read_csv(file_path_csv)
+  for row_id,row in file_path_csv_df.iterrows():
+    filename=row['FILE_PATH']
+    get_latest_filepath_from_metadata_arguments=arguments()
+    get_latest_filepath_from_metadata_arguments.stuff=['download_a_singlefile_with_URIString',filename,output_filename,dir_to_save]
+    download_a_singlefile_with_URIString(get_latest_filepath_from_metadata_arguments) #outputfiles_present=$(python3 system_analysis.py "${get_latest_filepath_from_metadata_arguments[@]}")
 
-URI="/data/projects/"+project_ID #${project_ID}
-dir_to_receive_the_data=working_dir #${working_dir}
-resource_dir=project_ID+"_SESSION_ANALYTICS_2" #${project_ID}_SESSION_ANALYTICS_1"
-file_path_csv=os.path.join(dir_to_receive_the_data,project_ID+"_"+resource_dir+"_resultfilepath.csv") #${dir_to_receive_the_data}/${project_ID}"_${resource_dir}_resultfilepath.csv"
+URI="/data/projects/"+project_ID
+dir_to_receive_the_data=working_dir
+resource_dir=project_ID+"_SESSION_ANALYTICS_2"
+file_path_csv=os.path.join(dir_to_receive_the_data,project_ID+"_"+resource_dir+"_resultfilepath.csv")
 get_latest_filepath_from_metadata_arguments=arguments()
-get_latest_filepath_from_metadata_arguments.stuff=['get_latest_filepath_from_metadata_for_analytics',URI,resource_dir,".csv", "sessions_"+project_ID+"_ANALYTICS_STEP2_", file_path_csv]
+get_latest_filepath_from_metadata_arguments.stuff=['get_latest_filepath_from_metadata_for_analytics',URI,resource_dir,".csv", "sessions"+project_ID+"_ANALYTICS_STEP2_", file_path_csv]
+print(get_latest_filepath_from_metadata_arguments.stuff)
 get_latest_filepath_from_metadata_for_analytics(get_latest_filepath_from_metadata_arguments)
 sessions_list=os.path.join(working_dir,'sessions.csv')
 time_now=datetime.datetime.now().strftime('%Y%m%d%H%M%S')
-copy_session=os.path.join(dir_to_receive_the_data,os.path.basename(sessions_list).split('.csv')[0]+project_ID+'_ANALYTICS_STEP2_'+time_now+'.csv')
+copy_session=os.path.join(dir_to_receive_the_data,os.path.basename(sessions_list).split('.csv')[0]+project_ID+'_ANALYTICS_STEP3_'+time_now+'.csv')
 download_a_single_file(file_path_csv,dir_to_receive_the_data,project_ID,os.path.basename(copy_session))
+copy_session_df=pd.read_csv(copy_session)
+counter=0
+# dir_to_save=output_directory
+
 # #
 # copy_session_df=pd.read_csv(copy_session)
 # counter=0

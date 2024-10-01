eb6b55b031a747d3b3b5729d5f1d7587b0d9700b
dharlabwustl <atulkumar@wustl.edu>
2024-10-01 12:43:51 -0500

    New Commitdgfdgf

--- bringtooriginalrefframe.sh
+++ bringtooriginalrefframe.sh
@@ -302,7 +302,7 @@ for niftifile_csvfilename in ${working_dir}/*NIFTILOCATION.csv; do
       ## CALCULATE EDEMA BIOMARKERS
       nwucalculation_each_scan
       cp ${working_dir}/*.mat  ${output_directory}/
-      cp ${working_dir}/*.nii  ${output_directory}/
+#      cp ${working_dir}/*.nii  ${output_directory}/
       cp ${working_dir}/*_resaved_csf_unet.nii.gz  ${output_directory}/
       cp ${working_dir}/*resaved_levelset_brain_f.nii.gz ${output_directory}/
       cp ${working_dir}/*resaved_levelset_bet.nii.gz ${output_directory}/

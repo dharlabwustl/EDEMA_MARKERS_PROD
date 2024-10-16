531929e00ba24d234785699ed0117e9ba99f75f4
dharlabwustl <atulkumar@wustl.edu>
2024-10-16 16:41:48 -0500

    New Commitdgfdgf

--- linear_rigid_registration_onlytrasnformwith_matfile10162024.sh
+++ linear_rigid_registration_onlytrasnformwith_matfile10162024.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 moving_image_filename=${1}
 fixed_image_filename=${2}
-output_directory=${3}
+T_output_filename=${3}
+output_directory=${4}
 output_filename=${output_directory}/'mov_'$(basename ${moving_image_filename%.nii*})_fixed_$(basename  ${fixed_image_filename%.nii*})_lin1
-T_output_filename=${output_filename}.mat
+#T_output_filename=${output_filename}.mat
 echo "RUNNING:: /usr/lib/fsl/5.0/flirt -in ${moving_image_filename} -ref ${fixed_image_filename} -out "${output_filename}" -init ${T_output_filename}  -applyxfm"
 /usr/lib/fsl/5.0/flirt -in ${moving_image_filename} -ref ${fixed_image_filename} -out "${output_filename}" -init ${T_output_filename}  -applyxfm

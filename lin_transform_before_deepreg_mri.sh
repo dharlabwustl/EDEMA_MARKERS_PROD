3e20ea8c44f9ad82ccee94c004e58840dfd27d5e
dharlabwustl <atulkumar@wustl.edu>
2024-11-04 19:14:09 -0600

    New Commitdgfdgf

--- lin_transform_before_deepreg_mri.sh
+++ lin_transform_before_deepreg_mri.sh
@@ -338,9 +338,9 @@ echo $file
 function_with_arguments=('call_gray2binary' ${file}  $(dirname ${file}) 1 )
 echo "outputfiles_present="'$(python3 utilities_simple_trimmed.py' "${function_with_arguments[@]}"
 outputfiles_present=$(python3 utilities_simple_trimmed.py "${function_with_arguments[@]}")
-
+file1=${file%.nii*}_BET.nii.gz
 echo "uploadsinglefile ${sessionID} ${scanID} $(dirname ${file}) ${snipr_output_foldername} $(basename ${file} )"
-uploadsinglefile ${sessionID} ${scanID} $(dirname ${file}) ${snipr_output_foldername} $(basename ${file} )
+uploadsinglefile ${sessionID} ${scanID} $(dirname ${file1}) ${snipr_output_foldername} $(basename ${file1} )
 done
 all_moved_files=$(find ${output_directory} -name 'mov_'* )
 for file in ${all_moved_files} ; do

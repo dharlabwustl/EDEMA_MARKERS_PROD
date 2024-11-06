0d61f3c251534eb64a052ef1065f8fe67dbae492
dharlabwustl <atulkumar@wustl.edu>
2024-11-06 11:20:08 -0600

    New Commitdgfdgf

--- lin_transform_before_deepreg_mri.sh
+++ lin_transform_before_deepreg_mri.sh
@@ -279,7 +279,7 @@ get_maskfile_scan_metadata()" ${sessionId} ${scanId} ${resource_foldername} ${di
 scanID='MRI1'
 snipr_output_foldername='PREPROCESS_LINR'
 function_with_arguments=('call_delete_file_with_ext' ${sessionID} ${scanID} ${snipr_output_foldername} '*.nii.gz' 'warped_1_mov_mri_region_' )
-echo "outputfiles_present="'$(python3 utilities_simple_trimmed.py' "${function_with_arguments[@]}"
+echo "outputfiles_present="'$(python3 download_with_session_ID.py' "${function_with_arguments[@]}"
 outputfiles_present=$(python3 utilities_simple_trimmed.py "${function_with_arguments[@]}")
 # download the niftifile
 #sessionID=$sessionID, scanID=$scanID , resource_dir=NIFTI

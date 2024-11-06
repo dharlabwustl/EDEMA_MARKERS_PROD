a1b43826a262bed7cb2672b5becfb6d35abf7b1a
dharlabwustl <atulkumar@wustl.edu>
2024-11-06 12:03:22 -0600

    New Commitdgfdgf

--- download_with_session_ID.py
+++ download_with_session_ID.py
@@ -2333,17 +2333,18 @@ def delete_file_with_ext(session_id,scan_id,resource_dir,extensions_to_delete,pr
         df_scan = pd.read_json(json.dumps(metadata_masks))
         #         extensions_to_delete=['_resaved_levelset_sulci_above_ventricle.nii.gz','_resaved_levelset_sulci_at_ventricle.nii.gz','_resaved_levelset_sulci_below_ventricle.nii.gz',
         #                                  '_resaved_levelset_sulci_total.nii.gz','_resaved_levelset_ventricle_total.nii.gz']
-        for each_extension in extensions_to_delete:
-            matched_rows = df_scan[df_scan['URI'].str.contains(each_extension, case=False, na=False)]
-            command = "echo  success at  DELETED: " +  inspect.stack()[0][3]  + " >> " + "/output/error.txt"
-            subprocess.call(command,shell=True)
-            if len(prefix_if_any)>0:
-                matched_rows = matched_rows[matched_rows['URI'].str.contains(prefix_if_any, case=False, na=False)]
+        # for each_extension in extensions_to_delete:
+        matched_rows = df_scan[df_scan['URI'].str.contains(extensions_to_delete, case=False, na=False)]
+        command = "echo  success at  DELETED: " +  inspect.stack()[0][3]  + " >> " + "/output/error.txt"
+        subprocess.call(command,shell=True)
+        if len(prefix_if_any)>0:
+            matched_rows = matched_rows[matched_rows['URI'].str.contains(prefix_if_any, case=False, na=False)]
 
-            if matched_rows.shape[0]>0:
-                matched_rows=matched_rows.reset_index()
-                print(matched_rows)
-                url=matched_rows.at[0,'URI']
+        if matched_rows.shape[0]>0:
+            matched_rows=matched_rows.reset_index()
+            print(matched_rows)
+            for each_row_id,each_row in matched_rows.iterrows():
+                url=each_row['URI'] #matched_rows.at[0,'URI']
                 print(url)
                 delete_a_file_with_URIString(url)
                 print("DELETED::{}".format(url))

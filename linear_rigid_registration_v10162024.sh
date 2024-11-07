942af7c09a62da85324cf0e78512ae379c41d73c
dharlabwustl <atulkumar@wustl.edu>
2024-11-06 18:00:55 -0600

    New Commitdgfdgf

--- linear_rigid_registration_v10162024.sh
+++ linear_rigid_registration_v10162024.sh
@@ -4,4 +4,4 @@ fixed_image_filename=${2}
 output_directory=${3}
 output_filename=${output_directory}/'mov_'$(basename ${moving_image_filename%.nii*})_fixed_$(basename  ${fixed_image_filename%.nii*})_lin1
 T_output_filename=${output_filename}.mat
-/usr/lib/fsl/5.0/flirt  -in "${moving_image_filename}" -ref "${fixed_image_filename}"  -dof 12 -out "${output_filename}" -omat ${T_output_filename}
+/usr/lib/fsl/5.0/flirt  -in "${moving_image_filename}" -ref "${fixed_image_filename}"  -dof 12 -out "${output_filename}" -omat ${T_output_filename} -cost mutualinfo -searchrx -10 10 -searchry -10 10 -searchrz -10 10

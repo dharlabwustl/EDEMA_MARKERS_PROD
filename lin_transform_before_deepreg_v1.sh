fbbafc82d387e63bec33391b656cf2623f8809b2
dharlabwustl <atulkumar@wustl.edu>
2024-11-06 19:41:01 -0600

    New Commitdgfdgf

--- lin_transform_before_deepreg_v1.sh
+++ lin_transform_before_deepreg_v1.sh
@@ -414,7 +414,7 @@ snipr_output_foldername="PREPROCESS_SEGM"
 #    file_name=str(sys.argv[5])
 /software/bet_withlevelset.sh ${registration_nii_file} ${mask_binary_output_dir}/${bet_binary_output_filename%.nii*}_BET.nii.gz
 full_image_filename=${registration_nii_file}
-full_image_filename_betgray=${full_image_filename%.nii*}_brain_f.nii.gz
+full_image_filename_betgray=${mask_binary_output_dir}/$(basename ${full_image_filename%.nii*}_brain_f.nii.gz)
 
 uploadsinglefile ${sessionID} ${scanID} ${mask_binary_output_dir} ${snipr_output_foldername} ${bet_binary_output_filename%.nii*}_BET.nii.gz
 uploadsinglefile ${sessionID} ${scanID} $(dirname ${full_image_filename_betgray}) ${snipr_output_foldername} $(basename  ${full_image_filename_betgray})

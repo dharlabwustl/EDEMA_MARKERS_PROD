23f0ce7a521a6f28fb95d384ca977cda08cc39ed
dharlabwustl <atulkumar@wustl.edu>
2024-11-20 22:20:16 -0600

    New Commitdgfdgf

--- lin_transform_before_deepreg_v2.sh
+++ lin_transform_before_deepreg_v2.sh
@@ -410,7 +410,7 @@ outputfiles_present=$(python3 utilities_simple_trimmed.py "${function_with_argum
 snipr_output_foldername="PREPROCESS_SEGM"
 #snipr_output_foldername='PREPROCESS_SEGM'
 function_with_arguments=('call_delete_file_with_ext' ${sessionID} ${scanID} ${snipr_output_foldername} '.nii.gz' ) ##'warped_1_mov_mri_region_' )
-echo "outputfiles_present=$(python3 download_with_session_ID.py ${function_with_arguments[@]})"
+echo "outputfiles_present="'$(python3 utilities_simple_trimmed.py' "${function_with_arguments[@]}"
 outputfiles_present=$(python3 download_with_session_ID.py "${function_with_arguments[@]}")
 #
 ##    sessionId=str(sys.argv[1])

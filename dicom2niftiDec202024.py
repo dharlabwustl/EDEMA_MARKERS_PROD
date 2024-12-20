51af3229033a3472b1b27c73fc71fcced7398de7
dharlabwustl <atulkumar@wustl.edu>
2024-12-20 13:49:23 -0600

    New Commitdgfdgf

--- dicom2niftiDec202024.py
+++ dicom2niftiDec202024.py
@@ -46,7 +46,7 @@ if __name__ == "__main__":
     import sys
     # Ensure input arguments are provided
     if len(sys.argv) != 1:
-        print("Usage: python run_pyradiomics.py <grayscale_image> <mask_image>")
+        print("Usage: python dicom2niftiDec202024.py <grayscale_image> <mask_image>")
         sys.exit(1)
     session_id = sys.argv[1]
     try:

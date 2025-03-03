3f4aee2d73b2355338d445d2cbca20c86357c450
dharlabwustl <atulkumar@wustl.edu>
2025-03-03 14:55:45 -0600

    New Commitdgfdgf

--- lin_transform_before_deepreg_scct_as_moving.sh
+++ lin_transform_before_deepreg_scct_as_moving.sh
@@ -293,7 +293,7 @@ while IFS=',' read -ra array; do
 scanID=${array[2]}
 echo sessionId::${sessionID}
 echo scanId::${scanID}
-snipr_output_foldername="PREPROCESS_SEGM"
+snipr_output_foldername="PREPROCESS_SEGM_2"
 ### check if the file exists:
 call_check_if_a_file_exist_in_snipr_arguments=('call_check_if_a_file_exist_in_snipr' ${sessionID} ${scanID} ${snipr_output_foldername} .pdf .csv)
 outputfiles_present=$(python3 download_with_session_ID.py "${call_check_if_a_file_exist_in_snipr_arguments[@]}")
@@ -327,7 +327,7 @@ done < <(tail -n +2 "${niftifile_csvfilename}")
 echo working_dir::${working_dir}
 echo output_dirname::${output_dirname}
 copy_masks_data ${sessionID} ${scanID} ${resource_dirname} ${output_dirname}
-resource_dirname='PREPROCESS_SEGM'
+resource_dirname='PREPROCESS_SEGM_2'
 copy_masks_data ${sessionID} ${scanID} ${resource_dirname} ${output_dirname}
 resource_dirname='EDEMA_BIOMARKER'
 copy_masks_data ${sessionID} ${scanID} ${resource_dirname} ${output_dirname}
@@ -339,16 +339,16 @@ session_ct=$( ls ${working_dir_1}/*'.nii' )
 session_ct_bname_noext=$(basename ${session_ct})
 session_ct_bname_noext=${session_ct_bname_noext%.nii*}
 #fixed_image_filename='/software/COLIHM620406202215542.nii.gz'  ####${template_prefix}.nii.gz'
-infarct_mask_from_yasheng=$(ls ${working_dir}/${nifti_file_without_ext}*_resaved_infarct_auto_removesmall.nii.gz)
+#infarct_mask_from_yasheng=$(ls ${working_dir}/${nifti_file_without_ext}*_resaved_infarct_auto_removesmall.nii.gz)
 #      template_masks_dir='/software/mritemplate/NONLINREGTOCT/' 	COLI_HM62_04062022_1554_2_resaved_csf_unet.nii.gz
 bet_mask_from_yasheng=$(ls ${working_dir}/${nifti_file_without_ext}*_resaved_levelset_bet.nii.gz)
-csf_mask_from_yasheng=$(ls ${working_dir}/${nifti_file_without_ext}*_resaved_csf_unet.nii.gz)
-echo "levelset_bet_mask_file:${levelset_bet_mask_file}"
-python3 -c "
-
-import sys ;
-sys.path.append('/software/') ;
-from utilities_simple_trimmed import * ;  levelset2originalRF_new_flip()" "${session_ct}" "${infarct_mask_from_yasheng}" "${output_directory}"
+#csf_mask_from_yasheng=$(ls ${working_dir}/${nifti_file_without_ext}*_resaved_csf_unet.nii.gz)
+#echo "levelset_bet_mask_file:${levelset_bet_mask_file}"
+#python3 -c "
+#
+#import sys ;
+#sys.path.append('/software/') ;
+#from utilities_simple_trimmed import * ;  levelset2originalRF_new_flip()" "${session_ct}" "${infarct_mask_from_yasheng}" "${output_directory}"
 
 python3 -c "
 
@@ -356,11 +356,11 @@ import sys ;
 sys.path.append('/software/') ;
 from utilities_simple_trimmed import * ;  levelset2originalRF_new_flip()" "${session_ct}" "${bet_mask_from_yasheng}" "${output_directory}"
 
-python3 -c "
-
-import sys ;
-sys.path.append('/software/') ;
-from utilities_simple_trimmed import * ;  levelset2originalRF_new_flip()" "${session_ct}" "${csf_mask_from_yasheng}" "${output_directory}"
+#python3 -c "
+#
+#import sys ;
+#sys.path.append('/software/') ;
+#from utilities_simple_trimmed import * ;  levelset2originalRF_new_flip()" "${session_ct}" "${csf_mask_from_yasheng}" "${output_directory}"
 
 
 # now let us make bet gray for session ct:
@@ -368,11 +368,12 @@ from utilities_simple_trimmed import * ;  levelset2originalRF_new_flip()" "${ses
 
 
 moving_image_filename=/software/scct_strippedResampled1.nii.gz ###COLIHM620406202215542.nii.gz ##'  ####${template_prefix}.nii.gz ##${session_ct_bet_gray}
-template_prefix=${session_ct_bname_noext} ##'COLIHM620406202215542'
+##'COLIHM620406202215542'
 fixed_image_filename=${output_directory}/${session_ct_bname_noext}_brain_f.nii.gz
-
+template_prefix=$(basename ${session_ct_bname_noext%.nii*})
 /software/linear_rigid_registration_v10162024.sh ${moving_image_filename}  ${fixed_image_filename} ${output_directory}
-session_ct_bet_gray_lin_reg_output=${output_directory}/'mov_'$(basename ${moving_image_filename%.nii*})_fixed_$(basename  ${fixed_image_filename%.nii*})_lin1.nii.gz
+#session_ct_bet_gray_lin_reg_output=${output_directory}/'mov_'$(basename ${moving_image_filename%.nii*})_fixed_$(basename  ${fixed_image_filename%.nii*})_lin1.nii.gz
+#movingimage_gray_registered=${output_directory}/'mov_'$(basename ${moving_image_filename%.nii*})_fixed_$(basename  ${fixed_image_filename%.nii*})_lin1.nii.gz
 
 moving_image_filename=$(basename ${moving_image_filename})
 registration_mat_file=${output_directory}/'mov_'$(basename ${moving_image_filename%.nii*})_fixed_$(basename  ${fixed_image_filename%.nii*})_lin1.mat

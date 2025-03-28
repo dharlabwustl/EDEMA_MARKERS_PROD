ae5790b664fbeca4e8fe7e4c2779a61190254c3a
dharlabwustl <atulkumar@wustl.edu>
2025-03-28 15:52:51 -0500

    New Commitdgfdgf

--- infarct_in_each_arterial_regionsNov262024.py
+++ infarct_in_each_arterial_regionsNov262024.py
@@ -735,7 +735,7 @@ def arterial_region_volumes_n_display(SESSION_ID):
         #                            .replace(r'\textbackslash includegraphics',r'\includegraphics'))
 
         latex_insert_line_nodek(latexfilename,text=latex_table)
-        binarized_region_artery(csvfilename.split('.csv')[0]+'_Transpose.csv',binarized_region_artery)
+        binarized_region_artery(csvfilename.split('.csv')[0]+'_Transpose.csv',latexfilename)
         # latex_end_table2c(latexfilename)
         command="echo " + "start" + " >> /workingoutput/error.txt"
         subprocess.call(command,shell=True)

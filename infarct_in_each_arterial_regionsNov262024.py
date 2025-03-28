ee389355cf85527c523c143831d9573612e3ab32
dharlabwustl <atulkumar@wustl.edu>
2025-03-28 16:04:22 -0500

    New Commitdgfdgf

--- infarct_in_each_arterial_regionsNov262024.py
+++ infarct_in_each_arterial_regionsNov262024.py
@@ -203,7 +203,7 @@ def binarized_region_artery(f,latexfilename):
             print(all_regions_df)
             subprocess.call("echo " + "I  of try 1_1 ::{}  >> /workingoutput/error.txt".format(f) ,shell=True )
             all_regions_df.to_csv(f.split('.csv')[0]+"_"+str(thresh_percentage)+"_binarized.csv",index=False)
-            latex_insert_line_nodek(latexfilename,text='THRESHOLD::{}'.format(str(thresh_percentage)))
+            latex_insert_line_nodek(latexfilename,text='THRESHOLD::{}\n'.format(str(thresh_percentage)))
             # latex_table = df_to_latex_1(all_regions_df)
             latex_insert_line_nodek(latexfilename,text=all_regions_df.to_latex(index=False))
 

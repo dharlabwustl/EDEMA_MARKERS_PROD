bc2a13f371f7c633caf8cb875beb681df8c1b579
dharlabwustl <atulkumar@wustl.edu>
2025-04-01 14:09:34 -0500

    New Commitdgfdgf

--- infarct_in_each_lobar_regionsNov262024.py
+++ infarct_in_each_lobar_regionsNov262024.py
@@ -572,6 +572,7 @@ def lobar_region_volumes_n_display(SESSION_ID):
 
     # latexfilename=create_a_latex_filename(filename_prefix,filename_to_write)
     latex_start(latexfilename)
+    latex_additionalPackages(latexfilename,["pdflscape","longtable"])
     latex_begin_document(latexfilename)
 
     # latex_start_table1c(latexfilename)

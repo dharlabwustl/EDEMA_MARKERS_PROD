b19758271b430ccd3035deb41658a2d7363d1d3a
dharlabwustl <atulkumar@wustl.edu>
2025-04-01 12:29:52 -0500

    New Commitdgfdgf

--- call_arterial_location_distribution.py
+++ call_arterial_location_distribution.py
@@ -2,6 +2,8 @@
 # from infarct_in_each_aretrial_regions_v2 import *
 import sys,os,glob,subprocess,argparse
 from infarct_in_each_arterial_regionsNov262024 import *
+from infarct_in_each_lobar_regionsNov262024 import *
+import traceback
 from binarize_regions_arterial_involvement import *
 def clean_dirs():
     command='rm -r /workinginput/*'
@@ -57,6 +59,38 @@ def call_arterial_location_distribution(args): #SESSION_ID):
         error_msg = traceback.format_exc()
         subprocess.call("echo " + "I traceback error_msg  ::{}  >> /workingoutput/error.txt".format(error_msg) ,shell=True )
         return 0
+def call_lobar_location_distribution(args): #SESSION_ID):
+    return_value=0
+    try:
+        clean_dirs()
+        SESSION_ID=args.stuff[1] ##str(row_item['ID'])
+        print(SESSION_ID)
+        return_value=lobar_region_volumes_n_display(SESSION_ID)
+        subprocess.call("echo " + "I call_lobar_location_distribution return_value  ::{}  >> /workingoutput/error.txt".format(return_value) ,shell=True )
+
+        # if return_value==1:
+        #     subprocess.call("echo " + "I call_arterial_location_distribution return_value  ::{}  >> /workingoutput/error.txt".format(return_value) ,shell=True )
+        #     binarized_region_artery()
+        return  return_value
+
+        # error_msg = traceback.format_exc()
+        # subprocess.call("echo " + "I traceback error  ::{}  >> /workingoutput/error.txt".format("error_msg") ,shell=True )
+    except Exception as e :
+        # command = "echo error is " + str(e) + " >>" + "/workingoutput/error.txt"
+        # subprocess.call(command,shell=True)
+        # print(e)
+        # pass
+        # except Exception as e :
+        # command = "echo error is " + str(e) + " >>" + "/workingoutput/error.txt"
+        # subprocess.call(command,shell=True)
+        # print(e)
+        # pass
+        # except Exception as e:
+        error_msg = traceback.format_exc()
+        subprocess.call("echo " + "I traceback error_msg  ::{}  >> /workingoutput/error.txt".format(error_msg) ,shell=True )
+        return 0
+
+
 def main():
     print("WO ZAI ::{}".format("main"))
     parser = argparse.ArgumentParser()
@@ -66,7 +100,8 @@ def main():
     return_value=0
     if name_of_the_function=="call_arterial_location_distribution":
         return_value=call_arterial_location_distribution(args)
-
+    if name_of_the_function=="call_lobar_location_distribution":
+        return_value=call_lobar_location_distribution(args)
     return  return_value
 if __name__ == "__main__":
     main()
--- call_location_distribution_py.sh
+++ call_location_distribution_py.sh
@@ -9,9 +9,13 @@ output_directory=/workingoutput
 
 final_output_directory=/outputinsidedocker
 
-function_with_arguments=('call_arterial_location_distribution' ${sessionID} ) ## ${scanID} ${snipr_output_foldername} 'cistern_area' ) ##'warped_1_mov_mri_region_' )
-echo "outputfiles_present="'$(python3 call_arterial_location_distribution.py' "${function_with_arguments[@]}"
-outputfiles_present=$(python3 call_arterial_location_distribution.py "${function_with_arguments[@]}")
+#function_with_arguments=('call_arterial_location_distribution' ${sessionID} ) ## ${scanID} ${snipr_output_foldername} 'cistern_area' ) ##'warped_1_mov_mri_region_' )
+#echo "outputfiles_present="'$(python3 call_arterial_location_distribution.py' "${function_with_arguments[@]}"
+#outputfiles_present=$(python3 call_arterial_location_distribution.py "${function_with_arguments[@]}")
+
+function_with_arguments=('call_lobar_location_distribution' ${sessionID} ) ## ${scanID} ${snipr_output_foldername} 'cistern_area' ) ##'warped_1_mov_mri_region_' )
+echo "outputfiles_present="'$(python3 call_lobar_location_distribution.py' "${function_with_arguments[@]}"
+outputfiles_present=$(python3 call_lobar_location_distribution.py "${function_with_arguments[@]}")
 #
 ### get the scan used in this session
 #

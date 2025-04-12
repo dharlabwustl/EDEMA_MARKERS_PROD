42c9a0c8717031d2e8764b7a6dd78202a08a5d02
dharlabwustl <atulkumar@wustl.edu>
2025-04-11 19:09:32 -0500

    New Commitdgfdgf

--- lin_transform_before_deepreg_COLIHM62_as_moving_mat_on_mask.sh
+++ lin_transform_before_deepreg_COLIHM62_as_moving_mat_on_mask.sh
@@ -293,11 +293,11 @@ while IFS=',' read -ra array; do
 scanID=${array[2]}
 echo sessionId::${sessionID}
 echo scanId::${scanID}
-snipr_output_foldername="PREPROCESS_SEGM_2"
-function_with_arguments=('call_delete_file_with_ext' ${sessionID} ${scanID} ${snipr_output_foldername} 'cistern_area' ) ##'warped_1_mov_mri_region_' )
+snipr_output_foldername="PREPROCESS_SEGM_3"
+function_with_arguments=('call_delete_file_with_ext' ${sessionID} ${scanID} ${snipr_output_foldername} 'CISTERN_COLIHM62' ) ##'warped_1_mov_mri_region_' )
 echo "outputfiles_present="'$(python3 utilities_simple_trimmed.py' "${function_with_arguments[@]}"
 outputfiles_present=$(python3 download_with_session_ID.py "${function_with_arguments[@]}")
-snipr_output_foldername="PREPROCESS_SEGM_2"
+snipr_output_foldername="PREPROCESS_SEGM_3"
 ### check if the file exists:
 call_check_if_a_file_exist_in_snipr_arguments=('call_check_if_a_file_exist_in_snipr' ${sessionID} ${scanID} ${snipr_output_foldername} .pdf .csv)
 outputfiles_present=$(python3 download_with_session_ID.py "${call_check_if_a_file_exist_in_snipr_arguments[@]}")
@@ -331,7 +331,7 @@ done < <(tail -n +2 "${niftifile_csvfilename}")
 echo working_dir::${working_dir}
 echo output_dirname::${output_dirname}
 copy_masks_data ${sessionID} ${scanID} ${resource_dirname} ${output_dirname}
-resource_dirname='PREPROCESS_SEGM_2'
+resource_dirname='PREPROCESS_SEGM_3'
 copy_masks_data ${sessionID} ${scanID} ${resource_dirname} ${output_dirname}
 resource_dirname='EDEMA_BIOMARKER'
 copy_masks_data ${sessionID} ${scanID} ${resource_dirname} ${output_dirname}
@@ -371,7 +371,7 @@ from utilities_simple_trimmed import * ;  levelset2originalRF_new_flip()" "${ses
 /software/bet_withlevelset.sh ${session_ct} ${output_directory}/$(basename ${bet_mask_from_yasheng})
 
 
-moving_image_filename=/software/scct_strippedResampled1.nii.gz ###COLIHM620406202215542.nii.gz ##'  ####${template_prefix}.nii.gz ##${session_ct_bet_gray}
+moving_image_filename=/software/COLIHM620406202215542.nii.gz ###COLIHM620406202215542.nii.gz ##'  ####${template_prefix}.nii.gz ##${session_ct_bet_gray}
 ##'COLIHM620406202215542'
 fixed_image_filename=${output_directory}/${session_ct_bname_noext}_brain_f.nii.gz
 template_prefix=$(basename ${fixed_image_filename%.nii*})
@@ -384,9 +384,9 @@ registration_mat_file=${working_dir}/'mov_'$(basename ${moving_image_filename%.n
 
 registration_nii_file=${working_dir}/'mov_'$(basename ${moving_image_filename%.nii*})_fixed_$(basename  ${fixed_image_filename%.nii*})_lin1.nii.gz
 #################################################################################################
-moving_image_filename=/software/scct_strippedResampled1_onlyventricle.nii.gz #${output_directory}/${moving_image_filename} ##%.nii*}resampled_mov.nii.gz
+moving_image_filename=/software/VENTRICLE_COLIHM62.nii.gz #${output_directory}/${moving_image_filename} ##%.nii*}resampled_mov.nii.gz
 mask_binary_output_dir='/input'
-snipr_output_foldername="PREPROCESS_SEGM_2"
+snipr_output_foldername="PREPROCESS_SEGM_3"
 
 /software/linear_rigid_registration_onlytrasnformwith_matfile10162024.sh  ${moving_image_filename} ${fixed_image_filename} ${registration_mat_file} ${mask_binary_output_dir}
 mask_binary_output_filename=mov_$(basename ${moving_image_filename%.nii*})_fixed_${template_prefix}_lin1.nii.gz
@@ -400,7 +400,7 @@ uploadsinglefile ${sessionID} ${scanID} $(dirname ${infarct_mask_binary_output_f
 #################################################################################################
 moving_image_filename=/software/midlinecssfResampled1.nii.gz #${output_directory}/${moving_image_filename} ##%.nii*}resampled_mov.nii.gz
 mask_binary_output_dir='/input'
-snipr_output_foldername="PREPROCESS_SEGM_2"
+snipr_output_foldername="PREPROCESS_SEGM_3"
 /software/linear_rigid_registration_onlytrasnformwith_matfile10162024.sh  ${moving_image_filename} ${fixed_image_filename} ${registration_mat_file} ${mask_binary_output_dir}
 mask_binary_output_filename=mov_$(basename ${moving_image_filename%.nii*})_fixed_${template_prefix}_lin1.nii.gz
 threshold=0
@@ -411,10 +411,10 @@ infarct_mask_binary_output_filename=${mask_binary_output_dir}/${mask_binary_outp
 uploadsinglefile ${sessionID} ${scanID} $(dirname ${infarct_mask_binary_output_filename}) ${snipr_output_foldername} $(basename  ${infarct_mask_binary_output_filename})
 #######################################################################################################
 #################################################################################################
-moving_image_filename=/software/cistern_area.nii.gz #${output_directory}/${moving_image_filename} ##%.nii*}resampled_mov.nii.gz
+moving_image_filename=/software/CISTERN_COLIHM62.nii.gz #${output_directory}/${moving_image_filename} ##%.nii*}resampled_mov.nii.gz
 mask_binary_output_dir='/input'
-snipr_output_foldername="PREPROCESS_SEGM_2"
-function_with_arguments=('call_delete_file_with_ext' ${sessionID} ${scanID} ${snipr_output_foldername} 'cistern_area' ) ##'warped_1_mov_mri_region_' )
+snipr_output_foldername="PREPROCESS_SEGM_3"
+function_with_arguments=('call_delete_file_with_ext' ${sessionID} ${scanID} ${snipr_output_foldername} 'CISTERN_COLIHM62' ) ##'warped_1_mov_mri_region_' )
 echo "outputfiles_present="'$(python3 utilities_simple_trimmed.py' "${function_with_arguments[@]}"
 outputfiles_present=$(python3 download_with_session_ID.py "${function_with_arguments[@]}")
 
@@ -428,7 +428,7 @@ infarct_mask_binary_output_filename=${mask_binary_output_dir}/${mask_binary_outp
 uploadsinglefile ${sessionID} ${scanID} $(dirname ${infarct_mask_binary_output_filename}) ${snipr_output_foldername} $(basename  ${infarct_mask_binary_output_filename})
 #######################################################################################################
 ##########################################
-#snipr_output_foldername="PREPROCESS_SEGM_2"
+#snipr_output_foldername="PREPROCESS_SEGM_3"
 ##snipr_output_foldername='PREPROCESS_SEGM'
 #function_with_arguments=('call_delete_file_with_ext' ${sessionID} ${scanID} ${snipr_output_foldername} '.nii.gz' ) ##'warped_1_mov_mri_region_' )
 #echo "outputfiles_present="'$(python3 utilities_simple_trimmed.py' "${function_with_arguments[@]}"

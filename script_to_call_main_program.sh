8644dbee2c4b70801ba25a4c3013aa401f86750d
dharlabwustl <atulkumar@wustl.edu>
2025-05-06 15:17:59 -0500

    New Commitdgfdgf

--- script_to_call_main_program.sh
+++ script_to_call_main_program.sh
@@ -39,13 +39,13 @@ fi
 if [[ ${TYPE_OF_PROGRAM} == 'APPLY_MAT_TRANFORM_BEFORE_DEEPREG_SCCT_MOVING' ]]; then
   echo " I AM AT APPLY_MAT_TRANFORM_BEFORE_DEEPREG_SCCT_MOVING" >> /software/ERROR.txt
   echo " I AM AT APPLY_MAT_TRANFORM_BEFORE_DEEPREG_SCCT_MOVING"
-  /software/lin_transform_before_deepreg_scct_as_moving_mat_on_mask_v1.sh $SESSION_ID $XNAT_USER $XNAT_PASS $XNAT_HOST /input /output
+  /software/lin_transform_before_deepreg_scct_as_moving_mat_on_mask.sh $SESSION_ID $XNAT_USER $XNAT_PASS $XNAT_HOST /input /output
 fi
 
 if [[ ${TYPE_OF_PROGRAM} == 'APPLY_MAT_TRANFORM_BEFORE_DEEPREG_COLIHM62_MOVING' ]]; then
   echo " I AM AT APPLY_MAT_TRANFORM_BEFORE_DEEPREG_COLIHM62_MOVING" >> /software/ERROR.txt
   echo " I AM AT APPLY_MAT_TRANFORM_BEFORE_DEEPREG_COLIHM62_MOVING"
-  /software/lin_transform_before_deepreg_COLIHM62_as_moving_mat_on_mask.sh $SESSION_ID $XNAT_USER $XNAT_PASS $XNAT_HOST /input /output
+  /software/lin_transform_before_deepreg_COLIHM62_as_moving_mat_on_mask_v1.sh $SESSION_ID $XNAT_USER $XNAT_PASS $XNAT_HOST /input /output
 fi
 
 if [[ ${TYPE_OF_PROGRAM} == 'ARTERIAL_REGIONS' ]]; then

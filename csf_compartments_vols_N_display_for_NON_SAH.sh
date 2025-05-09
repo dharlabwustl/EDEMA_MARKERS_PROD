1821bc9ed615e8d0f6cfc7c45851d20cd13167c8
dharlabwustl <atulkumar@wustl.edu>
2025-05-08 21:01:58 -0500

    New Commitdgfdgf

--- csf_compartments_vols_N_display_for_NON_SAH.sh
+++ csf_compartments_vols_N_display_for_NON_SAH.sh
@@ -845,7 +845,7 @@ done < <(tail -n +2 "${working_dir}/${output_csvfile}")
 #call_download_a_singlefile_with_URIString_arguments=('call_download_a_singlefile_with_URIString' ${url} ${filename} ${working_dir})
 #outputfiles_present=$(python3 download_with_session_ID.py "${call_download_a_singlefile_with_URIString_arguments[@]}")
 unzip  ${working_dir}/MIDLINENPYFILES.zip #-d ${working_dir_1}
-mv ${working_dir}/*.npy ${working_dir_1}/
+mv /software/*.npy ${working_dir_1}/
 # mv ${working_dir_1}/MIDLINENPYFILES/*.npy  ${working_dir_1}/
 ################ DOWNLOAD MASKS ###############################
 ## METADATA in the MASK directory

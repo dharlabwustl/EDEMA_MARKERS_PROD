f3b4f6ce33941d70fce3940e5d5ea28482632e90
dharlabwustl <atulkumar@wustl.edu>
2025-05-12 21:28:40 -0500

    20250512

--- infarct_in_each_arterial_regionsNov262024.py
+++ infarct_in_each_arterial_regionsNov262024.py
@@ -682,6 +682,9 @@ def arterial_region_volumes_n_display(SESSION_ID):
         SCAN_ID,SCAN_NAME=get_selected_scan_info(SESSION_ID,file_output_dir)
         print(f'{SCAN_ID}::{SCAN_NAME}')
         # return
+        command=f"echo I am here ::{SCAN_ID}::{SCAN_NAME}:: {inspect.stack()[0][3]}" + ">> /output/error.txt"
+        subprocess.call(command,shell=True)
+        return
         working_dir_1='/input1'
         Version_Date="_VersionDate-" + '11122024' #dt.strftime("%m%d%Y")
         # DOWNLOAD THE REGISTERED INFARCT MASK and the REGISTERED SESSION CT

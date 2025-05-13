550dba1faf7c3f211e51467979643a70dca56bc6
dharlabwustl <atulkumar@wustl.edu>
2025-05-12 21:08:39 -0500

    New Commitdgfdgf

--- download_with_session_ID.py
+++ download_with_session_ID.py
@@ -2631,9 +2631,7 @@ def get_selected_scan_info(SESSION_ID, dir_to_save):
             os.path.basename(str(df_scan['URI'].iloc[0])),
             dir_to_save
         )
-        command=f"echo I am here  {inspect.stack()[0][3]}" + ">> /output/error.txt"
-        subprocess.call(command,shell=True)
-        return
+
         # Read the downloaded file to extract SCAN_ID and SCAN_NAME
         nifti_location = pd.read_csv(
             os.path.join(dir_to_save, os.path.basename(str(df_scan['URI'].iloc[0])))
@@ -2644,7 +2642,9 @@ def get_selected_scan_info(SESSION_ID, dir_to_save):
         # Log success message
         command = f"echo passed at each_row_id: {inspect.stack()[0][3]} >> /output/error.txt"
         subprocess.call(command, shell=True)
-
+        command=f"echo I am here  {inspect.stack()[0][3]}" + ">> /output/error.txt"
+        subprocess.call(command,shell=True)
+        return
         return SCAN_ID, SCAN_NAME
 
     except Exception as e:

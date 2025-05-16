9da6851b3a88444cb46d0bf8efe80be41461a289
dharlabwustl <atulkumar@wustl.edu>
2025-05-16 16:34:30 -0500

    New Commitdgfdgf

--- call_location_distribution_py.sh
+++ call_location_distribution_py.sh
@@ -37,7 +37,7 @@ working_dir_1=/input1
 output_directory=/workingoutput
 
 final_output_directory=/outputinsidedocker
-
+snipr_output_foldername="LOCATION_DISTRIBUTION"
 function_with_arguments=('call_arterial_location_distribution' ${sessionID} ) ## ${scanID} ${snipr_output_foldername} 'cistern_area' ) ##'warped_1_mov_mri_region_' )
 echo "outputfiles_present="'$(python3 call_arterial_location_distribution.py' "${function_with_arguments[@]}"
 outputfiles_present=$(python3 call_arterial_location_distribution.py "${function_with_arguments[@]}")

6c597548a1687cd31c14293b652c17df6a608087
dharlabwustl <atulkumar@wustl.edu>
2025-05-20 12:26:47 -0500

    New Commitdgfdgf

--- infarct_in_each_arterial_regionsNov262024.py
+++ infarct_in_each_arterial_regionsNov262024.py
@@ -1053,6 +1053,7 @@ def arterial_region_volumes_n_display(SESSION_ID):
         uploadsinglefile_with_URI(url,file_name,resource_dirname)
         file_name=latexfilename.split('.tex')[0] +'.csv'
         uploadsinglefile_with_URI(url,file_name,resource_dirname)
+        subprocess.call("echo " + "I file_name ::{}  >> /workingoutput/error.txt".format(file_name) ,shell=True )
         db_table_name=project_name
         csv_file_path=csvfilename
         id_column=SESSION_ID

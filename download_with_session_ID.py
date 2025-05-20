9d7e4c1ae59c2a4c0ad1e75a1eb47d8e0096b12f
dharlabwustl <atulkumar@wustl.edu>
2025-05-20 17:50:29 -0500

    New Commitdgfdgf

--- download_with_session_ID.py
+++ download_with_session_ID.py
@@ -76,7 +76,8 @@ def fill_google_mysql_db_from_csv(db_table_name, csv_file_path, id_column="sessi
         command = f"echo  failed at : Database initialization failed.::" +  inspect.stack()[0][3]  + " >> " + "/output/error1.txt"
         subprocess.call(command,shell=True)
         return
-
+    command = f"echo  failed at : {id_column}::" +  inspect.stack()[0][3]  + " >> " + "/output/error1.txt"
+    subprocess.call(command,shell=True)
     for index, row in df.iterrows():
         session_id = row[id_column]
         for column_name in df.columns:

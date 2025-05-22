3980dcb73162e92ea7a52ed3d2ecee3f9fb3ef4b
dharlabwustl <atulkumar@wustl.edu>
2025-05-22 15:48:01 -0500

    New Commitdgfdgf

---
+++ register_masks_to_a_template.sh
@@ -0,0 +1,11 @@
+# Session ID given
+
+SESSION_ID=${1}
+
+# find the scan id
+
+# donwload respective nifti and relevant masks
+
+# resample the masks to the original nifti
+
+# use transformation matrix to register all of them to the template (rigid registration)

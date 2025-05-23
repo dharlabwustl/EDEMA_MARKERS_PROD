9b669665bd1e7cb54368914ca3e961d341ed4601
dharlabwustl <atulkumar@wustl.edu>
2025-05-23 13:13:49 -0500

    New Commitdgfdgf

--- register_masks_to_a_template.sh
+++ register_masks_to_a_template.sh
@@ -9,14 +9,15 @@ echo ${sessionID}::${scanID}
 
 # donwload respective nifti and relevant masks
 resource_dir='NIFTI'
+download_resource_dir ${sessionID}  ${scanID} ${resource_dir}
 ## download as zip, then unzip and arrange the directory like that in the SNIPR
 #call_download_files_in_a_resource_in_a_session_arguments=('call_dowload_a_folder_as_zip' ${sessionID} ${scanID}  ${resource_dir})
 #outputfiles_present=$(python3 download_with_session_ID.py "${call_download_files_in_a_resource_in_a_session_arguments[@]}")
-curl -u "$XNAT_USER:$XNAT_PASS" -X GET \
-  "${XNAT_HOST}/data/experiments/${sessionID}/scans/${scanID}/resources/${resource_dir}/files?format=zip" \
-  -o "${working_dir}/${sessionID}_${scanID}_${resource_dir}.zip"
-
-echo "curl -u $XNAT_USER:$XNAT_PASS -X GET $XNAT_HOST/data/experiments/${sessionID}/scans/${scanID}/resources/${resource_dir}/files?format=zip > ${sessionID}_${scanID}_${resource_dir}.zip"
+#curl -u "$XNAT_USER:$XNAT_PASS" -X GET \
+#  "${XNAT_HOST}/data/experiments/${sessionID}/scans/${scanID}/resources/${resource_dir}/files?format=zip" \
+#  -o "${working_dir}/${sessionID}_${scanID}_${resource_dir}.zip"
+#
+#echo "curl -u $XNAT_USER:$XNAT_PASS -X GET $XNAT_HOST/data/experiments/${sessionID}/scans/${scanID}/resources/${resource_dir}/files?format=zip > ${sessionID}_${scanID}_${resource_dir}.zip"
 # resample the masks to the original nifti
 
 # use transformation matrix to register all of them to the template (rigid registration)

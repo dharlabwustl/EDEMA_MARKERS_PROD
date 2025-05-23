c2de0b0a8cdcd56eba8de0bb0cb85d80819ccd07
dharlabwustl <atulkumar@wustl.edu>
2025-05-23 13:41:40 -0500

    New Commitdgfdgf

--- register_masks_to_a_template.sh
+++ register_masks_to_a_template.sh
@@ -4,14 +4,20 @@ working_dir=/workinginput
 working_dir_1=/input1
 output_directory=/workingoutput
 sessionID=${1}
+#get the selected scan id
 scanID=$(get_scan_id ${sessionID})
 echo ${sessionID}::${scanID}
-
 # donwload respective nifti and relevant masks
 resource_dir='NIFTI'
 download_resource_dir ${sessionID}  ${scanID} ${resource_dir}
 resource_dir='MASKS'
 download_resource_dir ${sessionID}  ${scanID} ${resource_dir}
+# bring masks from yasheng's work to the original nifti reference frame
+scan_file_basename=$(basename $(ls ${working_dir}/NIFTI/*.nii))
+scan_file_basename_no_ext=${scan_file_basename%.nii*}
+original_ct_file=$(ls ${working_dir}/NIFTI/${scan_file_basename_no_ext}.nii)
+levelset_mask_file=$(ls ${working_dir}/MASKS/${scan_file_basename_no_ext}_resaved_csf_unet.nii.gz)
+to_original_nifti_rf ${original_ct_file} ${levelset_mask_file} ${output_directory}
 ## download as zip, then unzip and arrange the directory like that in the SNIPR
 #call_download_files_in_a_resource_in_a_session_arguments=('call_dowload_a_folder_as_zip' ${sessionID} ${scanID}  ${resource_dir})
 #outputfiles_present=$(python3 download_with_session_ID.py "${call_download_files_in_a_resource_in_a_session_arguments[@]}")

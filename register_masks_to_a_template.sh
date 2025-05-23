5cd2238de277aea028f3633129e4248f7c7aa0a9
dharlabwustl <atulkumar@wustl.edu>
2025-05-23 12:52:02 -0500

    New Commitdgfdgf

--- register_masks_to_a_template.sh
+++ register_masks_to_a_template.sh
@@ -12,6 +12,7 @@ resource_dir='NIFTI'
 ## download as zip, then unzip and arrange the directory like that in the SNIPR
 call_download_files_in_a_resource_in_a_session_arguments=('call_dowload_a_folder_as_zip' ${sessionID} ${scanID}  ${resource_dir})
 outputfiles_present=$(python3 download_with_session_ID.py "${call_download_files_in_a_resource_in_a_session_arguments[@]}")
+curl -u $XNAT_USER:$XNAT_PASS -X GET $XNAT_HOST/data/projects/experiments/${sessionID}/scans/${scanID}/resources/${resource_dir}/files?format=zip > ${sessionID}_${scanID}_${resource_dir}.zip
 
 # resample the masks to the original nifti
 

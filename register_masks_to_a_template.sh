8828eaf75da219f4952ea234e4e920c9f32dfe77
dharlabwustl <atulkumar@wustl.edu>
2025-05-23 17:51:45 -0500

    New Commitdgfdgf

--- register_masks_to_a_template.sh
+++ register_masks_to_a_template.sh
@@ -30,25 +30,27 @@ copy_nifti_parameters ${half_brain_mask}  ${original_ct_file}  ${output_director
 ##half_brain_mask=$(echo "$full_string" | awk -F'_parsehere_' '{print $2}' | xargs)
 ##echo half_brain_mask::${half_brain_mask}
 #mv  ${output_directory}/ ${output_directory}/${scan_file_basename_no_ext}_left_half_brain_nib_img.nii.gz
-#half_brain_mask=${output_directory}/${scan_file_basename_no_ext}_left_half_brain_nib_img.nii.gz
-##
-#levelset_mask_file=$(ls ${working_dir}/MASKS/${scan_file_basename_no_ext}_resaved_levelset_bet.nii.gz)
-#full_string=$(to_original_nifti_rf ${original_ct_file} ${levelset_mask_file} ${output_directory})
-#bet_mask=$(echo "$full_string" | awk -F'_parsehere_' '{print $2}' | xargs)
-#echo bet_mask::${bet_mask}
+half_brain_mask=${output_directory}/'with_copied_params_'left_half_brain_nib_img.nii.gz
+mv $half_brain_mask ${output_directory}/${scan_file_basename_no_ext}_left_half_brain_nib_img.nii.gz
+half_brain_mask=${output_directory}/${scan_file_basename_no_ext}_left_half_brain_nib_img.nii.gz
 #
-### make bet of the gray image:
-##levelset_mask_file=$(ls ${working_dir}/MASKS/${scan_file_basename_no_ext}_resaved_levelset_bet.nii.gz)
-#full_string=$(to_betgray_given_gray_n_binary ${original_ct_file} ${bet_mask} ${output_directory})
-#bet_gray=$(echo "$full_string" | awk -F'_parsehere_' '{print $2}' | xargs)
-#echo bet_gray::${bet_gray}
-#### apply transform matrix to align to the template image:
-#transform_mat_file=$(ls ${working_dir}/EDEMA_BIOMARKER/${scan_file_basename_no_ext}_resaved_levelset_brain_f_scct_strippedResampled1lin1.mat)
-#echo "transform_files_with_given_mat_wrt_sccttemplate ${bet_gray} ${bet_gray%.nii*}_RF_scct.nii.gz ${transform_mat_file}"
-#transform_files_with_given_mat_wrt_sccttemplate ${bet_gray} ${bet_gray%.nii*}_RF_scct.nii.gz ${transform_mat_file}
-#transform_files_with_given_mat_wrt_sccttemplate ${half_brain_mask} ${half_brain_mask%.nii*}_RF_scct.nii.gz ${transform_mat_file}
-#snipr_output_foldername='HEMISPHERE_MASK'
-#substring='nii'
+levelset_mask_file=$(ls ${working_dir}/MASKS/${scan_file_basename_no_ext}_resaved_levelset_bet.nii.gz)
+full_string=$(to_original_nifti_rf ${original_ct_file} ${levelset_mask_file} ${output_directory})
+bet_mask=$(echo "$full_string" | awk -F'_parsehere_' '{print $2}' | xargs)
+echo bet_mask::${bet_mask}
+
+## make bet of the gray image:
+#levelset_mask_file=$(ls ${working_dir}/MASKS/${scan_file_basename_no_ext}_resaved_levelset_bet.nii.gz)
+full_string=$(to_betgray_given_gray_n_binary ${original_ct_file} ${bet_mask} ${output_directory})
+bet_gray=$(echo "$full_string" | awk -F'_parsehere_' '{print $2}' | xargs)
+echo bet_gray::${bet_gray}
+### apply transform matrix to align to the template image:
+transform_mat_file=$(ls ${working_dir}/EDEMA_BIOMARKER/${scan_file_basename_no_ext}_resaved_levelset_brain_f_scct_strippedResampled1lin1.mat)
+echo "transform_files_with_given_mat_wrt_sccttemplate ${bet_gray} ${bet_gray%.nii*}_RF_scct.nii.gz ${transform_mat_file}"
+transform_files_with_given_mat_wrt_sccttemplate ${bet_gray} ${bet_gray%.nii*}_RF_scct.nii.gz ${transform_mat_file}
+transform_files_with_given_mat_wrt_sccttemplate ${half_brain_mask} ${half_brain_mask%.nii*}_RF_scct.nii.gz ${transform_mat_file}
+snipr_output_foldername='HEMISPHERE_MASK'
+substring='nii'
 #delete_a_file ${sessionID}  ${scanID} ${snipr_output_foldername} ${substring}
 #uploadsinglefile ${sessionID} ${scanID} $(dirname ${bet_gray%.nii*}_RF_scct.nii.gz) ${snipr_output_foldername} $(basename  ${bet_gray%.nii*}_RF_scct.nii.gz)
 #uploadsinglefile ${sessionID} ${scanID} $(dirname ${half_brain_mask%.nii*}_RF_scct.nii.gz) ${snipr_output_foldername} $(basename  ${half_brain_mask%.nii*}_RF_scct.nii.gz)

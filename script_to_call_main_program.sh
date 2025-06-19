d15a97f5d75e42b2431fe57c6a7b159fc8404b62
dharlabwustl <atulkumar@wustl.edu>
2025-06-19 14:18:57 -0500

    New Commitdgfdgf

--- script_to_call_main_program.sh
+++ script_to_call_main_program.sh
@@ -45,8 +45,8 @@ if [[ ${TYPE_OF_PROGRAM} == 'TRANFORM_BEFORE_DEEPREG_COLIHM62_MOVING' ]]; then
   /software/lin_transform_before_deepreg_COLIHM62_as_moving.sh $SESSION_ID $XNAT_USER $XNAT_PASS $XNAT_HOST /input1 /output
 fi
 if [[ ${TYPE_OF_PROGRAM} == 'TRANFORM_BEFORE_DEEPREG_COLIHM62_STATIC' ]]; then
-  echo " I AM AT TRANFORM_BEFORE_DEEPREG_COLIHM62_MOVING" >> /software/ERROR.txt
-  echo " I AM AT TRANFORM_BEFORE_DEEPREG_COLIHM62_MOVING"
+  echo " I AM AT TRANFORM_BEFORE_DEEPREG_COLIHM62_STATIC" >> /software/ERROR.txt
+  echo " I AM AT TRANFORM_BEFORE_DEEPREG_COLIHM62_STATIC"
   /software/lin_transform_before_deepreg_COLIHM62_as_static.sh $SESSION_ID $XNAT_USER $XNAT_PASS $XNAT_HOST /input1 /output
 fi
 

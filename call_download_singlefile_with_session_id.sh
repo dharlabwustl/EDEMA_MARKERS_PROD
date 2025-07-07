b43228790a59ad0eeda222f7b1a6c520a72b6b68
dharlabwustl <atulkumar@wustl.edu>
2025-07-07 14:47:47 -0500

    New Commitdgfdgf

--- call_download_singlefile_with_session_id.sh
+++ call_download_singlefile_with_session_id.sh
@@ -4,11 +4,11 @@ echo ${sessionID}
 ##get the selected scan id
 scanID=$(get_scan_id ${sessionID})
 echo ${scanID}
-#echo ${sessionID}::${scanID}
-#snipr_output_foldername='MASKS'
-#file_extension='ventricle'
-#outputdir='/workingoutput/'
-#echo "${sessionID} ${scanID} ${snipr_output_foldername} ${file_extension} ${outputdir} "
+echo ${sessionID}::${scanID}
+snipr_output_foldername='MASKS'
+file_extension='ventricle'
+outputdir='/workingoutput/'
+echo "${sessionID} ${scanID} ${snipr_output_foldername} ${file_extension} ${outputdir} "
 #function_with_arguments=('call_download_a_file_with_ext' ${sessionID} ${scanID} ${snipr_output_foldername} ${file_extension} ${outputdir} ) ##'warped_1_mov_mri_region_' )
 #echo "outputfiles_present="'$(python3 download_with_session_ID.py' "${function_with_arguments[@]}"
 #

71a1e1a0911179a1f405d5059817a252582ae205
dharlabwustl <atulkumar@wustl.edu>
2025-07-07 13:48:08 -0500

    New Commitdgfdgf

--- call_download_singlefile_with_session_id.sh
+++ call_download_singlefile_with_session_id.sh
@@ -3,4 +3,4 @@ resource_dir=${2} #'PREPROCESS_SEGM_4'  ###NIFTI'
 file_extension=${3} ##'_fixed_COLIHM620406202215542_lin1_BET.nii.gz' #'_brain_f.nii.gz'
 echo "./download_singlefile_with_session_id.sh $XNAT_USER $XNAT_PASS ${XNAT_HOST} ${session_id}   ${resource_dir} ${file_extension}"
 
-/download_singlefile_with_session_id.sh $XNAT_USER $XNAT_PASS ${XNAT_HOST} ${session_id}   ${resource_dir} ${file_extension}
+/software/download_singlefile_with_session_id.sh $XNAT_USER $XNAT_PASS ${XNAT_HOST} ${session_id}   ${resource_dir} ${file_extension}

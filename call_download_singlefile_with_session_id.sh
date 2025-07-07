9f3b56c25e5c5c3d0546b3852dff0d5120c8c4e9
dharlabwustl <atulkumar@wustl.edu>
2025-07-07 14:39:30 -0500

    New Commitdgfdgf

--- call_download_singlefile_with_session_id.sh
+++ call_download_singlefile_with_session_id.sh
@@ -2,7 +2,7 @@ source ./bash_utilities.sh
 sessionID=${1}
 echo ${sessionID}
 ##get the selected scan id
-#scanID=$(get_scan_id ${sessionID})
+scanID=$(get_scan_id ${sessionID})
 #echo ${sessionID}::${scanID}
 #snipr_output_foldername='MASKS'
 #file_extension='ventricle'

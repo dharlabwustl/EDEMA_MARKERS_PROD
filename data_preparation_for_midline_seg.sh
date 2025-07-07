77a715273ccf7c06051e838fd2f8488f62f67f83
dharlabwustl <atulkumar@wustl.edu>
2025-07-07 17:21:27 -0500

    New Commitdgfdgf

--- data_preparation_for_midline_seg.sh
+++ data_preparation_for_midline_seg.sh
@@ -9,8 +9,8 @@ echo ${sessionID}::${scanID}
 snipr_output_foldername='NIFTI'
 file_extension='.nii'
 outputdir='/workinginput/'
-download_a_single_file_with_ext "${sessionID} ${scanID} ${snipr_output_foldername} ${file_extension} ${outputdir} "
-original_ct_file=$(find ${outputdir} -name "*${file_extension}" )
+#download_a_single_file_with_ext "${sessionID} ${scanID} ${snipr_output_foldername} ${file_extension} ${outputdir} "
+#original_ct_file=$(find ${outputdir} -name "*${file_extension}" )
 ## download ventricle mask
 snipr_output_foldername='MASKS'
 file_extension='_ventricle_total.nii.gz'

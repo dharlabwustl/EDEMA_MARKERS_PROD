0ac00f70f1cb9222b8fa8b31957b20141a11b017
dharlabwustl <atulkumar@wustl.edu>
2025-07-07 19:44:54 -0500

    New Commitdgfdgf

--- bash_utilities.sh
+++ bash_utilities.sh
@@ -39,7 +39,8 @@ outputdir=${5} ##'/workingoutput/'
 #echo "${sessionID} ${scanID} ${snipr_output_foldername} ${file_extension} ${outputdir} "
 function_with_arguments=('call_download_a_file_with_ext' ${sessionID} ${scanID} ${snipr_output_foldername} ${file_extension} ${outputdir} ) ##'warped_1_mov_mri_region_' )
 #echo "outputfiles_present="'$(python3 download_with_session_ID.py' "${function_with_arguments[@]}"
-outputfiles_present=$(python3 download_with_session_ID.py "${function_with_arguments[@]}")
+#outputfiles_present=$(
+python3 download_with_session_ID.py "${function_with_arguments[@]}" ##)
 
 
 }

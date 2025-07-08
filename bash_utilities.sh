f7397683ad15a9fc51c008ea11b91bc0672641a1
dharlabwustl <atulkumar@wustl.edu>
2025-07-07 19:40:24 -0500

    New Commitdgfdgf

--- bash_utilities.sh
+++ bash_utilities.sh
@@ -29,16 +29,16 @@ get_scan_id(){
 }
 
 download_a_single_file_with_ext(){
- echo ${scanID}
-echo ${sessionID}::${scanID}
+# echo ${scanID}
+#echo ${sessionID}::${scanID}
 sessionID=${1}
 scanID=${2}
 snipr_output_foldername=${3} #'MASKS'
 file_extension=${4} #'infarct_auto_removesmall.nii.gz'
 outputdir=${5} ##'/workingoutput/'
-echo "${sessionID} ${scanID} ${snipr_output_foldername} ${file_extension} ${outputdir} "
+#echo "${sessionID} ${scanID} ${snipr_output_foldername} ${file_extension} ${outputdir} "
 function_with_arguments=('call_download_a_file_with_ext' ${sessionID} ${scanID} ${snipr_output_foldername} ${file_extension} ${outputdir} ) ##'warped_1_mov_mri_region_' )
-echo "outputfiles_present="'$(python3 download_with_session_ID.py' "${function_with_arguments[@]}"
+#echo "outputfiles_present="'$(python3 download_with_session_ID.py' "${function_with_arguments[@]}"
 outputfiles_present=$(python3 download_with_session_ID.py "${function_with_arguments[@]}")
 
 

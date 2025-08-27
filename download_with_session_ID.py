667d551de0fd78993b70f007e886736a342c24f7
dharlabwustl <atulkumar@wustl.edu>
2025-08-27 13:30:00 -0500

    New Commitdgfdgf

--- download_with_session_ID.py
+++ download_with_session_ID.py
@@ -1240,7 +1240,7 @@ def decision_which_nifti(sessionId, dir_to_receive_the_data="", output_csvfile="
     else:
         print("No scan selected")
         with open("/output/error.txt", "w") as file:
-            file.write(f"{sessionId}::Hello, this is a simple text file.\n")
+            file.write(f"not selected::{sessionId}::Hello, this is a simple text file.\n")
         return False
 
     # Export and upload

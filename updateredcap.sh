ab6dc8c7d945a523354934cd3370cfdb7ed4d5a1
DHAR LAB <106468137+dharlabwustl@users.noreply.github.com>
2025-10-20 06:35:46 -0500

    Update updateredcap.sh

--- updateredcap.sh
+++ updateredcap.sh
@@ -85,7 +85,7 @@ dir_to_save=${output_directory}
 
 fill_redcap_for_selected_scan_arguments=('call_pdf_fill_for_each_row' ${project_name} ${csvfilename} ${dir_to_save}) #${subj_listfile})
 # outputfiles_present=$(python3 uploadpdftoredcap.py "${fill_redcap_for_selected_scan_arguments[@]}") 
-echo ${fill_redcap_for_selected_scan_arguments}
+echo "${fill_redcap_for_selected_scan_arguments[@]}"
 echo  "${project_name} ${csvfilename} ${dir_to_save}"
 echo ${copy_session}
 exit

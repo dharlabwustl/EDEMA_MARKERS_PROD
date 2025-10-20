48a0bc58551add287ae7a10ba6a8f46ef21e6f97
DHAR LAB <106468137+dharlabwustl@users.noreply.github.com>
2025-10-20 10:09:37 -0500

    Update uploadpdftoredcap.py

--- uploadpdftoredcap.py
+++ uploadpdftoredcap.py
@@ -34,7 +34,7 @@ def fill_redcap_pdffilename(project_name,session_label,pdf_file_name):
         # api_token='EC6A2206FF8C1D87D4035E61C99290FF'
         df_scan_latest=download_latest_redcapfile(api_token,this_project_redcapfile_latest)
         this_session_redcap_repeat_instance_df=df_scan_latest[df_scan_latest['snipr_session']==session_label]
-        subprocess.call("echo " + "I am at fill_redcap_pdffilename  ::{} >> /workingoutput/error.txt".format(this_session_redcap_repeat_instance_df) ,shell=True )
+        subprocess.call("echo " + "I am at fill_redcap_pdffilename  ::{} >> /workingoutput/error.txt".format(session_label) ,shell=True )
         
         this_session_redcap_repeat_instance=str(this_session_redcap_repeat_instance_df['redcap_repeat_instance'].item())
         imaging_data_complete=str(this_session_redcap_repeat_instance_df['imaging_data_complete'].item())

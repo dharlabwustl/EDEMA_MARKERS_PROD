a5ee8ba26b85722af1c27a7de0a8180c1b3de293
DHAR LAB <106468137+dharlabwustl@users.noreply.github.com>
2025-10-20 07:45:50 -0500

    Update uploadpdftoredcap.py

--- uploadpdftoredcap.py
+++ uploadpdftoredcap.py
@@ -54,9 +54,10 @@ def call_pdf_fill_for_each_row(args):
         for _, row in df.iterrows():
             if '.pdf' in str(row['PDF_FILE_PATH']):
                 subprocess.call("echo " + "I AM AT {} ::{}  >> /workingoutput/error.txt".format(row['PDF_FILE_PATH'], inspect.stack()[0][3]), shell=True)
-
-                download_a_singlefile_with_URIString(str(row['PDF_FILE_PATH']),os.path.basename(str(row['PDF_FILE_PATH'])),output_dirname)
-                break
+                thissubjectnum=int(os.path.basename(str(row['PDF_FILE_PATH']).split('_')[1])
+                if thissubjectnum> 689:
+                    download_a_singlefile_with_URIString(str(row['PDF_FILE_PATH']),os.path.basename(str(row['PDF_FILE_PATH'])),output_dirname)
+                    break
             # fill_redcap_pdffilename(project_name,row['snipr_session'],os.path.join(ouput_dirname,os.path.basename(row['PDF_FILE_PATH'])))
     except:
         subprocess.call("echo " + "I FAILED AT ::{}  >> /workingoutput/error.txt".format(inspect.stack()[0][3]) ,shell=True )

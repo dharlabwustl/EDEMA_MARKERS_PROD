70086c6a2f448c6fe05ea59ae652fa2aeaf11df1
dharlabwustl <atulkumar@wustl.edu>
2025-10-29 13:13:08 -0500

    New Commitdgfdgf

--- delete_intermediate_files.sh
+++ delete_intermediate_files.sh
@@ -1,9 +1,9 @@
 echo ${1}
 #!/bin/bash
 # Usage: ./export_experiments.sh PROJECT_NAME OUTPUT_CSV
-
+output_dir=/workingoutput
 PROJECT="$1"
-OUTCSV="${1}_experiments.csv"
+OUTCSV="${output_dir}/${1}_experiments.csv"
 
 python3 - <<EOF
 from download_with_session_ID import export_project_experiments_to_csv

49ed283b9ccb692c27b748e04d2285a2f5b7ac5b
dharlabwustl <atulkumar@wustl.edu>
2025-11-03 20:30:02 -0600

    New Commitdgfdgf

--- download_files_with_given_proj_partialsessionlabel_ext_1.py
+++ download_files_with_given_proj_partialsessionlabel_ext_1.py
@@ -20,10 +20,10 @@ def given_experiment_n_scan_download_a_resource_flist(experiment_id,scan_id,reso
     URI=f'/data/experiments/{experiment_id}/scans/{scan_id}'
     metadata_resource=get_resourcefiles_metadata(URI, resource_dir)
     df_scan = pd.read_json(json.dumps(metadata_resource))
-    outputfie=os.path.join('/workingoutput',f'{experiment_id}_{scan_id}_{resource_dir}.csv'
+    outputfie=os.path.join('/workingoutput',f'{experiment_id}_{scan_id}_{resource_dir}.csv')
     pd.DataFrame(df_scan).to_csv(outputfie,index=False)
     return "X"
-def given_experiment_n_scan_download_a_resource_fflist():
+def given_experiment_find_selected_scan(experiment_id):
     return "X"
 def given_experiment_n_scan_resource_file_ext_download_the_files():
     return "X"

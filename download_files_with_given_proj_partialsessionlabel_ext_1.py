336d28b9b13a4320216f32c8cf39ce855dd95e16
dharlabwustl <atulkumar@wustl.edu>
2025-11-03 21:10:11 -0600

    New Commitdgfdgf

--- download_files_with_given_proj_partialsessionlabel_ext_1.py
+++ download_files_with_given_proj_partialsessionlabel_ext_1.py
@@ -48,10 +48,28 @@ def given_experiment_find_selected_scan(experiment_id):
             scan_id=str(row['ID'])
             print(scan_id)
             return scan_id
-
     except Exception as e:
         print(e)
 
     return "NO_SCAN_ID_FOUND"
-def given_experiment_n_scan_resource_file_ext_download_the_files():
-    return "X"
+def given_experiment_n_scan_download_a_resource_flist(experiment_id,scan_id,resource_dir):
+    URI=f'/data/experiments/{experiment_id}/scans/{scan_id}'
+    metadata_resource=get_resourcefiles_metadata(URI, resource_dir)
+    df_scan = pd.read_json(json.dumps(metadata_resource))
+    outputfie=os.path.join('/workingoutput',f'{experiment_id}_{scan_id}_{resource_dir}.csv')
+    pd.DataFrame(df_scan).to_csv(outputfie,index=False)
+    return outputfie
+    # return "X"
+
+def given_a_file_ext_n_resource_list_download_the_file(files_in_resource,extension):
+    try:
+        files_in_resource_df = pd.read_csv(files_in_resource)
+        for row_id, row in files_in_resource_df.iterrows():
+            if extension in str(row['URI']):
+                uri = str(row['URI'])
+                download_a_singlefile_with_URIString(
+                uri,
+                os.path.basename(uri),
+                '/workingoutput')
+    except Exception as e:
+        pass

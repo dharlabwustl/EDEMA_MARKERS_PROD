4bf122c20bd144fca4b6f43890cfdb542b7cc74a
dharlabwustl <atulkumar@wustl.edu>
2025-11-24 13:52:00 -0600

    New Commitdgfdgf

--- csf_compartments_vols_N_display_for_NON_SAH_local_computer.sh
+++ csf_compartments_vols_N_display_for_NON_SAH_local_computer.sh
@@ -452,8 +452,6 @@ betfile="NONE"  ##'/media/atul/WDJan2022/WASHU_WORKS/PROJECTS/DOCKERIZE/CSFSEPER
 csffile="NONE"  ##'/media/atul/WDJan2022/WASHU_WORKS/PROJECTS/DOCKERIZE/CSFSEPERATION/TESTING_CSF_SEPERATION/Krak_003_09042014_0949_MOZG_6.0_H31s_final_seg.nii.gz'
 NIFTI_SCAN_URI=''
 
-cp /EDEMABIOMARKEROUTPUT/*_infarct.png ${output_directory}/
-cp /EDEMABIOMARKEROUTPUT/*columndropped.csv ${output_directory}/
 
 for each_npy in  $(find /input/SCANS/2/PREPROCESS_SEGM_3/ -name '*.npy') ;  do  if [[ $each_npy  == *'V2'* ]] ; then  cp $each_npy ${working_dir_1} ; fi ; done
  for each_npy in  $(find /input/SCANS/2/PREPROCESS_SEGM_3/ -name '*.npy') ;  do  if [[ $each_npy  == *'.npy'* ]] ; then  cp $each_npy ${output_directory} ; fi ; done
@@ -791,8 +789,6 @@ call_calculate_volume ${mask_filename29} ${grayscale_filename_basename_noext}
         i=$(($i + 1))
 
         images[$i]=${output_directory}/${grayscale_filename_basename_noext}_resaved_levelset_CSF_COMPARTMENTS_${suffix}.jpg
-        i=$(($i + 1))
-        images[$i]=${output_directory}/${grayscale_filename_basename_noext}_resaved_levelset_${suffix}_infarct.png
         i=$(($i + 1))
 
         outputfiles_present=$(python3 utilities_simple_trimmed.py "${images[@]}")

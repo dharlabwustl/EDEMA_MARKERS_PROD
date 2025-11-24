deef2e3872b3e14540bedf2d91606c6b2045addd
dharlabwustl <atulkumar@wustl.edu>
2025-11-24 16:57:04 -0600

    New Commitdgfdgf

--- nwucalculation_session_level_allsteps_on_local_computer.sh
+++ nwucalculation_session_level_allsteps_on_local_computer.sh
@@ -35,6 +35,7 @@ cp /input/SCANS/2/NIFTI/*.* "${working_dir_1}/"
 cp /input/SCANS/2/PREPROCESS_SEGM/*.* "${working_dir}/"
 cp /input/SCANS/2/MASKS/*.*   "${working_dir}/"
 cp /input/SCANS/2/PREPROCESS_SEGM_3/*.*  ${output_directory}/
+cp /input/SCANS/2/PREPROCESS_SEGM/*.*  ${output_directory}/
 # Output dirs
 output_directory=/workingoutput #"${3:-/workingoutput}"
 final_output_directory=/outputinsidedocker ##"${4:-/outputinsidedocker}"

21444af54b4073d63496e43cbe7bb49244df9663
dharlabwustl <atulkumar@wustl.edu>
2025-11-24 12:36:29 -0600

    New Commitdgfdgf

--- script_to_call_main_program.sh
+++ script_to_call_main_program.sh
@@ -278,6 +278,7 @@ fi
 
 if [[ ${TYPE_OF_PROGRAM} == 'PDF_FOR_EDEMA_BIOMARKER_N_CSF_COMPARTMENT_LOCAL_COMPUTER' ]]; then
   /software/nwucalculation_session_level_allsteps_on_local_computer.sh $SESSION_ID $XNAT_USER $XNAT_PASS $XNAT_HOST /input1 /output
+  cp /workingoutput/*.* /EDEMABIOMARKEROUTPUT/
 #  /software/edema_biomarker_N_csf_compartments_vols_N_display_for_NON_SAH_local_computer.sh ${SESSION_ID} $XNAT_USER $XNAT_PASS $XNAT_HOST
 fi
 if [[ ${TYPE_OF_PROGRAM} == 'PDF_AFTER_CSF_COMPARTMENT_WITH_DEEPREG_FOR_NON_SAH_LOCAL_COMPUTER' ]]; then

6580e86684ea7b576b76fa6e29c02ef57b5ec9d1
dharlabwustl <atulkumar@wustl.edu>
2025-12-09 17:44:30 -0600

    New Commitdgfdgf

--- edema_biomarker_N_csf_compartments_vols_N_display_for_NON_SAH_REST_API.sh
+++ edema_biomarker_N_csf_compartments_vols_N_display_for_NON_SAH_REST_API.sh
@@ -484,8 +484,8 @@ returnvalue=$(python3 -c "from download_with_session_ID import downloadfile_with
 returnvalue=$(python3 -c "from download_with_session_ID import downloadfile_withasuffix; downloadfile_withasuffix('${sessionID}','${scanID}','${output_directory}','EDEMA_BIOMARKER','.')")
 
 
-for each_npy in  $(find /input/SCANS/${scanID}/PREPROCESS_SEGM_3/ -name '*.npy') ;  do  if [[ $each_npy  == *'V2'* ]] ; then  cp $each_npy ${working_dir_1} ; fi ; done
- for each_npy in  $(find /input/SCANS/${scanID}/PREPROCESS_SEGM_3/ -name '*.npy') ;  do  if [[ $each_npy  == *'.npy'* ]] ; then  cp $each_npy ${output_directory} ; fi ; done
+for each_npy in  $(find ${output_directory} -name '*.npy') ;  do  if [[ $each_npy  == *'V2'* ]] ; then  mv $each_npy ${working_dir_1} ; fi ; done
+# for each_npy in  $(find /input/SCANS/${scanID}/PREPROCESS_SEGM_3/ -name '*.npy') ;  do  if [[ $each_npy  == *'.npy'* ]] ; then  cp $each_npy ${output_directory} ; fi ; done
     split_masks_into_two_halves "_resaved_levelset_sulci_total.nii.gz"
     split_masks_into_two_halves "_resaved_levelset_sulci_above_ventricle.nii.gz"
     split_masks_into_two_halves "_resaved_levelset_sulci_at_ventricle.nii.gz"

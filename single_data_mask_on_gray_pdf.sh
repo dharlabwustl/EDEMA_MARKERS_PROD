c4da5c08ab7265b325c6526e425d555d19ed5ebe
dharlabwustl <atulkumar@wustl.edu>
2025-12-10 20:14:08 -0600

    New Commitdgfdgf

--- single_data_mask_on_gray_pdf.sh
+++ single_data_mask_on_gray_pdf.sh
@@ -43,11 +43,12 @@ call_masks_on_grayscale_colored_arguments=(
   "${midline_mask}"
   "${midline_mask}"
 )
-echo "python3 dividemasks_into_left_right_Nov20_2025.py ${call_masks_on_grayscale_colored_arguments[@]}"
+#echo "python3 dividemasks_into_left_right_Nov20_2025.py ${call_masks_on_grayscale_colored_arguments[@]}"
 outputfiles_present=$(python3 dividemasks_into_left_right_Nov20_2025.py "${call_masks_on_grayscale_colored_arguments[@]}")
 
 # ---- Loop over JPGs ----
 for x in "${outputfile_dir}/${grayscale_filename_basename_noext}"*.jpg; do
+  echo ${x}
   # If the glob doesn't match anything, skip
   [ -e "$x" ] || continue
 

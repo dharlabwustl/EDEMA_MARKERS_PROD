4478995d1a266349743c24ac06aa3b19e37cb7d3
dharlabwustl <atulkumar@wustl.edu>
2025-12-15 11:23:24 -0600

    New Commitdgfdgf

--- lin_transform_before_deepreg_COLIHM62_as_moving_mat_on_mask.sh
+++ lin_transform_before_deepreg_COLIHM62_as_moving_mat_on_mask.sh
@@ -524,7 +524,7 @@ template_prefix=$(basename ${fixed_image_filename%.nii*})
 /software/linear_rigid_registration_v10162024.sh ${moving_image_filename}  ${fixed_image_filename} ${output_directory}
 session_ct_bet_gray_lin_reg_output=${output_directory}/'mov_'$(basename ${moving_image_filename%.nii*})_fixed_$(basename  ${fixed_image_filename%.nii*})_lin1.nii.gz
 movingimage_gray_registered=${output_directory}/'mov_'$(basename ${moving_image_filename%.nii*})_fixed_$(basename  ${fixed_image_filename%.nii*})_lin1.nii.gz
-
+exit
 moving_image_filename=$(basename ${moving_image_filename})
 registration_mat_file=${working_dir}/'mov_'$(basename ${moving_image_filename%.nii*})_fixed_$(basename  ${fixed_image_filename%.nii*})_lin1.mat
 

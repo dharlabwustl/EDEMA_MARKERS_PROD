0eca910eee4037c7dd9c08b1dfde1ff7c240462c
dharlabwustl <atulkumar@wustl.edu>
2025-12-19 13:28:57 -0600

    New Commitdgfdgf

---
+++ download_analytics_from_railway.sh
@@ -0,0 +1,17 @@
+project_id=${1}
+todaydate="_from_railway_$(date +"%Y_%m_%d")"".csv"
+filename=${project_id}${todaydate}
+python3 -c "from utilities_using_xnat_python import call_apply_single_row_csv_to_table; call_apply_single_row_csv_to_table( '${project_id}','${filename}')"
+resource_label='ANALYTICS'
+local_file_path=${filename}
+remote_filename=${filename}
+python3 -c "from utilities_using_xnat_python import call_apply_single_row_csv_to_table; call_apply_single_row_csv_to_table( '${project_id}','${resource_label}','${filename}','${filename}')"
+#def upload_file_to_project_resource(
+#    project_id: str,
+#    resource_label: str,
+#    local_file_path: str,
+#    remote_filename: str | None = None,
+#    verify: bool = True,
+#    use_multipart: bool = True,
+#    log_file: str = "xnat_upload_errors.log",
+#)

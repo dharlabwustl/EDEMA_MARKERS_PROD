e4b25f09eab53c209162ec66be33271f3f3bba0f
dharlabwustl <atulkumar@wustl.edu>
2025-12-29 10:25:41 -0600

    New Commitdgfdgf

--- script_to_call_main_program.sh
+++ script_to_call_main_program.sh
@@ -129,6 +129,9 @@ fi
 if [[ ${TYPE_OF_PROGRAM} == 'TRANFORM_BEFORE_DEEPREG_V2' ]]; then
  source /software/lin_transform_before_deepreg_v2.sh $SESSION_ID $XNAT_USER $XNAT_PASS $XNAT_HOST /input1 /output
 fi
+if [[ ${TYPE_OF_PROGRAM} == 'TRANFORM_BEFORE_DEEPREG_V2_AT_RIS_APPLYDEEPREG_V2_SNIPR_CALL_LOCATION_DISTRIBUTION' ]]; then
+ source /software/lin_transform_before_deepreg_v3.sh $SESSION_ID $XNAT_USER $XNAT_PASS $XNAT_HOST /input1 /output
+fi
 
 if [[ ${TYPE_OF_PROGRAM} == 'MRI_TO_CT_REF_FRAME' ]]; then
   /software/lin_transform_before_deepreg_mri.sh $SESSION_ID $XNAT_USER $XNAT_PASS $XNAT_HOST /input1 /output

49dc7f1a2898df4f995f7a43b5adff4abfba2990
dharlabwustl <atulkumar@wustl.edu>
2026-01-07 10:50:39 -0600

    New Commitdgfdgf

--- test_a_code.sh
+++ test_a_code.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 SESSION_ID=${1}
-ID_VALUE=$(
-python3 -c "from utilities_using_xnat_python import log_error; print(log_error('${SESSION_ID}'))"
-)
+#ID_VALUE=$(
+#python3 -c "from utilities_using_xnat_python import log_error; print(log_error('${SESSION_ID}'))"
+#)
 
+python3 -c "from utilities_using_xnat_python import log_error; log_error('TEST MESSAGE'); print('done')"
+ls -l error.txt
+cat error.txt
 
 echo "NIFTI_LOCATION ID = ${ID_VALUE}"

d3dba10df112f517b6eba02ed53a5381d028ea7c
dharlabwustl <atulkumar@wustl.edu>
2026-01-07 10:34:13 -0600

    New Commitdgfdgf

--- test_a_code.sh
+++ test_a_code.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 ID_VALUE=$(
-python3 -c "from utilities_using_xnat_python import get_id_from_nifti_location_csv; print(get_id_from_nifti_location_csv('${SESSION_ID}'))"
+python3 -c "from utilities_using_xnat_python import log_error; print(log_error('${SESSION_ID}'))"
 )
 
 

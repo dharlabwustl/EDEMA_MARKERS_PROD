cf05930823e528c24b3124a1a45b76bee8268ca2
dharlabwustl <atulkumar@wustl.edu>
2026-01-07 14:18:29 -0600

    New Commitdgfdgf

--- upload_pdf_to_redcap.sh
+++ upload_pdf_to_redcap.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 SESSION_ID=${1}
-echo "REDCAP_API::${REDCAP_API}"
-#exit
+echo "REDCAP_API::${REDCAP_API}::${SESSION_ID}"
+exit
 #ID_VALUE=$(
 #python3 -c "from utilities_using_xnat_python import log_error; print(log_error('${SESSION_ID}'))"
 #)

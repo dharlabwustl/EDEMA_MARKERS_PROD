c50bcd705d38189641062a4c25b6b74b84d7b0ad
dharlabwustl <atulkumar@wustl.edu>
2026-01-07 13:15:37 -0600

    New Commitdgfdgf

--- downloadcodefromgithub.sh
+++ downloadcodefromgithub.sh
@@ -8,13 +8,13 @@ mv ${git_dir}/* /software/
 chmod +x /software/*.sh 
 
 SESSION_ID=${1}
-XNAT_USER=${2}
-XNAT_PASS=${3}
-export XNAT_USER=$XNAT_USER
-export XNAT_PASS=$XNAT_PASS
-echo XNAT_USER=$XNAT_USER
-echo XNAT_PASS=$XNAT_PASS
+#XNAT_USER=${2}
+#XNAT_PASS=${3}
+#export XNAT_USER=$XNAT_USER
+#export XNAT_PASS=$XNAT_PASS
+#echo XNAT_USER=$XNAT_USER
+#echo XNAT_PASS=$XNAT_PASS
 TYPE_OF_PROGRAM=${5}
-export XNAT_HOST=${6}
-export REDCAP_API=${7}
+#export XNAT_HOST=${6}
+#export REDCAP_API=${7}
 /software/script_to_call_main_program.sh $SESSION_ID $XNAT_USER $XNAT_PASS ${TYPE_OF_PROGRAM} ${6}

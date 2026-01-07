497cce2d373dabe3d425c8e50dca9ed0f8095cfa
dharlabwustl <atulkumar@wustl.edu>
2026-01-07 11:06:47 -0600

    New Commitdgfdgf

--- utilities_using_xnat_python.py
+++ utilities_using_xnat_python.py
@@ -35,7 +35,7 @@ from datetime import datetime
 
 LOG_FILE = "/software/railway_db_errors.log"
 
-ERROR_FILE = "/software/error.txt"
+ERROR_FILE = "/software/error.log"
 def log_error(msg):
     ts = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     err = (

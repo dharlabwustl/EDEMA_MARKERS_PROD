a816c02ca81d72b69bba639378d6ce012eb82989
dharlabwustl <atulkumar@wustl.edu>
2026-01-07 12:34:35 -0600

    New Commitdgfdgf

--- test_a_code.sh
+++ test_a_code.sh
@@ -46,12 +46,13 @@ python3 -c "from utilities_using_xnat_python import get_session_label_from_sessi
 print(get_session_label_from_session_id('${SESSION_ID}'))"
 )
 echo "PROJECT_ID=$PROJECT_ID"
-echo "SUBJECT_LABEL=${SESSION_LABEL}"
+echo "SUBJECT_LABEL=${SUBJECT_LABEL}"
 echo "SESSION_LABEL=${SESSION_LABEL}"
 echo "OUT_PATH=${OUT_PATH}"
 #fill_redcap_for_selected_scan_arguments=('fill_redcap_for_pdffile_given_subject_label' ${xml_filename} ${pdffilename}) #${subj_listfile})
 #outputfiles_present=$(python3 download_with_session_ID.py "${fill_redcap_for_selected_scan_arguments[@]}")
 #subject_name,session_label,project_name,file_name
-#ID_VALUE=$(
-#python3 -c "from download_with_session_ID import fill_redcap_for_pdffile_given_subject_label; print(fill_redcap_for_pdffile_given_subject_label('${SUBJECT_LABEL}','${SESSION_LABEL}','${PROJECT_ID},'${OUT_PATH}'))"
-#)
+SUCCESS_VALUE_0_FAIL_1=$(
+python3 -c "from download_with_session_ID import fill_redcap_for_pdffile_given_subject_label; print(fill_redcap_for_pdffile_given_subject_label('${SUBJECT_LABEL}','${SESSION_LABEL}','${PROJECT_ID},'${OUT_PATH}'))"
+)
+echo SUCCESS_VALUE_0_FAIL_1="${SUCCESS_VALUE_0_FAIL_1}"

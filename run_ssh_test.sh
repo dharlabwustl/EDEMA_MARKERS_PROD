6226d927a15ffa975ec0aef66c92e3ac49ae7397
dharlabwustl <atulkumar@wustl.edu>
2026-01-12 10:01:19 -0600

    New Commitdgfdgf

--- run_ssh_test.sh
+++ run_ssh_test.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 SESSION_ID=${1} ##'SNIPR01_E00001'
 # Optional: command passed as arguments; otherwise run a simple test
 REMOTE_CMD="${*:-hostname; whoami; pwd}"
-echo "I AM HERE IN THE DOCKER"
+echo "I AM HERE IN THE DOCKER::${SESSION_ID}"
 sshpass -p "$CLUSTER_PASS" \
   ssh \
     -o StrictHostKeyChecking=no \

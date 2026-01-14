3df28cd967951e05ab6b743152922e3a3d0a28b6
dharlabwustl <atulkumar@wustl.edu>
2026-01-14 11:26:17 -0600

    New Commitdgfdgf

--- scan_selection_fill_REDCap_Feb09_2024.sh
+++ scan_selection_fill_REDCap_Feb09_2024.sh
@@ -550,7 +550,7 @@ if  ls ${working_dir}/*_NIFTILOCATION.csv 1> /dev/null 2>&1; then
 #  fill_redcap_for_selected_scan_arguments=('fill_redcap_for_selected_scan' ${xml_filename} ${csvfile_for_redcap} ) #${subj_listfile})
 #  outputfiles_present=$(python3 download_with_session_ID.py "${fill_redcap_for_selected_scan_arguments[@]}")
   SUCCESS_VALUE_0_FAIL_1=$(
-python3 -c "from download_with_session_ID import fill_redcap_for_selected_scan_01142026; print(fill_redcap_for_selected_scan_01142026('${SESSION_ID}','${csvfile_for_redcap}','${xml_filename}'))"
+python3 -c "from download_with_session_ID import fill_redcap_for_selected_scan_01142026; print(fill_redcap_for_selected_scan_01142026('${sessionID}','${csvfile_for_redcap}','${xml_filename}'))"
 )
   echo SUCCESS_VALUE_0_FAIL_1::${SUCCESS_VALUE_0_FAIL_1}
 fi

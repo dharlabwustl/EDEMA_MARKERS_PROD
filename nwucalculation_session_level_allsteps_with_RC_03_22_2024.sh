d791a51afa21269177d8516b92221ea41bef70ee
dharlabwustl <atulkumar@wustl.edu>
2026-01-15 22:52:53 -0600

    New Commitdgfdgf

--- nwucalculation_session_level_allsteps_with_RC_03_22_2024.sh
+++ nwucalculation_session_level_allsteps_with_RC_03_22_2024.sh
@@ -360,8 +360,9 @@ for niftifile_csvfilename in ${working_dir}/*NIFTILOCATION.csv; do
 #          fill_redcap_for_selected_scan_arguments=('fill_redcap_for_selected_scan' ${xml_filename} ${csvfile_for_redcap}) #${subj_listfile})
 #          outputfiles_present=$(python3 download_with_session_ID.py "${fill_redcap_for_selected_scan_arguments[@]}")
               /software/write_to_redcap.sh ${sessionID} ${csvfile_for_redcap}
-          fill_redcap_for_selected_scan_arguments=('fill_redcap_for_pdffile' ${xml_filename} ${pdffilename}) #${subj_listfile})
-          outputfiles_present=$(python3 download_with_session_ID.py "${fill_redcap_for_selected_scan_arguments[@]}")
+              /software/upload_pdf_to_redcap.sh ${sessionID} ##${csvfile_for_redcap}
+#          fill_redcap_for_selected_scan_arguments=('fill_redcap_for_pdffile' ${xml_filename} ${pdffilename}) #${subj_listfile})
+#          outputfiles_present=$(python3 download_with_session_ID.py "${fill_redcap_for_selected_scan_arguments[@]}")
 
 
 

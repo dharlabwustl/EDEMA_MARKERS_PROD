9aeb4b791f8de1c4d07c4e6b0c955b0f20e37f55
dharlabwustl <atulkumar@wustl.edu>
2026-02-03 13:21:00 -0600

    New Commitdgfdgf

--- upload_sessions_list_to_railway.sh
+++ upload_sessions_list_to_railway.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-project_id="ICH"
+project_id="SAH"
 python3 -c "from utilities_using_xnat_python import create_new_sessionlist_table_in_railway; create_new_sessionlist_table_in_railway('${project_id}')"
 echo "TABLE SEARCH DONE"

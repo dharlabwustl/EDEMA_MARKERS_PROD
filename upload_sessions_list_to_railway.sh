e05832f8d978e48ae77bc4205a679722007ac116
dharlabwustl <atulkumar@wustl.edu>
2026-02-03 12:07:01 -0600

    New Commitdgfdgf

--- upload_sessions_list_to_railway.sh
+++ upload_sessions_list_to_railway.sh
@@ -2,3 +2,4 @@
 
 project_id="COLI"
 python3 -c "from utilities_using_xnat_python import create_new_sessionlist_table_in_railway; create_new_sessionlist_table_in_railway('${project_id}')"
+echo "TABLE SEARCH DONE"

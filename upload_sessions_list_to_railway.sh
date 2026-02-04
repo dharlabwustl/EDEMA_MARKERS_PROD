122baa789fb3f4aa13e2ac6105afc8e93bbd3cce
dharlabwustl <atulkumar@wustl.edu>
2026-02-04 10:47:16 -0600

    New Commitdgfdgf

--- upload_sessions_list_to_railway.sh
+++ upload_sessions_list_to_railway.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for x in SNIPR02_E01687 ;  #"COLI" "BM" ; ##,"SAH","ICH","Krakow","BJH","WashU")
+for x in SNIPR02_E01687 SNIPR02_E02550 ;  #"COLI" "BM" ; ##,"SAH","ICH","Krakow","BJH","WashU")
 do
 project_id=$x ##"SAH"
 python3 -c "from utilities_using_xnat_python import fill_after_dicom2nifti; fill_after_dicom2nifti('${project_id}')"

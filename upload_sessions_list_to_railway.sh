6f008ccd389418df49f4f2d00cf0277161b7571f
dharlabwustl <atulkumar@wustl.edu>
2026-02-04 10:14:33 -0600

    New Commitdgfdgf

--- upload_sessions_list_to_railway.sh
+++ upload_sessions_list_to_railway.sh
@@ -3,7 +3,7 @@
 for x in SNIPR02_E02550 ;  #"COLI" "BM" ; ##,"SAH","ICH","Krakow","BJH","WashU")
 do
 project_id=$x ##"SAH"
-python3 -c "from utilities_using_xnat_python import count_z_axial_scans_in_session; count_z_axial_scans_in_session('${project_id}')"
+python3 -c "from utilities_using_xnat_python import fill_after_dicom2nifti; fill_after_dicom2nifti('${project_id}')"
 #python3 -c "from utilities_using_xnat_python import create_new_sessionlist_table_in_railway; create_new_sessionlist_table_in_railway('${project_id}')"
 #count_z_axial_scans_in_session
 echo "TABLE SEARCH DONE"

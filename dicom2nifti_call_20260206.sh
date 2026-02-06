c7dcae589c6b400ec4997492110927f4f012446f
dharlabwustl <atulkumar@wustl.edu>
2026-02-06 14:13:07 -0600

    New Commitdgfdgf

--- dicom2nifti_call_20260206.sh
+++ dicom2nifti_call_20260206.sh
@@ -5,5 +5,5 @@ export XNAT_HOST=${4} #"https://snipr-dev-test1.nrg.wustl.edu"
 #export REDCAP_HOST=${5}  #'https://redcap.wustl.edu/redcap/api/' #
 #export REDCAP_API=${5} #'EC6A2206FF8C1D87D4035E61C99290FF' #
 sessionID=${1}
-python3 -c "from dicom2nifti_20260206.py import run_dicom2nifti_for_session; run_dicom2nifti_for_session('${1}')"
+python3 -c "from dicom2nifti_20260206 import run_dicom2nifti_for_session; run_dicom2nifti_for_session('${1}')"
 #python3 -c "from utilities_using_xnat_python import fill_after_dicom2nifti; fill_after_dicom2nifti('${1}')"

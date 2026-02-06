fa325b7e1add56a0d87545f93ae2aabfa4f97e08
dharlabwustl <atulkumar@wustl.edu>
2026-02-06 12:43:37 -0600

    New Commitdgfdgf

--- dicom2nifti_call_sessionlevel_selected.sh
+++ dicom2nifti_call_sessionlevel_selected.sh
@@ -2,6 +2,7 @@
 export XNAT_USER=${2} 
 export XNAT_PASS=${3} 
 export XNAT_HOST=${4} #"https://snipr-dev-test1.nrg.wustl.edu"
-python3 /software/dicom2nifiti_sessionlevel_selected.py  ${1}  
+python3 /software/dicom2nifiti_sessionlevel_selected.py  ${1}
+python3 -c "from utilities_using_xnat_python import fill_after_dicom2nifti; fill_after_dicom2nifti('${1}')"
 
 

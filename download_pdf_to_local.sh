7fa95b1958b77da8ceb6e1b6765e36796a5460e3
dharlabwustl <atulkumar@wustl.edu>
2026-02-10 15:50:00 -0600

    New Commitdgfdgf

--- download_pdf_to_local.sh
+++ download_pdf_to_local.sh
@@ -12,7 +12,7 @@ ID_VALUE=$(
 python3 -c "from utilities_using_xnat_python import get_id_from_nifti_location_csv; print(get_id_from_nifti_location_csv('${SESSION_ID}'))"
 )
 
-RESOURCE_DIR="EDEMA_BIOMARKER_WITH_COMPARTMENTS" #"EDEMA_BIOMARKER_WITH_COMPARTMENTS" ##"EDEMA_BIOMARKER"
+RESOURCE_DIR="SAH_CSF_ANALYSIS" ###"EDEMA_BIOMARKER_WITH_COMPARTMENTS" #"EDEMA_BIOMARKER_WITH_COMPARTMENTS" ##"EDEMA_BIOMARKER"
 EXT='.pdf'
 LATEST_URI=$(
 python3 -c "from utilities_using_xnat_python import get_latest_file_uri_from_scan_resource; \

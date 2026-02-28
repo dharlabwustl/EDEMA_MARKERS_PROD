87f4d674ffc764c20097ee6f4dfecc5566e27867
sharmaatul11@gmail.com <your@email.com>
2026-02-28 15:38:54 -0600

    New Commitdgfdgf

--- delete_files_given_pattern.sh
+++ delete_files_given_pattern.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 sessionID=${1}
-nifti_id=$(python3 - <<EOF
+scanID=$(python3 - <<EOF
 from utilities_using_xnat_python import get_id_from_nifti_location_csv
 
 val = get_id_from_nifti_location_csv("${sessionID}")
@@ -10,5 +10,36 @@ if val is not None:
     print(val)
 EOF
 )
+resourceName="MASKS"
+pattern='ventricle'
+patternType='regex'
+echo "NIFTI_ID = $scanID"
+python3 - <<EOF
+from utilities_using_xnat_python import xnat_delete_files_in_scan_resource_by_pattern
 
-echo "NIFTI_ID = $nifti_id"
+summary = xnat_delete_files_in_scan_resource_by_pattern(
+    session_id="${sessionID}",
+    scan_id="${scanID}",
+    resource_name="${resourceName}",
+    pattern=r"${pattern}",
+    pattern_type="${patternType}",   # "regex" or "glob"
+    dry_run=False
+)
+
+print(summary)
+EOF
+pattern='total'
+python3 - <<EOF
+from utilities_using_xnat_python import xnat_delete_files_in_scan_resource_by_pattern
+
+summary = xnat_delete_files_in_scan_resource_by_pattern(
+    session_id="${sessionID}",
+    scan_id="${scanID}",
+    resource_name="${resourceName}",
+    pattern=r"${pattern}",
+    pattern_type="${patternType}",   # "regex" or "glob"
+    dry_run=False
+)
+
+print(summary)
+EOF
